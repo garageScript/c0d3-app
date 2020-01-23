bdf2f77aeb9e3842ea79be4da1afe4cccd073b5f
hwong0305 <hwong.0305@gmail.com>
2020-01-22 21:08:28 -0800

    initial tmeplate setup with next.js and typescript

---
+++ pages/login.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Login: React.FC = () => {
+  return <h1>Login</h1>
+}
+
+export default Login
---
+++ pages/register.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Register: React.FC = () => {
+  return <h1>Register</h1>
+}
+
+export default Register
