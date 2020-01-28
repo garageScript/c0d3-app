16dbd5785187a95ac7488f0140101a508592b5d3
hwong0305 <hwong.0305@gmail.com>
2020-01-28 00:36:17 -0800

    Removed Tailwind.stories which was used for testing

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,6 +4,10 @@ type Props = {
   text: string
 }
 
-const Button = ({ text }: Props) => <button>{text}</button>
+const Button = ({ text }: Props) => (
+  <button className="bg-blue-500 hover: bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+    {text}
+  </button>
+)
 
 export default Button
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import Button from '../components/Button'
+import '../styles/tailwind.css'
 
 storiesOf('Button', module).add('with text', () => {
   return <Button text="Click Me" />
