c3b8eab960e73b37881799f248c839c55f0cf2a9
David De Wulf <ledavid64@gmail.com>
2020-02-05 20:21:56 -0800

    Change let to const

--- __tests__/pages/components/InputGroup.js
+++ __tests__/pages/components/InputGroup.js
@@ -5,25 +5,25 @@ import InputGroup from '../../../components/InputGroup'
 describe('Snapshot for InputGroup', () => {
   test('Small InputGroup', () => {
     const component = renderer.create(<InputGroup size="small" />)
-    let tree = component.toJSON()
+    const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   test('Default InputGroup', () => {
     const component = renderer.create(<InputGroup />)
-    let tree = component.toJSON()
+    const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   test('Large InputGroup', () => {
     const component = renderer.create(<InputGroup size="large" />)
-    let tree = component.toJSON()
+    const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 
   test('Large InputGroup', () => {
     const component = renderer.create(<InputGroup isDisabled />)
-    let tree = component.toJSON()
+    const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
