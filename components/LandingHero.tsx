6d9647480b4d3a567073c5107c023be8d7952399
hwong0305 <hwong.0305@gmail.com>
2020-02-05 23:23:33 -0800

    Added new section

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -10,22 +10,69 @@ const LandingHero = () => {
       </div>
       <div className="row justify-center mt-5">
         <div className="col-md-8 offset-md-2">
-          <h1 style={{ fontWeight: 900 }} className="display-4 md-display-3">
+          <h1 style={{ fontWeight: 900 }} className="display-4">
             Learn Javascript the old school way
           </h1>
         </div>
       </div>
-      <div className="row justify-center mt-3">
-        <div className="col-md-8 offset-md-2">
-          <h4>
-            Learn all the foundations you need to be a full stack software
+      <div className="row justify-center mt-4">
+        <div className="col-md-6 offset-md-3">
+          <h4 style={{ lineHeight: 1.5 }} className="font-weight-normal">
+            Learn all the foundations you would need to be a full stack software
             engineer
           </h4>
         </div>
       </div>
-      <div className="row mt-3">
-        <div className="col-md-6 offset-md-3">
+      <div className="row mt-5">
+        <div className="col-md-6 offset-md-3 mb-5">
           <button className="btn btn-primary py-2 px-4">Get Started</button>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col-md-6 offset-md-3">
+          <h1 className="font-weight-bold">Why Choose Us</h1>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col-md-10 offset-md-1">
+          <h4 className="font-weight-normal" style={{ lineHeight: 1.5 }}>
+            Our learning process is interactive and follows the same practice as
+            a well functioning engineering team. From the first line of code you
+            write, you will be code reviewed by engineers to ensure you are
+            always following industry best practices.
+          </h4>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col-md-4">
+          <img src="https://dummyimage.com/212x141/000/fff"></img>
+          <h4>Updated and modern curriculum</h4>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            illum consectetur earum temporibus obcaecati repudiandae esse, sed
+            necessitatibus, voluptas dolorum adipisci molestiae magni
+            praesentium doloribus odio laudantium, consequuntur aliquam? Ad?
+          </p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://dummyimage.com/212x141/000/fff"></img>
+          <h4>Real-time help from your mentors and peer</h4>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            quos magni cum voluptates autem molestiae vel, eligendi ipsam
+            eveniet omnis laudantium ipsa similique distinctio numquam aliquid
+            quis odio? Nostrum, aspernatur!
+          </p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://dummyimage.com/212x141/000/fff" />
+          <h4>Practical experience by working with other engineers</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            recusandae dolorum fuga tempore officiis exercitationem eum debitis
+            necessitatibus facere praesentium molestiae, iure iusto accusantium
+            a ullam cumque atque sapiente accusamus.
+          </p>
         </div>
       </div>
     </div>
