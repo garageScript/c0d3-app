89ce9096cf304eaf58332a0751e70169fb07e4a6
hwong0305 <hwong.0305@gmail.com>
2020-02-05 23:53:24 -0800

    Added spacing layout

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -10,7 +10,7 @@ const LandingHero = () => {
       </div>
       <div className="row justify-center mt-5">
         <div className="col-md-8 offset-md-2">
-          <h1 style={{ fontWeight: 900 }} className="display-4">
+          <h1 style={{ fontWeight: 900, fontSize: '4rem' }}>
             Learn Javascript the old school way
           </h1>
         </div>
@@ -25,12 +25,12 @@ const LandingHero = () => {
       </div>
       <div className="row mt-5">
         <div className="col-md-6 offset-md-3 mb-5">
-          <button className="btn btn-primary py-2 px-4">Get Started</button>
+          <button className="btn btn-primary py-3 px-5">Get Started</button>
         </div>
       </div>
       <div className="row mt-5">
         <div className="col-md-6 offset-md-3">
-          <h1 className="font-weight-bold">Why Choose Us</h1>
+          <h1 className="display-4 font-weight-bold">Why choose us?</h1>
         </div>
       </div>
       <div className="row mt-5">
@@ -46,8 +46,10 @@ const LandingHero = () => {
       <div className="row mt-5">
         <div className="col-md-4">
           <img src="https://dummyimage.com/212x141/000/fff"></img>
-          <h4>Updated and modern curriculum</h4>
-          <p>
+          <h4 className="mt-3 font-weight-bold">
+            Updated and modern curriculum
+          </h4>
+          <p className="mt-4">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
             illum consectetur earum temporibus obcaecati repudiandae esse, sed
             necessitatibus, voluptas dolorum adipisci molestiae magni
@@ -56,8 +58,10 @@ const LandingHero = () => {
         </div>
         <div className="col-md-4">
           <img src="https://dummyimage.com/212x141/000/fff"></img>
-          <h4>Real-time help from your mentors and peer</h4>
-          <p>
+          <h4 className="mt-3 font-weight-bold">
+            Real-time help from your mentors and peer
+          </h4>
+          <p className="mt-4">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore
             quos magni cum voluptates autem molestiae vel, eligendi ipsam
             eveniet omnis laudantium ipsa similique distinctio numquam aliquid
@@ -66,8 +70,10 @@ const LandingHero = () => {
         </div>
         <div className="col-md-4">
           <img src="https://dummyimage.com/212x141/000/fff" />
-          <h4>Practical experience by working with other engineers</h4>
-          <p>
+          <h4 className="mt-3 font-weight-bold">
+            Practical experience by working with other engineers
+          </h4>
+          <p className="mt-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
             recusandae dolorum fuga tempore officiis exercitationem eum debitis
             necessitatibus facere praesentium molestiae, iure iusto accusantium
