6e79a7997929994f630978de2189eba6d3011f50
rkalra247 <rkalra247@gmail.com>
2020-02-08 15:30:08 -0800

    testing login page ui

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react'
 
 const Login: React.FC = () => {
-  return <h1>Login</h1>
+  return (
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Panel title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Panel subtitle</h6>
+        <p class="card-text">Some quick example text to build on the panel title and make up the bulk of the panel's content.</p>
+        <a href="#!" class="card-link">Card link</a>
+        <a href="#!" class="card-link">Another link</a>
+      </div>
+    </div>
+  )
 }
 
 export default Login
