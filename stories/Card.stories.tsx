93a6a80825098ba51e88dcb01854f05e3ab58d70
David De Wulf <ledavid64@gmail.com>
2020-02-08 18:29:22 -0800

    Add Stories

---
+++ stories/Card.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Card from '../components/Card'
+
+export default {
+  component: Card,
+  title: 'Card'
+}
+
+export const basic: React.FC = () => <Card title="Title of the card" />
+
+export const withText: React.FC = () => (
+  <Card
+    title="Title of the card"
+    text="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+  />
+)
+
+export const withChildren: React.FC = () => (
+  <Card
+    title="Title of the card"
+    text="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+  >
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text" id="inputGroup-sizing-default">
+          Default
+        </span>
+      </div>
+      <input
+        type="text"
+        className="form-control"
+        aria-label="Sizing example input"
+        aria-describedby="inputGroup-sizing-default"
+      />
+      <div className="col-5">
+        <button type="button" className="btn btn-dark">
+          Primary
+        </button>
+      </div>
+    </div>
+  </Card>
+)
+
+export const withSuccess: React.FC = () => (
+  <Card
+    success
+    title="Title of the card"
+    text="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+  />
+)
