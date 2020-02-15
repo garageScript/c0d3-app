5f0696e62d8d2bdc7d737836ce14a8c9365e24a9
David De Wulf <ledavid64@gmail.com>
2020-02-14 22:11:58 -0800

    Update formValidation schema

--- helpers/formValidation.tsx
+++ helpers/formValidation.tsx
@@ -3,12 +3,16 @@ import * as Yup from 'yup'
 const TEXT_MIN = 2
 const TEXT_MAX = 20
 const PASSWORD_MIN = 6
+const REGEX_ALPHANUMERICS = /^[a-zA-Z0-9_]*$/
 
 const signupValidation = Yup.object({
   email: Yup.string()
     .email('Invalid email address')
     .required('Required'),
   userName: Yup.string()
+    .strict(true)
+    .lowercase('Must be a lowercase string')
+    .matches(REGEX_ALPHANUMERICS, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
     .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .required('Required'),
@@ -17,10 +21,12 @@ const signupValidation = Yup.object({
     .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .required('Required'),
   firstName: Yup.string()
+    .matches(REGEX_ALPHANUMERICS, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
     .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .required('Required'),
   lastName: Yup.string()
+    .matches(REGEX_ALPHANUMERICS, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
     .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .required('Required')
