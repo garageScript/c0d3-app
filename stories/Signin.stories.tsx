6012d4de4fc0dbeed97763c5bd1e66597657853e
rkalra247 <rkalra247@gmail.com>
2020-02-25 11:37:17 -0800

    update stories with Login Props

--- stories/Signin.stories.tsx
+++ stories/Signin.stories.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import Login from '../pages/login'
+import { Props } from '../@types/login'
 
 export default {
   component: Login,
   title: 'Pages/signin'
 }
 
-export const Signin: React.FC = () => <Login />
+export const Signin: React.FC<Props> = props => <Login submitLogin={() => {}} />
