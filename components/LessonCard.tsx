875d8c3343a1e2168afebaf0d546adf07c7d60ca
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-02-26 23:54:50 -0800

    Improved for mobile

--- components/LessonCard.tsx
+++ components/LessonCard.tsx
@@ -14,19 +14,17 @@ type Props = {
 const LessonCard: React.FC<Props> = props => {
   return (
     <>
-      {/* <div className="row mt-5"> */}
       <div className="card shadow-sm mt-5">
         <div className="card-body p-1">
           <div className="row no-gutters">
-            {/* <div className="col- cover-img px-1 py-2"> */}
-            <div className="cover-img col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 px-1 py-2">
+            <div className="cover-img col-6 col-sm-6 col-md-3 col-lg-3 col-xl-3 pl-1 pr-4 py-2 w-25 mx-auto">
               <img
+                className="card-img"
                 src={`/curriculumAssets/lessonCoversSvg/${props.coverImg}`}
                 alt={props.coverImg}
               />
             </div>
-            <div className="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9 my-3">
-              {/* <div className="col-auto my-3"> */}
+            <div className="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 my-3">
               <h4 className="card-title font-weight-bold">{props.title}</h4>
               <div className="icon-messages mt-3">
                 <div className="mr-3">
--- pages/curriculum.tsx
+++ pages/curriculum.tsx
@@ -41,8 +41,10 @@ const Curriculum: React.FC = () => {
       <Layout>
         <>
           <div className="row">
-            <div className="col-8">{sortedLessons}</div>
-            <div className="col-4"></div>
+            <div className="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+              {sortedLessons}
+            </div>
+            <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4"></div>
           </div>
         </>
       </Layout>
