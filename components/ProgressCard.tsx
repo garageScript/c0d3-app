9a1d443dc39ee3cf8d3561e1462e20366a9ca496
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-03 12:55:15 -0800

    Removed most CSS and added bootstrap classes

--- components/ProgressCard.tsx
+++ components/ProgressCard.tsx
@@ -8,9 +8,9 @@ type Props = {
 
 const ProgressCard: React.FC<Props> = ({ progressCount }) => {
   return (
-    <div className="progress-card__container card shadow-sm mt-3">
+    <div className="progress-card__container d-flex card shadow-sm mt-3 bg-primary text-white p-2">
       <div className="card-body">
-        <div className="progress-card__progress-bar">
+        <div className="progress-card__progress-bar d-flex justify-content-center align-items-center rounded-circle">
           <span className="font-weight-bold">{progressCount}%</span>
         </div>
         <h4 className="progress-card__title mt-3">
@@ -23,7 +23,7 @@ const ProgressCard: React.FC<Props> = ({ progressCount }) => {
           </p>
         </div>
         <a
-          className="btn progress-card__button mt-2"
+          className="btn btn-light progress-card__button mt-2 text-primary"
           href="https://www.notion.so/Setup-Instructions-fc8f8fcc1376482ead839fa6b1034cb4#10ab6286f4d942c58bd330f68cb8cb95"
         >
           Setup Development Environment
