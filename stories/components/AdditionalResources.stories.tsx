92c78ced0bbe172687ade209809855194e2358bc
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-04 02:36:34 -0800

    Added AdditionalResources story

---
+++ stories/components/AdditionalResources.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import AdditionalResources from '../../components/AdditionalResources'
+
+export default {
+  component: AdditionalResources,
+  title: 'Components/AdditionalResources'
+}
+
+export const Basic: React.FC = () => <AdditionalResources />
