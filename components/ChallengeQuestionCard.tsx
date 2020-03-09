8c47f90bef64afe8749fa23f1dc683fe67aa2889
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-08 19:34:58 -0700

    Added back shadow for cards

--- components/ChallengeQuestionCard.tsx
+++ components/ChallengeQuestionCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const ChallengeQuestionCard: React.FC<Props> = props => {
   return (
-    <div className="card border-0">
+    <div className="card shadow-sm border-0">
       <div className="card-body">
         <h1 className="challenge-question-card__title">{props.title}</h1>
         <div>
