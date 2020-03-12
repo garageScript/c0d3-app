bfb4a5b8ee8532b03e008d84603fb24293a53b52
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-12 10:10:58 -0700

    Removed env build file

--- helpers/withApollo.ts
+++ helpers/withApollo.ts
@@ -1,9 +1,6 @@
 import withApollo from 'next-with-apollo'
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
 
-/* This URL is only used for development.
-   This needs to be an environmental variable for production.
-*/
 const SERVER_URL = process.env.SERVER_URL
 
 export default withApollo(
