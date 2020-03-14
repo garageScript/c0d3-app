c8f3416669f3c7c1f9f0449c223bb52a1361c819
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-13 20:42:14 -0700

    Improved Alert coverage

--- stories/components/Alert.stories.tsx
+++ stories/components/Alert.stories.tsx
@@ -9,3 +9,21 @@ export default {
 export const Basic: React.FC = () => (
   <Alert text="Set up your computer to submit challenges." />
 )
+
+export const withIcon: React.FC = () => (
+  <Alert
+    icon="/curriculumAssets/icons/icon-tip.svg"
+    text="Set up your computer to submit challenges."
+  />
+)
+
+export const withInstructions: React.FC = () => (
+  <Alert
+    text="Set up your computer to submit challenges."
+    instructionsUrl="https://www.notion.so/JS-0-Foundations-a43ca620e54945b2b620bcda5f3cf672#b45ed85a95e24c9d9fb784afb7a46bcc"
+  />
+)
+
+export const withClasses: React.FC = () => (
+  <Alert error text="Set up your computer to submit challenges." />
+)
