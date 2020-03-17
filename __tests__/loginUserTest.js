28f09e1796129cc8db20050f9e22e9eff5771ed9
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-17 00:58:27 -0700

    Test coverage for loginUser updated to better tests

--- __tests__/loginUserTest.js
+++ __tests__/loginUserTest.js
@@ -1,19 +1,45 @@
 import { loginUser } from '../helpers/loginUser'
-describe('loginUser helper', () => {
-  test('Should return object', async () => {
-    window.fetch = jest.fn().mockReturnValue(
-      Promise.resolve({
-        redirected: false
-      })
-    )
-    expect(Promise.resolve(loginUser('user123', 'pass123'))).resolves.toBe({})
-  })
-  test('Should return null', async () => {
+
+describe('loginUser function', () => {
+  test('should call fetch with the correct data', async () => {
     window.fetch = jest.fn().mockReturnValue(
       Promise.resolve({
         redirected: true
       })
     )
-    expect(Promise.resolve(loginUser('user123', 'pass123'))).resolves.toBe(null)
+    await loginUser('hello', 'hpass')
+    expect(fetch).toBeCalledWith('undefined/signin', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: 'hello',
+        password: 'hpass'
+      })
+    })
+  })
+
+  test('should return null', async () => {
+    window.fetch = jest.fn().mockReturnValue(
+      Promise.resolve({
+        redirected: true
+      })
+    )
+    const result = await loginUser('hello', 'hpass')
+    expect(result).toEqual(null)
+  })
+
+  test('should return null', async () => {
+    window.fetch = jest.fn().mockReturnValue(
+      Promise.resolve({
+        redirected: false,
+        json: () => {
+          return 'sampleResult'
+        }
+      })
+    )
+    const result = await loginUser('hello', 'hpass')
+    expect(result).toEqual('sampleResult')
   })
 })
