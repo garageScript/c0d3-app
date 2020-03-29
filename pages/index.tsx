e34b59d3d57ae78fceb03fc800a510e836fbadd9
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-29 12:03:40 -0700

    Close #74 - render curriculum if logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react'
+import Curriculum from './curriculum'
 import AppNav from '../components/AppNav'
 import LandingPage from '../components/LandingPage'
 import Footer from '../components/Footer'
+import useSWR from 'swr'
 
-const IndexPage: React.FC = () => {
+const fetcher = (url: string) =>
+  fetch(url, { credentials: 'include' }).then(r => r.json())
+
+const IndexPage: any = () => {
+  const { data, error } = useSWR('https://c0d3-back.a0d3.com/session', fetcher)
+  if (data && data.userInfo) {
+    return <Curriculum />
+  }
+  // while loading, don't show anything to user
+  if (!data && !error) {
+    return ''
+  }
   return (
     <>
       <AppNav loggedIn={false} />
