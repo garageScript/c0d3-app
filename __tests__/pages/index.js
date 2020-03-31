6ad7e62d94f0c610d2ba1928da2ac0a76f051e4a
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-03-30 19:42:47 -0700

    Update #74 - test to render index page

--- __tests__/pages/index.js
+++ __tests__/pages/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { render } from '@testing-library/react'
 import ReactDOM from 'react-dom'
-import IndexPage from '../../pages'
+import Curriculum from '../../pages/curriculum'
+import useSWR from 'swr'
 
-test('should render without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<IndexPage />, div)
+describe('Index Page', () => {
+  test('Should return empty string while loading', async () => {
+    jest.mock('swr', () => {
+      return jest.fn().mockReturnValue({
+        data: {
+          userInfo: {name:'tester'}
+        }
+      })
+    })
+    jest.mock('../../pages/curriculum', () => {
+      return jest.fn().mockReturnValue( <h1>Hello Curriculum</h1> )
+    })
+    const IndexPage = require('../../pages').default
+    const { getByText } = render(<IndexPage />)
+    getByText('Hello Curriculum')
+  })
+
 })
