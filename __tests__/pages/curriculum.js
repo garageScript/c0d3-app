17ec97143ad2ccce1e33d5bd9de38ecd13ee3023
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-04-02 22:01:21 -0700

    Close #92 - curriculum testing

---
+++ __tests__/pages/curriculum.js
@@ -0,0 +1,104 @@
+jest.mock('@apollo/react-hooks')
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import Curriculum from '../../pages/curriculum'
+import { render } from '@testing-library/react'
+
+describe('Curriculum Page', () => {
+  test('Should render loading when loading', async () => {
+    useQuery.mockReturnValue({
+      loading: true
+    })
+
+    const { container } = render(<Curriculum />)
+    expect(container).toMatchSnapshot()
+  })
+  test('Should render ellpsis when no data is present', async () => {
+    useQuery.mockReturnValue({
+      loading: false
+    })
+
+    const { container } = render(<Curriculum />)
+    expect(container).toMatchSnapshot()
+  })
+  test('Should render data with lesson state completed', async () => {
+    useQuery.mockReturnValue({
+      data: {
+        curriculumStatus: [
+          {
+            id: 4,
+            title: 'lesson 4',
+            description: '4th lesson',
+            order: 4,
+            challenges: [{
+              id: 4
+            }],
+            currentUser: {
+              userLesson: {
+              }
+            },
+            docUrl: 'lesson4.com',
+          },
+          {
+            id: 1,
+            title: 'lesson 1',
+            description: '1st lesson',
+            order: 1,
+            challenges: [{
+              id: 1
+            }],
+            currentUser: {
+              userLesson: {
+              }
+            },
+            docUrl: 'lesson4.com',
+          }
+        ]
+      }
+    })
+
+    const { container } = render(<Curriculum />)
+    expect(container).toMatchSnapshot()
+  })
+  test('Should render data with lesson state inProgress', async () => {
+    useQuery.mockReturnValue({
+      data: {
+        curriculumStatus: [
+          {
+            id: 4,
+            title: 'lesson 4',
+            description: '4th lesson',
+            order: 4,
+            challenges: [{
+              id: 4
+            }],
+            currentUser: {
+              userLesson: {
+                isPassed: '234234'
+              }
+            },
+            docUrl: 'lesson4.com',
+          },
+          {
+            id: 1,
+            title: 'lesson 1',
+            description: '1st lesson',
+            order: 1,
+            challenges: [{
+              id: 1
+            }],
+            currentUser: {
+              userLesson: {
+                isPassed: '9878990'
+              }
+            },
+            docUrl: 'lesson4.com',
+          }
+        ]
+      }
+    })
+
+    const { container } = render(<Curriculum />)
+    expect(container).toMatchSnapshot()
+  })
+})
