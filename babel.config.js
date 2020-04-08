1ecb70394c67e40e841b43b1a13adbeea96db8c7
rkalra247 <rkalra247@gmail.com>
2020-04-07 19:07:20 -0700

    fix build

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,23 @@
+const inlineReactSvg = [
+  'inline-react-svg',
+  {
+    svgo: {
+      plugins: [{
+        removeAttrs: { attrs: '(data-name)' }
+      },
+      {
+        cleanupIDs: true
+      }
+      ]
+    }
+  }
+]
+
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-react',
     '@babel/preset-typescript'
   ],
-  plugins: ['inline-react-svg', '@babel/syntax-dynamic-import']
+  plugins: [ inlineReactSvg, '@babel/syntax-dynamic-import']
 }
