9e8a8113981037538dabeeada14ec76a6431e08e
rkalra247 <rkalra247@gmail.com>
2020-04-07 19:26:01 -0700

    added comments for folloq up

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 const inlineReactSvg = [
   // We are disabling svg optimization because it breaks tests.
-  // // Issue: https://github.com/garageScript/c0d3-app/issues/104
+  //Issue: https://github.com/garageScript/c0d3-app/issues/104
   'inline-react-svg',
   {
     svgo: {
