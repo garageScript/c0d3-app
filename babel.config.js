133711c42c6a6a81b044ef00936a6bc00cc59a07
rkalra247 <rkalra247@gmail.com>
2020-04-07 19:22:09 -0700

    added comments to follow up

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 const inlineReactSvg = [
+  // We are disabling svg optimization because it breaks tests.
+  // // Issue: https://github.com/garageScript/c0d3-app/issues/104
   'inline-react-svg',
   {
     svgo: {
       plugins: [{
         removeAttrs: { attrs: '(data-name)' }
-      },
-      {
+      },{
         cleanupIDs: true
-      }
-      ]
+      }]
     }
   }
 ]
