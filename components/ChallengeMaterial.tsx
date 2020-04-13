947c0bdd276f76173536bdc6c895cd1ffea96cf5
Michael Dinh <michaeldinh1994@gmail.com>
2020-04-12 23:48:49 -0700

    refactor to remove undefined type

--- components/ChallengeMaterial.tsx
+++ components/ChallengeMaterial.tsx
@@ -88,9 +88,12 @@ const ChallengeMaterial: React.FC<ChallengeMaterialProps> = props => {
   const sortedChallenges: Challenge[] = props.challenges.sort(
     (a, b) => a.order - b.order
   )
+  //TODO: once user has completed a challenge find the first incomplete challenge that
+  //hasn't been completed to set as default challengeID
   const [currentChallengeID, setCurrentChallenge] = useState<
     CurrentChallengeID
   >()
+  //assign currentChallenge to first challenge if no match found
   const currentChallenge =
     sortedChallenges.find(
       (challenge: Challenge) => challenge.id === currentChallengeID
