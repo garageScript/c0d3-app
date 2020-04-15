a2e3819ea8b212214e9231a53b78c6590a82288e
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-15 15:47:55 -0700

    add SessionContext inside _app to be used anywhere a components need session data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,30 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import withApollo from '../helpers/withApollo'
 import { ApolloClient, NormalizedCacheObject } from 'apollo-boost'
 import { AppProps } from 'next/app'
+import useSession, { SessionData } from '../helpers/useSession'
 import '../scss/index.scss'
+
+export const SessionContext = React.createContext<SessionData>({
+  data: null,
+  error: null
+})
 
 interface IProps extends AppProps {
   apollo: ApolloClient<NormalizedCacheObject>
 }
 
 function MyApp({ Component, pageProps, apollo }: IProps) {
+  const session = useSession()
+
+  if (!session.data && !session.error) {
+    return null
+  }
+
   return (
     <ApolloProvider client={apollo}>
-      <Component {...pageProps} />
+      <SessionContext.Provider value={session}>
+        <Component {...pageProps} />
+      </SessionContext.Provider>
     </ApolloProvider>
   )
 }
