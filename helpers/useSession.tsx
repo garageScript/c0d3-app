48300eb9a4d725eb1f5d3280a067d0c6b20fd1c4
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-15 15:47:55 -0700

    add SessionContext inside _app to be used anywhere a components need session data

--- helpers/useSession.tsx
+++ helpers/useSession.tsx
@@ -6,17 +6,18 @@ const SERVER_URL = process.env.SERVER_URL
 export const fetcher = (url: string) =>
   fetch(url, { credentials: 'include' }).then(r => r.json())
 
-type SessionData = {
+export type SessionData = {
   data: {
     userInfo: User
     errorMessage?: string
     success: boolean
-  }
-  error: boolean
+  } | null
+  error: boolean | null
 }
 
 const useSession = (): SessionData => {
   const { data, error } = useSWR(`${SERVER_URL}/session`, fetcher)
+
   return { data, error }
 }
 
