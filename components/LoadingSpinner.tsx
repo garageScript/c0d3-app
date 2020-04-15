ae217e1038bc2483a7236e130c656668a8e550e1
Michael Dinh <michaeldinh1994@gmail.com>
2020-04-15 16:59:36 -0700

    spinner style updates

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -7,7 +7,7 @@ const LoadingSpinner = () => {
       <div className="mt-4 card border-0 shadow-sm">
         <div className="card-body d-flex flex-column align-items-center">
           <h1>Loading</h1>
-          <div className="spinner-border text-primary" role="status">
+          <div className="spinner-border text-primary mt-1" role="status">
             <span className="sr-only">Loading...</span>
           </div>
         </div>
