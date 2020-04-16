936bce565138f928ac816b6fe2b8082b28494a71
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-16 15:17:00 -0700

    Add active state to default NavLink + storybook components for NavLink

--- __tests__/components/NavLink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import NavLink from '../../components/NavLink'
-
-describe('NavLink Component', () => {
-  test('Should crash if given internal && blank props', () => {
-    // expect(() => render(<NavLink internal blank />)).toThrow()
-  })
-})
