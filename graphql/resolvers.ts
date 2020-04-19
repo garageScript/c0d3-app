57f0c98aa0392bc196753d8600882b7a2a6c757a
hwong0305 <hwong.0305@gmail.com>
2020-04-19 05:37:38 +0000

    Removed Lesson changes to a different PR

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,13 +1,23 @@
-import { findLessons } from '../helpers/controllers/lessonController'
 import { login, logout } from '../helpers/controllers/authController'
-import { Request } from 'express'
+import db from '../helpers/dbload'
+
+const { Lesson, User } = db
 
 export default {
   Query: {
-    lessons: async (_parent: void, _arg: void, ctx: { req: Request }) => {
-      const userId = ctx.req.session ? ctx.req.session.userId : null
-      const lessons = await findLessons(userId)
-      return lessons
+    lessons() {
+      return Lesson.findAll({
+        include: [
+          'challenges',
+          {
+            model: User,
+            through: {
+              attributes: ['isPassed', 'isTeaching']
+            }
+          }
+        ],
+        order: [['order', 'ASC']]
+      })
     }
   },
 
