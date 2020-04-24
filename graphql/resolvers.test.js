9da7b541b5b7a9e756415c7630d0fbacf3d76252
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-24 07:04:16 -0700

    added tests for session resolver

--- graphql/resolvers.test.js
+++ graphql/resolvers.test.js
@@ -1,10 +1,49 @@
 import resolvers from '../graphql/resolvers'
 import db from '../helpers/dbload'
 
-describe('resolvers', () => {
+describe('GraphQL resolvers', () => {
   const { Lesson } = db
   test('lessons should return an empty array', async () => {
     Lesson.findAll = jest.fn().mockReturnValue([])
     expect(resolvers.Query.lessons()).toEqual([])
+  })
+
+  test('session should return null if no userId', async () => {
+    const req = { session: null }
+
+    return expect(await resolvers.Query.session({}, {}, { req })).toEqual(null)
+  })
+
+  test('session should return user null if no user found', async () => {
+    const { User } = db
+    const req = { session: { userId: 2 } }
+    User.findOne = jest.fn().mockReturnValue(null)
+
+    return expect(await resolvers.Query.session({}, {}, { req })).toEqual(null)
+  })
+
+  test('session should return user including submissions and lessonStatus', async () => {
+    const { User } = db
+    const req = { session: { userId: 2 } }
+
+    const userResult = {
+      user: { username: 'test' },
+      Submissions: [{ id: '1' }],
+      UserLessons: [{ id: '1' }]
+    }
+
+    User.findOne = jest.fn().mockReturnValue(userResult)
+
+    const expectedReturnValue = {
+      user: { username: 'test' },
+      submissions: [{ id: '1' }],
+      lessonStatus: [{ id: '1' }]
+    }
+
+    const returnValue = await resolvers.Query.session({}, {}, { req })
+
+    expect(returnValue.user).toEqual(userResult)
+    expect(returnValue.submissions).toEqual(expectedReturnValue.submissions)
+    expect(returnValue.lessonStatus).toEqual(expectedReturnValue.lessonStatus)
   })
 })
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,5 +1,6 @@
-import { login, logout, signup } from '../helpers/controllers/authController'
+import { Request } from 'express'
 import db from '../helpers/dbload'
+import { login, logout, signup } from '../helpers/controllers/authController'
 import _ from 'lodash'
 
 const { User, Submission, UserLesson, Lesson } = db
@@ -12,26 +13,29 @@ export default {
         order: [['order', 'ASC']]
       })
     },
-    async session(_parent: any, _args: any, context: any) {
+    async session(_parent: void, _args: void, context: { req: Request }) {
       const userId = _.get(context, 'req.session.userId', false)
 
       if (!userId) {
         return null
       }
 
-      const user = await User.findOne({
-        where: { id: userId },
-        include: [ Submission, UserLesson ]
-      })
+      // https://github.com/garageScript/c0d3-app/wiki/Sequelize-Query-Performance
+
+      const [user, submissions, lessonStatus] = await Promise.all([
+        User.findOne({ where: { id: userId } }),
+        Submission.findAll({ where: { userId } }),
+        UserLesson.findAll({ where: { userId } })
+      ])
 
       if (!user) {
         return null
       }
 
-      return { 
+      return {
         user,
-        submissions: user.Submissions,
-        lessonStatus: user.UserLessons
+        submissions,
+        lessonStatus
       }
     }
   },
