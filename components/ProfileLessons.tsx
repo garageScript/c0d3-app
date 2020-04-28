3e8e5edcdf8b726b834ac94761e5299481777c17
rkalra247 <rkalra247@gmail.com>
2020-04-28 12:10:15 -0700

    update #106 add snapshots of tests for profile lessons

--- components/ProfileLessons.tsx
+++ components/ProfileLessons.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../scss/profileLessons.scss'
 
 type Lesson = {
   order: number
@@ -9,32 +10,21 @@ type LessonsProp = {
   lessons: Lesson[]
 }
 
-
-const LessonImage: React.FC<Lesson> = ({ progress=0, order }) => {
-  const lessonProgress = progress + '%' 
-  const color = lessonProgress === '100%'? 'success' : 'primary'
+const LessonImage: React.FC<Lesson> = ({ progress = 0, order }) => {
+  const lessonProgress = progress + '%'
+  const color = lessonProgress === '100%' ? 'success' : 'primary'
   const opacity = lessonProgress === '0%' ? 0.5 : 1
- 
+
   return (
-    <div style={{ position: 'relative', padding: '8px', opacity: opacity, display: 'inline-block' }}>
-      
-  {progress && (
-    <p
-      style={{
-        position: 'absolute',
-        top: '15px',
-        right: '15px',
-        border: 'solid white 2px',
-        opacity: opacity 
-      }}
-      className={`badge badge-pill badge-${color}`}
-    >
-      {lessonProgress}
-    </p>
-  )}
-      <img
-        src={`/curriculumAssets/lessonCoversSvg/js-${order}-cover.svg`}
-      />
+    <div className="lessonImage_container" style={{ opacity: opacity }}>
+      {progress && (
+        <p
+          className={`lessonImage_progressBadge badge badge-pill badge-${color}`}
+        >
+          {lessonProgress}
+        </p>
+      )}
+      <img src={`/curriculumAssets/lessonCoversSvg/js-${order}-cover.svg`} />
     </div>
   )
 }
@@ -42,15 +32,16 @@ const LessonImage: React.FC<Lesson> = ({ progress=0, order }) => {
 const ProfileLessons: React.FC<LessonsProp> = ({ lessons }) => {
   const displayLessons = lessons.map((lesson, i) => {
     return (
-      <LessonImage key={i} order={lesson.order} progress={lesson.progress} /> 
+      <LessonImage key={i} order={lesson.order} progress={lesson.progress} />
     )
   })
 
   return (
-    <div className='card shadow-sm'>
-     <div className='card-body'> 
-      {displayLessons}
-     </div>
+    <div className="card shadow-sm">
+      <div className="card-body">
+        <h3 className="profileLessons_title">Lessons</h3>
+        {displayLessons}
+      </div>
     </div>
   )
 }
