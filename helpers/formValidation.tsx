e8669b78b2df3dbebe75fdea6b8d1b4f8d339606
Colton Ehrman <coltonje95@gmail.com>
2020-04-29 03:39:29 -0700

    remove TEXT_MAX from login and signup form validation

--- helpers/formValidation.tsx
+++ helpers/formValidation.tsx
@@ -1,7 +1,6 @@
 import * as Yup from 'yup'
 
 const TEXT_MIN = 2
-const TEXT_MAX = 20
 const PASSWORD_MIN = 6
 const REGEX_ALPHANUMERICS_AND_SPACE = /^[a-zA-Z0-9_\s]*$/
 
@@ -14,25 +13,21 @@ const signupValidation = Yup.object({
     .lowercase('Must be a lowercase string')
     .matches(REGEX_ALPHANUMERICS_AND_SPACE, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
-    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .trim('Leading and trailing value must be alphanumeric character')
     .required('Required'),
   password: Yup.string()
     .min(PASSWORD_MIN, `Must be at least ${PASSWORD_MIN} characters`)
-    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .required('Required'),
   firstName: Yup.string()
     .strict(true)
     .matches(REGEX_ALPHANUMERICS_AND_SPACE, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
-    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .trim('Leading and trailing value must be alphanumeric character')
     .required('Required'),
   lastName: Yup.string()
     .strict(true)
     .matches(REGEX_ALPHANUMERICS_AND_SPACE, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
-    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .trim('Leading and trailing value must be alphanumeric character')
     .required('Required')
 })
@@ -43,12 +38,10 @@ const loginValidation = Yup.object({
     .lowercase('Must be a lowercase string')
     .matches(REGEX_ALPHANUMERICS_AND_SPACE, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
-    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .trim('Leading and trailing value must be alphanumeric character')
     .required('Required'),
   password: Yup.string()
     .min(PASSWORD_MIN, `Must be at least ${PASSWORD_MIN} characters`)
-    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .required('Required')
 })
 
