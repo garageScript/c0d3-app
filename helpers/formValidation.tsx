c79d15da3aa5a4a6745a5a96cc1cf8fb67c25626
Colton Ehrman <coltonje95@gmail.com>
2020-04-29 17:55:48 -0700

    add back TEXT_MAX for signup validation

--- helpers/formValidation.tsx
+++ helpers/formValidation.tsx
@@ -1,6 +1,7 @@
 import * as Yup from 'yup'
 
 const TEXT_MIN = 2
+const TEXT_MAX = 64
 const PASSWORD_MIN = 6
 const REGEX_ALPHANUMERICS_AND_SPACE = /^[a-zA-Z0-9_\s]*$/
 
@@ -13,21 +14,25 @@ const signupValidation = Yup.object({
     .lowercase('Must be a lowercase string')
     .matches(REGEX_ALPHANUMERICS_AND_SPACE, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
+    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .trim('Leading and trailing value must be alphanumeric character')
     .required('Required'),
   password: Yup.string()
     .min(PASSWORD_MIN, `Must be at least ${PASSWORD_MIN} characters`)
+    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .required('Required'),
   firstName: Yup.string()
     .strict(true)
     .matches(REGEX_ALPHANUMERICS_AND_SPACE, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
+    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .trim('Leading and trailing value must be alphanumeric character')
     .required('Required'),
   lastName: Yup.string()
     .strict(true)
     .matches(REGEX_ALPHANUMERICS_AND_SPACE, 'Must be alphanumerics characters')
     .min(TEXT_MIN, `Must be at least ${TEXT_MIN} characters`)
+    .max(TEXT_MAX, `Must be ${TEXT_MAX} characters or less`)
     .trim('Leading and trailing value must be alphanumeric character')
     .required('Required')
 })
