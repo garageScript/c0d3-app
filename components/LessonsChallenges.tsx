8043cae90ca08ae168a9e5085723247ee87dd25e
rkalra247 <rkalra247@gmail.com>
2020-05-02 03:10:48 -0700

    updates #106 display lessons and lessons titles

--- components/LessonsChallenges.tsx
+++ components/LessonsChallenges.tsx
@@ -2,29 +2,77 @@ import React from 'react'
 import _ from 'lodash'
 import '../scss/lessonsChallenges.scss'
 
-const LessonsChallenges: React.FC = props => {
-  console.log('PROPS:', props.lessons)
+type ChallengeStatusProps = {
+  challengesData: Challenge[]
+}
+
+type LessonChallengeProps = {
+  lessons: LessonChallenge[]
+}
+
+type LessonChallenge = {
+  order: number
+  title: string
+  totalChallenges: number
+  correctAnswers: number
+  challenges: Challenge[]
+}
+
+type Challenge = {
+  challengeNumber: number
+  challengeStatus: string
+}
+
+export const ChallengeStatus: React.FC<ChallengeStatusProps> = props => {
+  console.log('PROPS:', props)
+  const displayChallengeStatus = props.challengesData.map(eachChallenge => {
+    console.log('EACH CHALLENGE', eachChallenge)
+    let challengeStatus = 'default_challenge_status'
+    if (eachChallenge.challengeStatus === 'passed') {
+      challengeStatus = 'passed_challenge_status'
+    }
+    if (eachChallenge.challengeStatus === 'wrong answer') {
+      challengeStatus = 'unapproved_challenge_status'
+    }
+    if (eachChallenge.challengeStatus === 'pending') {
+      challengeStatus = 'pending_challenge_status'
+    }
+    return (
+      <span
+        key={_.uniqueId()}
+        className={`challenge_status ${challengeStatus}`}
+      ></span>
+    )
+  })
+  return <div className="challenges_container">{displayChallengeStatus}</div>
+}
+
+const LessonsChallenges: React.FC<LessonChallengeProps> = props => {
   const displayLessons = props.lessons.map(lesson => {
     return (
-      <div className="lessonimage_container" style={{ display: 'flex' }}>
+      <div key={_.uniqueId()} className="lesson_challenges">
         <img
           src={`/curriculumAssets/lessonCoversSvg/js-${lesson.order}-cover.svg`}
         />
-        <div className="titles_container">
-          <h6 style={{ marginLeft: '50px', marginTop: '15px', color: 'grey' }}>
-            {lesson.title}
+        <div className="lesson_title_container">
+          <h6 className="lesson_titles">{lesson.title}</h6>
+          <h6 className="challenges_stats">
+            {`${lesson.correctAnswers} of ${lesson.challenges.length}  Challenges completed`}
           </h6>
-          <h6 style={{ marginLeft: '50px', marginTop: '15px' }}>
-            {'15 of 15 Challenges completed'}
-          </h6>
+          <ChallengeStatus
+            className="lesson_challenges_container"
+            challengesData={lesson.challenges}
+          />
         </div>
       </div>
     )
   })
   return (
-    <div className="card">
-      <h3>Challenges</h3>
-      <div>{displayLessons}</div>
+    <div className="card shadow-sm">
+      <div className="card-body lessons_challenges_card_body">
+        <h3 className="main_title">Challenges</h3>
+        <div className="display_lessons">{displayLessons}</div>
+      </div>
     </div>
   )
 }
--- stories/profile/LessonsChallenges.stories.tsx
+++ stories/profile/LessonsChallenges.stories.tsx
@@ -1,24 +1,173 @@
 import * as React from 'react'
-import LessonsChallenges from '../../components/LessonsChallenges'
+import LessonsChallenges, {
+  ChallengeStatus
+} from '../../components/LessonsChallenges'
 
 export default {
   component: LessonsChallenges,
   title: 'Components/LessonChallenges'
 }
 
-const lessons = [
-  { order: 0, title: 'Foundation of Javascript' },
-  { order: 1, title: 'Variables & Functions' },
-  { order: 2, title: 'Arrays' },
-  { order: 3, title: 'Objects' },
-  { order: 4, title: 'Front End Engineering' },
-  { order: 5, title: 'End to End' },
-  { order: 6, title: 'React, GraphQL, SocketIO' },
-  { order: 7, title: 'Javscript Algorithms' },
-  { order: 8, title: 'Trees' },
-  { order: 9, title: 'General Algorithms' }
+const completedChallenges = [
+  { challengeNumber: 1, challengeStatus: 'passed' },
+  { challengeNumber: 2, challengeStatus: 'passed' },
+  { challengeNumber: 3, challengeStatus: 'passed' },
+  { challengeNumber: 4, challengeStatus: 'passed' },
+  { challengeNumber: 5, challengeStatus: 'passed' },
+  { challengeNumber: 6, challengeStatus: 'passed' },
+  { challengeNumber: 7, challengeStatus: 'passed' },
+  { challengeNumber: 8, challengeStatus: 'passed' },
+  { challengeNumber: 9, challengeStatus: 'passed' },
+  { challengeNumber: 10, challengeStatus: 'passed' }
 ]
 
-export const _LessonsChallenges: React.FC = () => (
+const defaultChallenges = [
+  { challengeNumber: 1, challengeStatus: 'default' },
+  { challengeNumber: 2, challengeStatus: 'default' },
+  { challengeNumber: 3, challengeStatus: 'default' },
+  { challengeNumber: 4, challengeStatus: 'default' },
+  { challengeNumber: 5, challengeStatus: 'default' },
+  { challengeNumber: 6, challengeStatus: 'default' },
+  { challengeNumber: 7, challengeStatus: 'default' },
+  { challengeNumber: 8, challengeStatus: 'default' },
+  { challengeNumber: 9, challengeStatus: 'default' },
+  { challengeNumber: 10, challengeStatus: 'default' },
+  { challengeNumber: 11, challengeStatus: 'default' },
+  { challengeNumber: 12, challengeStatus: 'default' }
+]
+
+const pendingChallenges = [
+  { challengeNumber: 1, challengeStatus: 'pending' },
+  { challengeNumber: 2, challengeStatus: 'pending' },
+  { challengeNumber: 3, challengeStatus: 'pending' },
+  { challengeNumber: 4, challengeStatus: 'pending' },
+  { challengeNumber: 5, challengeStatus: 'pending' },
+  { challengeNumber: 6, challengeStatus: 'pending' },
+  { challengeNumber: 7, challengeStatus: 'pending' },
+  { challengeNumber: 8, challengeStatus: 'pending' },
+  { challengeNumber: 9, challengeStatus: 'pending' },
+  { challengeNumber: 10, challengeStatus: 'pending' },
+  { challengeNumber: 11, challengeStatus: 'pending' }
+]
+
+const unapprovedChallenges = [
+  { challengeNumber: 1, challengeStatus: 'wrong answer' },
+  { challengeNumber: 2, challengeStatus: 'wrong answer' },
+  { challengeNumber: 3, challengeStatus: 'wrong answer' },
+  { challengeNumber: 4, challengeStatus: 'wrong answer' },
+  { challengeNumber: 5, challengeStatus: 'wrong answer' },
+  { challengeNumber: 6, challengeStatus: 'wrong answer' },
+  { challengeNumber: 7, challengeStatus: 'wrong answer' }
+]
+
+const challengesLesson6 = [
+  { challengeNumber: 1, challengeStatus: 'default' },
+  { challengeNumber: 2, challengeStatus: 'default' },
+  { challengeNumber: 3, challengeStatus: 'default' },
+  { challengeNumber: 4, challengeStatus: 'default' },
+  { challengeNumber: 5, challengeStatus: 'default' },
+  { challengeNumber: 6, challengeStatus: 'default' },
+  { challengeNumber: 7, challengeStatus: 'default' },
+  { challengeNumber: 8, challengeStatus: 'default' }
+]
+
+const challengesLesson5 = [
+  { challengeNumber: 1, challengeStatus: 'default' },
+  { challengeNumber: 2, challengeStatus: 'default' },
+  { challengeNumber: 3, challengeStatus: 'default' },
+  { challengeNumber: 4, challengeStatus: 'default' },
+  { challengeNumber: 5, challengeStatus: 'default' },
+  { challengeNumber: 6, challengeStatus: 'default' },
+  { challengeNumber: 7, challengeStatus: 'default' },
+  { challengeNumber: 8, challengeStatus: 'default' },
+  { challengeNumber: 9, challengeStatus: 'default' }
+]
+
+const challengesLesson9 = [
+  { challengeNumber: 1, challengeStatus: 'default' },
+  { challengeNumber: 2, challengeStatus: 'default' },
+  { challengeNumber: 3, challengeStatus: 'default' },
+  { challengeNumber: 4, challengeStatus: 'default' },
+  { challengeNumber: 5, challengeStatus: 'default' }
+]
+
+const lessons = [
+  {
+    correctAnswers: 10,
+    totalChallenges: 10,
+    order: 0,
+    title: 'Foundation of Javascript',
+    challenges: completedChallenges
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 12,
+    order: 1,
+    title: 'Variables & Functions',
+    challenges: defaultChallenges
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 11,
+    order: 2,
+    title: 'Arrays',
+    challenges: pendingChallenges
+  },
+  {
+    correctAnswers: 10,
+    totalChallenges: 10,
+    order: 3,
+    title: 'Objects',
+    challenges: completedChallenges
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 7,
+    order: 4,
+    title: 'Front End Engineering',
+    challenges: unapprovedChallenges
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 9,
+    order: 5,
+    title: 'End to End',
+    challenges: challengesLesson5
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 8,
+    order: 6,
+    title: 'React, GraphQL, SocketIO',
+    challenges: challengesLesson6
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 11,
+    order: 7,
+    title: 'Javscript Algorithms',
+    challenges: pendingChallenges
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 12,
+    order: 8,
+    title: 'Trees',
+    challenges: defaultChallenges
+  },
+  {
+    correctAnswers: 0,
+    totalChallenges: 5,
+    order: 9,
+    title: 'General Algorithms',
+    challenges: challengesLesson9
+  }
+]
+
+export const LessonsChallengesCard: React.FC = () => (
   <LessonsChallenges lessons={lessons} />
+)
+
+export const ChallengeStatusCard: React.FC = () => (
+  <ChallengeStatus challengesData={defaultChallenges} />
 )
