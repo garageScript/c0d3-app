6c51b28f8523a9dd68e8d0476ce6f25ae3bdcb88
rkalra247 <rkalra247@gmail.com>
2020-05-04 20:16:09 -0700

    update #161 add profileBravos component

---
+++ components/ProfileBravos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProfileBravos: React.FC = () => {
+
+  return (
+  <div className='card shadow-sm'>
+    <h3>Bravos</h3>
+    </div>
+  )
+}
+
+export default ProfileBravos
