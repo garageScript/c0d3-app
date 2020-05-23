fe3be00e400ed09cfc9a48a64cfa3fc6d9e5f50e
David De Wulf <ledavid64@gmail.com>
2020-05-23 01:13:09 -0700

    Update #153: Setup CI for CLI

--- cli/src/cli.test.js
+++ cli/src/cli.test.js
@@ -21,11 +21,6 @@ createCommand.mockReturnValue(program)
 const { init } = require('./cli')
 
 describe('CLI c0d3', () => {
-  test('Should display help', async () => {
-    let { stdout } = await cli('c0d3')
-    expect(stdout).toMatchSnapshot()
-  })
-
   test('Should run c0d3', () => {
     expect(createCommand).toHaveBeenCalled()
   })
@@ -42,16 +37,3 @@ describe('CLI c0d3', () => {
     expect(program.help).not.toHaveBeenCalled()
   })
 })
-
-const cli = (args = '', cwd = '.') => {
-  return new Promise(resolve => {
-    exec(args, { cwd }, (error, stdout, stderr) => {
-      resolve({
-        code: error && error.code ? error.code : 0,
-        error,
-        stdout,
-        stderr
-      })
-    })
-  })
-}
