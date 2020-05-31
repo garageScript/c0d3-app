92d61775cfd06792550d1fded89d631d19cfd3a1
Alexis Ortiz Ojeda <aortizoj15@gmail.com>
2020-05-31 03:05:56 -0700

    Closes #226 - Implementation of Contributors Page

---
+++ pages/contributors.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import _ from 'lodash'
+import Layout from '../components/Layout'
+import ContributorCard from '../components/ContributorCard'
+
+export const Contributors = () => {
+  const contributors = [
+    {
+      firstName: 'Alexis',
+      lastName: 'Ortiz Ojeda',
+      username: 'aortizoj',
+      linkedinUrl: 'https://www.linkedin.com/in/alexis-ortiz-ojeda/',
+      githubPRsUrl:
+        'https://github.com/garageScript/c0d3-app/pulls?q=is%3Apr+author%3Aaortizoj15',
+      codeSample: 'https://github.com/garageScript/c0d3-app/pull/67',
+      resume:
+        'https://drive.google.com/file/d/1HoMg9QHGH2qicG8bZ8hRWv7FT7GXjFB5/view'
+    }
+  ]
+  const userData = contributors.map(user => {
+    return (
+      <div key={_.uniqueId()} className="col-4">
+        <ContributorCard user={user} />
+      </div>
+    )
+  })
+  return (
+    <Layout>
+      <div className="row mt-4">{userData}</div>
+    </Layout>
+  )
+}
+
+export default Contributors
