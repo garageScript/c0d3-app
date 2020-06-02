0de21518b4f4594f582b369623713d107e06557c
Michael Dinh <michaeldinh1994@gmail.com>
2020-06-01 17:50:17 -0700

    posthog updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+//TODO type the posthog library
+// @ts-ignore
+import posthog from 'posthog-js'
 import { ApolloProvider } from '@apollo/react-hooks'
 import withApollo from '../helpers/withApollo'
 import { ApolloClient, NormalizedCacheObject } from 'apollo-boost'
@@ -14,13 +17,17 @@ interface IProps extends AppProps {
 
 function MyApp({ Component, pageProps, apollo }: IProps) {
   const session = useSession()
+  useEffect(() => {
+    posthog.init('Ofv0LFRVyklwR7a_fjKKbgemFuKsT9nOhG277svLZKM', {
+      api_host: 'https://app.posthog.com'
+    })
+  }, [])
   return (
     <ApolloProvider client={apollo}>
       <SessionContext.Provider value={session}>
         <Head>
           <title>C0D3.com</title>
           <link rel="shortcut icon" href="/favicon.ico" />
-          <script type="text/javascript" src="/posthog.js" />
         </Head>
         <Component {...pageProps} />
       </SessionContext.Provider>
