0d0c76fbbb92d892041c3e6f19b2530f1064d3c8
Rahul Kalra <noob101@Rahuls-MacBook-Air.local>
2020-06-02 18:38:49 -0700

    update #234 add profile lessons UI to profile page

--- pages/userprofile.tsx
+++ pages/userprofile.tsx
@@ -1,41 +1,72 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import ProfileImageInfo from '../components/ProfileImageInfo'
-//import ProfileLessons from '../components/ProfileLessons'
-//import { User } from '../@types/user'
-import withQueryLoader, { WithQueryProps } from '../containers/withQueryLoader'
-//import _ from 'lodash'
+import { Lesson } from '../@types/lesson'
+import ProfileLessons from '../components/ProfileLessons'
+import ProfileSubmissions from '../components/ProfileSubmissions'
+import withQueryLoader from '../containers/withQueryLoader'
 import { GET_APP } from '../graphql/queries'
 
-const UserProfile: React.FC<WithQueryProps> = ( { queryData } ) => {
-    console.log('QueryData:', queryData)
-    const userInfo = {
-        username: queryData.session.user.username,
-        firstName : queryData.session.user.name.split(' ')[0],
-        lastName : queryData.session.user.name.split(' ')[1]
+type SessionUser = {
+    username: string
+    name: string
+}
+
+type AppQuery = {
+    queryData: AppInfo
+}
+
+type AppInfo = {
+    session: Session,
+    lessons: Lesson[]
+}
+
+type Session = {
+    user: SessionUser
+    submissions: Submission[] 
+}
+
+type Submission = {
+   diff: string
+   status: string
+   lessonId: number 
+}
+
+const UserProfile: React.FC<AppQuery> = ({ queryData }) => {
+  console.log('QueryData:', queryData)
+  const userInfo = {
+    username: queryData.session.user.username,
+    firstName: queryData.session.user.name.split(' ')[0],
+    lastName: queryData.session.user.name.split(' ')[1]
+  }
+
+  const lessonInfo = queryData.lessons.map(lesson => {
+    const lessonProgress =
+      queryData.session.submissions.filter(
+        r => r.status === 'approved' && r.lessonId === lesson.id
+      ).length / lesson.challenges.length
+    const progress = Math.floor(lessonProgress * 100)
+    const order = lesson.order
+    return {
+      progress,
+      order
     }
-    console.log("userInfo:", userInfo)
+  })
 
-    const lessonInfo = queryData.lessons.map((lesson) => {
-        const lessonProgress = queryData.submissions.filter( (r) => r.status === 'approved' && r.lessonId === lesson.id ).length / lesson.challenges.length
-        const progress = Math.floor(lessonProgress*100)
-        const order = lesson.order
-        return {
-            progress, order 
-        }
-    })
-
-    return (
-   <Layout>
-       <ProfileImageInfo user={userInfo}/>
-       <ProfileLessons lessons={lessonInfo}/>
-   </Layout>
-    )
+  return (
+    <Layout>
+      <>
+      <ProfileImageInfo user={userInfo} />
+      <ProfileLessons lessons={lessonInfo} />
+      <ProfileSubmissions lessons={queryData.lessons} />
+      </>
+    </Layout>
+  )
 }
 
 export default withQueryLoader(
-    {
-        query: GET_APP
-    },
-    UserProfile
+  {
+    query: GET_APP
+  },
+  UserProfile
 )
