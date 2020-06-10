4cd29f80237994879e13a720984bdc4034ea061b
Colton Ehrman <coltonje95@gmail.com>
2020-06-10 10:06:22 -0500

    update user lesson when updating submissions

--- helpers/updateSubmission.test.js
+++ helpers/updateSubmission.test.js
@@ -2,7 +2,7 @@ jest.mock('./dbload')
 import db from './dbload'
 import { updateSubmission } from './updateSubmission'
 
-const { Submission } = db
+const { Challenge, Submission, UserLesson } = db
 
 describe('updateSubmission', () => {
   test('should return submission', async () => {
@@ -16,12 +16,53 @@ describe('updateSubmission', () => {
       set: setStub,
       save: saveStub
     }
+    const submissions = [
+      { status: 'passed' },
+      { status: 'passed' },
+      { status: 'passed' },
+      { status: 'not passed' }
+    ]
+    Challenge.count = jest.fn().mockReturnValue(submissions.length)
     Submission.findByPk = jest.fn().mockReturnValue(submission)
+    Submission.findAll = jest.fn().mockReturnValue(submissions)
     const result = await updateSubmission(submission)
 
     expect(setStub).toHaveBeenCalledWith('reviewerId', 2)
     expect(setStub).toHaveBeenCalledWith('status', 'fake status')
     expect(setStub).toHaveBeenCalledWith('comment', 'fake comment')
+    expect(saveStub).toHaveBeenCalled()
+    expect(result).toEqual(submission)
+  })
+
+  test('should update user lesson', async () => {
+    const submission = {
+      id: 1,
+      comment: 'fake comment',
+      status: 'fake status',
+      reviewerId: 2,
+      set: () => {},
+      save: () => {}
+    }
+    const submissions = [
+      { status: 'passed' },
+      { status: 'passed' },
+      { status: 'passed' },
+      { status: 'passed' }
+    ]
+    const setStub = jest.fn()
+    const saveStub = jest.fn()
+    const lesson = {
+      set: setStub,
+      save: saveStub
+    }
+    Challenge.count = jest.fn().mockReturnValue(submissions.length)
+    Submission.findByPk = jest.fn().mockReturnValue(submission)
+    Submission.findAll = jest.fn().mockReturnValue(submissions)
+    UserLesson.findOne = jest.fn().mockReturnValue(lesson)
+    const result = await updateSubmission(submission)
+
+    expect(setStub).toHaveBeenCalledWith('isPassed', true)
+    expect(setStub).toHaveBeenCalledWith('isTeaching', true)
     expect(saveStub).toHaveBeenCalled()
     expect(result).toEqual(submission)
   })
--- helpers/updateSubmission.ts
+++ helpers/updateSubmission.ts
@@ -1,6 +1,6 @@
 import db from './dbload'
 
-const { Submission } = db
+const { Submission, Challenge, UserLesson } = db
 
 export type ArgsUpdateSubmission = {
   id: number
@@ -14,11 +14,35 @@ export const updateSubmission = async (
     if (!args) throw new Error('Invalid args')
     const { id, comment, status, reviewerId } = args
     const submission = await Submission.findByPk(id)
+    const challengeCount = await Challenge.count({
+      where: { lessonId: submission.lessonId }
+    })
 
     submission.set('reviewerId', reviewerId)
     submission.set('status', status)
     submission.set('comment', comment)
     await submission.save()
+
+    const submissions = await Submission.findAll({
+      where: {
+        lessonId: submission.lessonId,
+        userId: submission.userId
+      }
+    })
+
+    const passedSubmissions = submissions.reduce(
+      (sum: number, s: any) => sum + (s.status === 'passed' ? 1 : 0),
+      0
+    )
+
+    if (challengeCount === passedSubmissions) {
+      const lesson = await UserLesson.findOne({
+        where: { lessonId: submission.lessonId }
+      })
+      lesson.set('isPassed', true)
+      lesson.set('isTeaching', true)
+      await lesson.save()
+    }
 
     return submission
   } catch (error) {
