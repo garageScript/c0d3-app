2f11fac46ec3f4f6136a5f2e760d7d1f7e12169a
Rahul Kalra <noob101@Rahuls-MacBook-Air.local>
2020-06-22 12:14:00 -0700

    update #243 refactor userInfoController tests

--- helpers/controllers/userInfoController.test.js
+++ helpers/controllers/userInfoController.test.js
@@ -1,18 +1,17 @@
 jest.mock('../dbload')
 import db from '../dbload'
-import fetch from 'node-fetch'
-import resolvers from '../../graphql/resolvers'
+import { userInfo } from './userInfoController'
 
 const { User, UserLesson, Submission } = db
 describe('userInfo controller tests', () => {
   test('should return error of invalid username if no username is passed in', async () => {
-    expect(resolvers.Query.userInfo({}, {})).rejects.toEqual('Invalid username')
+    expect(userInfo({}, {})).rejects.toEqual('Invalid username')
   })
   test('should return error if User.findOne returns invalid user object', async () => {
     User.findOne = jest.fn()
-    expect(
-      resolvers.Query.userInfo({}, { username: 'testing2020' })
-    ).rejects.toEqual('Invalid user object')
+    expect(userInfo({}, { username: 'testing2020' })).rejects.toEqual(
+      'Invalid user object'
+    )
   })
   test('should return correct submissions and user lesson', async () => {
     User.findOne = jest.fn().mockReturnValue({
@@ -29,10 +28,7 @@ describe('userInfo controller tests', () => {
         diff: 'testing2020'
       }
     ])
-    const returnValue = await resolvers.Query.userInfo(
-      {},
-      { username: 'noob222' }
-    )
+    const returnValue = await userInfo({}, { username: 'noob222' })
     expect(returnValue).toEqual({
       user: {
         id: '10',
