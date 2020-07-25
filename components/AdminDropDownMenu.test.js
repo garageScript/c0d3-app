9ea6d6a2d1e280e5893ac0b23db15a4270120b0a
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-07-24 19:48:19 -0700

    closes #346 - creates admin dropdown menu, integrates into navbar

---
+++ components/AdminDropDownMenu.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { AdminDropDownMenu } from './AdminDropDownMenu'
+
+describe('Admin dropdown menu component', () => {
+  test('Should add `show` class when Admin div is clicked on', () => {
+    const { container, queryByTestId } = render(<AdminDropDownMenu />)
+    const adminButton = queryByTestId('adminButton')
+    const menu = queryByTestId('menu')
+
+    fireEvent.click(adminButton)
+
+    expect(menu.classList.contains('show')).toBe(true)
+    expect(container).toMatchSnapshot()
+  })
+
+  test('Should remove `show` class onMouseOut of menu', () => {
+    const { container, queryByTestId } = render(<AdminDropDownMenu />)
+    const menu = queryByTestId('menu')
+
+    fireEvent.mouseOut(menu)
+    expect(menu.classList.contains('show')).toBeFalsy
+    expect(container).toMatchSnapshot()
+  })
+})
