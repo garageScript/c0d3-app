33639f97aa3af5308af3e686e03f39386f073c87
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-07-25 11:52:54 -0700

    closes #348 - update story for inputcard to include no background option

--- stories/components/InputCard.stories.tsx
+++ stories/components/InputCard.stories.tsx
@@ -13,9 +13,18 @@ const mockBtn = [
   }
 ]
 
-const mockValues = [{ title: 'super', value: 'saiyan' }]
+const mockValues = [
+  { title: 'Nickname', value: 'ChickenFarmer00' },
+  { title: 'Description', value: 'Amazing farm of chicken' },
+  { title: 'Title', value: 'oheuhehe' },
+  { title: 'Level', value: 'noob' }
+]
 
 export const Basic: React.FC = () => <InputCard values={mockValues} />
+
+export const _NoColorBackground: React.FC = () => (
+  <InputCard values={mockValues} bgColor="none" />
+)
 
 export const _WithTitle: React.FC = () => (
   <InputCard values={mockValues} title="clownfish" />
