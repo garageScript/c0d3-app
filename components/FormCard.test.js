f4d6a332f4993fd510ce8f5a5afccd42178055a1
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-07-30 21:43:53 -0700

    closes # 348 - Update FormCard to handle errors and new input values outside of the component

--- components/FormCard.test.js
+++ components/FormCard.test.js
@@ -6,93 +6,82 @@ let testBtnOnClick = ''
 
 const mockBtn = {
   title: 'Create New Challenge',
-  onClick: ok => (testBtnOnClick = ok)
+  onClick: () => (testBtnOnClick = 'lolzz')
 }
 
 let mockValues = [{ title: 'super' }]
 
 describe('FormCard Component', () => {
-  test('Input value should be nothing when value prop is not there', () => {
-    const { container, queryByTestId } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
-    )
-    const input = queryByTestId('inputsuper')
-
-    expect(input.value).toEqual('')
-    expect(container).toMatchSnapshot()
-  })
-
-  test('Should render text onto textbox when user types', () => {
-    const { container, queryByTestId } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
-    )
-    const input = queryByTestId('inputsuper')
-
-    fireEvent.change(input, {
-      target: { value: 'Javascript' }
-    })
-
-    expect(input.value).toContain('Javascript')
-    expect(container).toMatchSnapshot()
-  })
-
   test('Should render MdInput component when type is MD_INPUT', () => {
     mockValues = [{ title: 'fireStarter', type: 'MD_INPUT' }]
     const { container, queryByTestId } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
+      <FormCard
+        values={mockValues}
+        onSubmit={mockBtn}
+        onChange={value => (test = value)}
+      />
     )
     const input = queryByTestId('textbox')
     fireEvent.change(input, {
       target: { value: 'Javascript' }
     })
 
-    expect(input.value).toContain('Javascript')
+    expect(test).toEqual('Javascript')
     expect(container).toMatchSnapshot()
   })
 
   test('Should call function with updated array as parameter when button is clicked', () => {
     const { container, queryByText } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
+      <FormCard onChange={() => {}} values={mockValues} onSubmit={mockBtn} />
     )
     const button = queryByText('Create New Challenge')
     fireEvent.click(button, { button: 1 })
-    expect(testBtnOnClick).toEqual([
-      {
-        title: 'fireStarter',
-        type: 'MD_INPUT',
-        value: 'Javascript'
-      }
-    ])
+    expect(testBtnOnClick).toEqual('lolzz')
     expect(container).toMatchSnapshot()
   })
 
-  test('Should not call onSubmit callback function if there is a nums error', () => {
-    testBtnOnClick = 'cat'
-    mockValues = [...mockValues, { title: 'fiary', error: ['nums'] }]
-    const { container, queryByText } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
+  test('Should update testing onChange variable when person types MdInput', () => {
+    mockValues = [{ title: 'fireStarter', type: 'MD_INPUT' }]
+    let testOnChange = 'lolz'
+    const { container, queryByTestId } = render(
+      <FormCard
+        values={mockValues}
+        onSubmit={mockBtn}
+        onChange={(value, i) => (testOnChange = value + i)}
+      />
     )
-    const button = queryByText('Create New Challenge')
-    fireEvent.click(button, { button: 1 })
-    expect(testBtnOnClick).toEqual('cat')
+    const input = queryByTestId('textbox')
+    fireEvent.change(input, {
+      target: { value: 'Javascript' }
+    })
+
+    expect(testOnChange).toEqual('Javascript0')
     expect(container).toMatchSnapshot()
   })
 
-  test('Should not call onSubmit callback function if there is a require error', () => {
-    mockValues = [...mockValues, { title: 'fiary', error: ['require'] }]
-    const { container, queryByText } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
+  test('Should update testing onChange variable when person types', () => {
+    mockValues = [{ title: 'fireStarter' }]
+    let testOnChange = 'lolz'
+    const { container, queryByTestId } = render(
+      <FormCard
+        values={mockValues}
+        onSubmit={mockBtn}
+        onChange={(value, i) => (testOnChange = value + i)}
+      />
     )
-    const button = queryByText('Create New Challenge')
-    fireEvent.click(button, { button: 1 })
-    expect(testBtnOnClick).toEqual('cat')
+    const input = queryByTestId('input0')
+    fireEvent.change(input, {
+      target: { value: 'Javascript' }
+    })
+
+    expect(testOnChange).toEqual('Javascript0')
     expect(container).toMatchSnapshot()
   })
 
   test('Should output empty title when title is empty', () => {
     mockValues = [{ title: '' }]
     const { container, getByTestId } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
+      <FormCard onChange={() => {}} values={mockValues} onSubmit={mockBtn} />
     )
     const input = getByTestId('h50')
     expect(input.value).toBeFalsy
@@ -102,7 +91,7 @@ describe('FormCard Component', () => {
   test('Should render nothing if id is a title', () => {
     mockValues = [{ title: 'id' }]
     const { container } = render(
-      <FormCard values={mockValues} onSubmit={mockBtn} />
+      <FormCard onChange={() => {}} values={mockValues} onSubmit={mockBtn} />
     )
     expect(container).toBeFalsy
     expect(container).toMatchSnapshot()
--- stories/components/FormCard.stories.tsx
+++ stories/components/FormCard.stories.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FormCard } from '../../components/FormCard'
+import React, { useState } from 'react'
+import { FormCard, MD_INPUT, Option } from '../../components/FormCard'
 
 export default {
   component: FormCard,
@@ -11,17 +11,70 @@ const mockBtn = {
   onClick: () => {}
 }
 
-const mockValues = [
-  { title: 'Nickname', value: 'ChickenFarmer00' },
+const mockValues: Option[] = [
+  { title: 'Nickname', value: 'ChickenFarmer00', error: 'Required' },
   { title: 'Description', value: 'Amazing farm of chicken' },
+  { title: 'Title', value: 'oheuhehe', type: MD_INPUT },
   { title: 'Title', value: 'oheuhehe' },
   { title: 'Level', value: 'noob' }
 ]
 
-export const Basic: React.FC = () => (
-  <FormCard values={mockValues} onSubmit={mockBtn} />
-)
+const MockBasic: React.FC = () => {
+  const [options, setOptions] = useState(mockValues)
+  const onChange = (value: string, index: number) => {
+    const newOptions = [...options]
+    newOptions[index].value = value
+    setOptions(newOptions)
+  }
 
-export const _WithTitle: React.FC = () => (
-  <FormCard values={mockValues} title="clownfish" onSubmit={mockBtn} />
-)
+  return <FormCard onChange={onChange} values={options} onSubmit={mockBtn} />
+}
+
+const MockWithTitle: React.FC = () => {
+  const [options, setOptions] = useState(mockValues)
+  const onChange = (value: string, index: number) => {
+    const newOptions = [...options]
+    newOptions[index].value = value
+    setOptions(newOptions)
+  }
+
+  return (
+    <FormCard
+      onChange={onChange}
+      values={options}
+      title="clownfish"
+      onSubmit={mockBtn}
+    />
+  )
+}
+
+const MockWithValidation: React.FC = () => {
+  const [options, setOptions] = useState(mockValues)
+  const onChange = (value: string, index: number) => {
+    const newOptions = [...options]
+    newOptions[index].value = value
+    newOptions[index].error = value ? '' : 'Field cannot be empty'
+    setOptions(newOptions)
+  }
+
+  return (
+    <FormCard
+      onChange={onChange}
+      values={options}
+      title="clownfish"
+      onSubmit={mockBtn}
+    />
+  )
+}
+
+export const Basic: React.FC = () => {
+  return <MockBasic />
+}
+
+export const _WithTitle: React.FC = () => {
+  return <MockWithTitle />
+}
+
+export const _WithValidation: React.FC = () => {
+  return <MockWithValidation />
+}
