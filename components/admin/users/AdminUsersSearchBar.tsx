a9bedefcc8fb7f19e5f518db013d1d7fcff5aeb2
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-06 13:08:23 -0700

    closes #376 - add comment above debounce function of AdminUsersSearchBar.tsx

--- components/admin/users/AdminUsersSearchBar.tsx
+++ components/admin/users/AdminUsersSearchBar.tsx
@@ -21,6 +21,12 @@ export const SearchBar: React.FC<SearchBarProps> = ({
   searchOption,
   setSearchOption
 }) => {
+  /*
+		The reason debounce is used here is to prevent page rerenders on every keystroke.
+		If there is a rerender on every keystroke, the CPU consumption is high and
+		creates a perception of the page being slow and sluggish.
+    Therefore, we only rerender when the user stops typing
+  */
   const run = _.debounce(setSearchOption, 500)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
