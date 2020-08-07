159e122f43ba6470ebcd928aeaeba71c3423a3a3
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-07 15:03:18 -0700

    closes #380 - init commit, FilterButtons component, updated snapshots

---
+++ stories/components/FilterButtons.stories.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { FilterButtons } from '../../components/FilterButtons'
+
+export default {
+  component: FilterButtons,
+  title: 'Components/FilterButtons'
+}
+
+type MockThemeProps = {
+  theme: 'success' | 'danger' | 'primary'
+}
+
+const colorsArr = ['success', 'danger', 'primary']
+const mockOptions = ['Admins', 'Non-Admins', 'None']
+
+const MockBasic: React.FC = () => {
+  const [option, setOption] = useState(mockOptions[1])
+
+  return (
+    <FilterButtons
+      currentOption={option}
+      onClick={setOption}
+      options={mockOptions}
+    >
+      Filter By:
+    </FilterButtons>
+  )
+}
+
+const MockColor: React.FC<MockThemeProps> = ({ theme }) => {
+  const [option, setOption] = useState(mockOptions[0])
+
+  return (
+    <FilterButtons
+      currentOption={option}
+      onClick={setOption}
+      options={mockOptions}
+      theme={theme}
+    >
+      Filter By:
+    </FilterButtons>
+  )
+}
+
+export const Basic: React.FC = () => {
+  return <MockBasic />
+}
+
+export const Colors: React.FC = () => {
+  const list = colorsArr.map((theme: any, key: number) => {
+    return <MockColor theme={theme} key={key} />
+  })
+
+  return <div>{list}</div>
+}
