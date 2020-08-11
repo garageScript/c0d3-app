442f7af6387b11a6b8022d717e1c1591a27ab4bc
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-11 16:18:20 -0700

    closes #376 - remove originalWord function, use substr instead, make more comments, take out unused variable

--- components/admin/users/AdminUsersSplitSearch.tsx
+++ components/admin/users/AdminUsersSplitSearch.tsx
@@ -1,51 +1,38 @@
 import React from 'react'
 
+/*
+	convert string to array with searchTerm removed (split removes it)
+	Ex:
+		Inputs: searchTerm='bon', str = 'bonjourbon'
+    Output: [jour]
+*/
 const split = (str: string, lowerCaseSearchTerm: string) => {
   const splitArr = str.toLowerCase().split(lowerCaseSearchTerm)
-  let tracker = 0
 
-  const res = splitArr.reduce((acc: string[], word: string) => {
+  const res = splitArr.reduce((acc: string[], word: string, i: number) => {
     acc.push(word)
-    tracker += word.length
 
     // used to prevent over-adding of searchTerm back into array
-    if (tracker === str.length) return acc
-
+    if (i === splitArr.length - 1) return acc
     acc.push(lowerCaseSearchTerm)
-    tracker += lowerCaseSearchTerm.length
     return acc
   }, [])
 
   return res
 }
 
-const originalWord: any = (
-  str: string,
-  strIndex: number[],
-  word: string,
-  res: string = '',
-  wordIndex: number = 0
-) => {
-  if (wordIndex === word.length) return res
-  res += str[strIndex[0]]
-  strIndex[0] += 1
-  return originalWord(str, strIndex, word, res, wordIndex + 1)
-}
-
+// converts words back into their original capitalization
 const originalCapitalization = (str: string, value: string[]) => {
-  /*
-    strIndex is an array, so that originalWord function can be put
-    outside of the originalCapitalization function
-  */
-  const strIndex = [0]
-
-  // converts words back into their original capitalization
-  value = value.reduce((acc: string[], word: string) => {
-    acc.push(originalWord(str, strIndex, word))
+  // tracker is used for `str.substr` to extract characters from the original string at the right places
+  let tracker = 0
+  const correctedArr = value.reduce((acc: string[], word: string) => {
+    const originalWord = str.substr(tracker, word.length)
+    tracker += word.length
+    acc.push(originalWord)
     return acc
   }, [])
 
-  return value
+  return correctedArr
 }
 
 export const AdminUsersSplitSearch = (str: string, searchTerm: string) => {
