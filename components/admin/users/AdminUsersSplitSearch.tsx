ff25e10ff6743be0a70362f66050df417848ed36
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-11 19:18:10 -0700

    closes #376 - merge split and originalcapitalization functoin together

--- components/admin/users/AdminUsersSplitSearch.tsx
+++ components/admin/users/AdminUsersSplitSearch.tsx
@@ -3,19 +3,29 @@ import React from 'react'
 /*
 	convert string to array, separated by and including searchTerm
 	Ex:
-		Inputs: searchTerm='bon', str = 'bonjourbon'
-		Output: [bon, jour, bon]
+		Inputs: searchTerm='bon', str = 'bonJourbon'
+		Output: [bon, Jour, bon]
 */
-const split = (str: string, lowerCaseSearchTerm: string) => {
+const split = (
+  str: string,
+  lowerCaseSearchTerm: string,
+  searchTerm: string
+) => {
   const splitArr = str.toLowerCase().split(lowerCaseSearchTerm)
+  // tracker is used for `str.substr` to extract characters from the original string at the correct places
+  let tracker = 0
 
   const res = splitArr.reduce(
     (acc: string[], word: string, splitArrIndex: number) => {
-      acc.push(word)
+      // converts words back into their original capitalization
+      const originalWord = str.substr(tracker, word.length)
+      acc.push(originalWord)
+      tracker += word.length
 
       // used to prevent over-adding of searchTerm back into array
       if (splitArrIndex === splitArr.length - 1) return acc
-      acc.push(lowerCaseSearchTerm)
+      acc.push(searchTerm)
+      tracker += searchTerm.length
       return acc
     },
     []
@@ -24,32 +34,18 @@ const split = (str: string, lowerCaseSearchTerm: string) => {
   return res
 }
 
-// converts words back into their original capitalization
-const originalCapitalization = (str: string, value: string[]) => {
-  // tracker is used for `str.substr` to extract characters from the original string at the right places
-  let tracker = 0
-  const correctedArr = value.reduce((acc: string[], word: string) => {
-    const originalWord = str.substr(tracker, word.length)
-    tracker += word.length
-    acc.push(originalWord)
-    return acc
-  }, [])
-
-  return correctedArr
-}
-
 export const AdminUsersSplitSearch = (str: string, searchTerm: string) => {
   // make all lowercase now to ensure both lower and uppercase characters can be searched for
   const lowerCaseSearchTerm = searchTerm.toLowerCase()
 
   // convert string to array with searchTerm included
-  const splitArr = split(str, lowerCaseSearchTerm)
+  const splitArr = split(str, lowerCaseSearchTerm, searchTerm)
 
   // make sure all capilization is correct
-  const correctedArr = originalCapitalization(str, splitArr)
+  // const correctedArr = originalCapitalization(str, splitArr)
 
   // highlight search Term
-  const res = correctedArr.map((word: string, key: number) => {
+  const res = splitArr.map((word: string, key: number) => {
     const bgColor =
       word.toLowerCase() === lowerCaseSearchTerm ? 'rgb(84, 64, 216, .25)' : ''
     return (
