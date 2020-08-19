ea8737c8ddbecd13c9acbdeb6a67a67f5ac77e58
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-19 12:37:19 -0700

    closes #383 - fix spacing on comments

--- components/admin/users/AdminUsersTable.tsx
+++ components/admin/users/AdminUsersTable.tsx
@@ -125,9 +125,9 @@ const UsersList: React.FC<UsersListProps> = ({
     let bool = true
 
     /* 
-			Need to make searchTerm and value of user[option] lowercase, 
-      to ensure both lower and uppercase characters can be searched for
-		*/
+    Need to make searchTerm and value of user[option] lowercase, 
+    to ensure both lower and uppercase characters can be searched for
+    */
     const lowerCaseSearchTerm = searchTerm.toLowerCase()
     const lowerCaseOptionValue = user[option].toLowerCase()
 
