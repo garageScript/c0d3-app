e2c9916227b8663eba8aee1a63d05f5d1a0c7f25
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-24 18:43:56 -0700

    closes #383 - update comment

--- helpers/splitWithSearchTerm.tsx
+++ helpers/splitWithSearchTerm.tsx
@@ -7,9 +7,10 @@
 
 export const splitWithSearchTerm = (str: string, searchTerm: string) => {
   /*
-	Using regex with split will return an array with the separator included.
-	The `i` flag is the case insensitive flag, used to ensure that the returned array
-	will include the searchTerm, regardless of the original capialization of the searchTerm
+	Using regex containing capturing parentheses `()` with split will return an array
+	with the separator included. The `i` flag is the case insensitive flag, used to
+	ensure that the returned array will include the searchTerm, regardless of the
+	original capialization of str or searchTerm.
   */
   const regex = new RegExp(`(${searchTerm})`, 'i')
   return str.split(regex).filter(s => s !== '')
