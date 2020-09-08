e5ee80f6d983626325d56cdb5b7250546a59bb83
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-09-07 19:56:00 -0700

    closes #392 - init commit, clean up tests, remove res.status(200)

--- pages/api/lessons.test.js
+++ pages/api/lessons.test.js
@@ -8,33 +8,26 @@ describe('lessonsAPI', () => {
     status: jest.fn(noop)
   }
 
-  getLessons.lessons = jest
-    .fn()
-    .mockReturnValue(['man', 'bear', 'pig', 'manbearpig'])
+  const req = null
 
-  test('should respond with 200 status code when there is no error', async () => {
-    await lessonsAPI(null, res)
-    expect(res.status).toBeCalledWith(200)
-  })
+  test('should respond with data from lessons query resolver when there is no \
+	error', async () => {
+    getLessons.lessons = jest
+      .fn()
+      .mockReturnValue(['man', 'bear', 'pig', 'manbearpig'])
 
-  test('should respond with json data from lessons() when there is no error', async () => {
-    await lessonsAPI(null, res)
+    await lessonsAPI(req, res)
     expect(res.json).toBeCalledWith(['man', 'bear', 'pig', 'manbearpig'])
   })
 
-  test('should respond with 500 status code when there is an error', async () => {
-    getLessons.lessons = jest.fn().mockImplementation(() => {
-      throw new Error('Error')
-    })
-    await lessonsAPI(null, res)
-    expect(res.status).toBeCalledWith(500)
-  })
-
-  test('should respond with error message when there is an error', async () => {
+  test('should respond with 500 status code and display error message when \
+	there is an error', async () => {
     getLessons.lessons = jest.fn().mockImplementation(() => {
       throw new Error('Error occured :(')
     })
-    await lessonsAPI(null, res)
+
+    await lessonsAPI(req, res)
+    expect(res.status).toBeCalledWith(500)
     expect(res.json).toBeCalledWith('Error occured :(')
   })
 })
--- pages/api/lessons.ts
+++ pages/api/lessons.ts
@@ -5,7 +5,6 @@ import { NextApiResponse } from 'next'
 export default async (_: LoggedRequest, res: NextApiResponse) => {
   try {
     const allLessons = await lessons()
-    res.status(200)
     res.json(allLessons)
   } catch (err) {
     res.status(500)
