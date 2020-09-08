f9d1aef31a249b9c94ceeae3d36ac25e7aae36cd
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-09-07 19:06:50 -0700

    closes #387 - clean up duplicate code in testing file

--- pages/api/lessons.test.js
+++ pages/api/lessons.test.js
@@ -8,18 +8,16 @@ describe('lessonsAPI', () => {
     status: jest.fn(noop)
   }
 
+  getLessons.lessons = jest
+    .fn()
+    .mockReturnValue(['man', 'bear', 'pig', 'manbearpig'])
+
   test('should respond with 200 status code when there is no error', async () => {
-    getLessons.lessons = jest
-      .fn()
-      .mockReturnValue(['man', 'bear', 'pig', 'manbearpig'])
     await lessonsAPI(null, res)
     expect(res.status).toBeCalledWith(200)
   })
 
   test('should respond with json data from lessons() when there is no error', async () => {
-    getLessons.lessons = jest
-      .fn()
-      .mockReturnValue(['man', 'bear', 'pig', 'manbearpig'])
     await lessonsAPI(null, res)
     expect(res.json).toBeCalledWith(['man', 'bear', 'pig', 'manbearpig'])
   })
@@ -34,7 +32,7 @@ describe('lessonsAPI', () => {
 
   test('should respond with error message when there is an error', async () => {
     getLessons.lessons = jest.fn().mockImplementation(() => {
-      throw new Error('Error')
+      throw new Error('Error occured :(')
     })
     await lessonsAPI(null, res)
     expect(res.json).toBeCalledWith('Error occured :(')
