81dde4c2476637bbd113eba739ad3a78dbe25372
cijin <cijininurhood@gmail.com>
2020-10-01 21:09:58 +0700

    Removing star component and story

--- stories/components/GiveStarCard.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { GiveStarCard } from '../../components/GiveStarCard'
-
-export default {
-  component: GiveStarCard,
-  title: 'Components/GiveStarCard'
-}
-
-export const Primary: React.FC = () => <GiveStarCard show={true} />
