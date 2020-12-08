2e77884a0da41290de7d7b6d22348b4b1349c5fb
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-12-08 19:31:02 -0300

    add hasPassedLesson helper function and tests

---
+++ helpers/hasPassedLesson.ts
@@ -0,0 +1,17 @@
+import { UserLesson } from './dbload'
+import _ from 'lodash'
+import { LessonStatus } from '../@types/lesson'
+
+export const hasPassedLesson = async (
+  reviewerId: number,
+  lessonId: string
+): Promise<Boolean> => {
+  // query userlesson that belongs to lesson and user
+  const userLesson = (await UserLesson.findOne({
+    where: {
+      lessonId: lessonId,
+      userId: reviewerId
+    }
+  })) as LessonStatus
+  return userLesson ? Boolean(userLesson.isPassed) : false
+}
