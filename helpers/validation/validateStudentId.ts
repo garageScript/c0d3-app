20b4171fa408a3b7a6ee0d930fc5e91ba523092c
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-12-13 23:51:16 -0800

    closes #442 - remove try catch block because theres no promises being dealt with..

--- helpers/validation/validateStudentId.ts
+++ helpers/validation/validateStudentId.ts
@@ -2,13 +2,9 @@ import { LoggedRequest } from '../../@types/helpers'
 import _ from 'lodash'
 
 export const validateStudentId = (req: LoggedRequest, errMsg?: string) => {
-  try {
-    const studentId = _.get(req, 'user.id', false)
-    if (!studentId) {
-      throw new Error(errMsg || 'Student is not logged in')
-    }
-    return studentId
-  } catch (err) {
-    throw new Error(err)
+  const studentId = _.get(req, 'user.id', false)
+  if (!studentId) {
+    throw new Error(errMsg || 'Student is not logged in')
   }
+  return studentId
 }
