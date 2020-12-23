25f1e257923bf84d9e528bea1ec3e9dd8ea0e4aa
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-12-22 17:27:49 -0800

    closes #452 - fix spacing in comment

--- __tests__/storyshots.test.js
+++ __tests__/storyshots.test.js
@@ -1,9 +1,9 @@
 import initStoryshots from '@storybook/addon-storyshots'
 
 /*	
-		This mock is necessary because there is currently a problem
-		with taking storyshots of Modals. See here for more info:
-		https://github.com/storybookjs/storybook/issues/2822
+	This mock is necessary because there is currently a problem
+	with taking storyshots of Modals. See here for more info:
+	https://github.com/storybookjs/storybook/issues/2822
 */
 jest.mock('react-bootstrap/Modal', () => {
   return props => (props.isOpen ? props.children : null)
