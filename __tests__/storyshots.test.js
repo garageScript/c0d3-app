f4a75473e946acfa916db918d418dc8ea4ad63e1
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-12-22 17:27:07 -0800

    closes #452 - fix spacing in comment

--- __tests__/storyshots.test.js
+++ __tests__/storyshots.test.js
@@ -1,6 +1,7 @@
 import initStoryshots from '@storybook/addon-storyshots'
 
-/*	This mock is necessary because there is currently a problem
+/*	
+		This mock is necessary because there is currently a problem
 		with taking storyshots of Modals. See here for more info:
 		https://github.com/storybookjs/storybook/issues/2822
 */
