99af538027583bc5a8f76e47715c2483ed7e4d4d
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-12-24 01:24:46 -0800

    closes #454 - fix wording in test

--- components/Thanks.test.js
+++ components/Thanks.test.js
@@ -3,14 +3,14 @@ import { render, fireEvent } from '@testing-library/react'
 import { Thanks } from './Thanks'
 
 describe('Thanks Component', () => {
-  test('should call callback function when exit button is clicked', () => {
+  test('should call callback function when done button is clicked', () => {
     let res = ''
     const expectedResult = 'babushka'
     const { container, getByRole } = render(
       <Thanks close={() => (res = expectedResult)} />
     )
-    const doneBtn = getByRole('button')
-    fireEvent.click(doneBtn)
+    const doneButton = getByRole('button')
+    fireEvent.click(doneButton)
     expect(res).toEqual(expectedResult)
     expect(container).toMatchSnapshot()
   })
