b5a901922e07b854f5cfeb4abb4d84a8e2ae3503
khoa <khoa@Anthonys-MacBook-Pro.local>
2021-01-28 22:46:31 -0800

    closes #478 - add mocks to challengescompleted stories, update corresponding snapshot

--- stories/components/ChallengesCompletedCard.stories.tsx
+++ stories/components/ChallengesCompletedCard.stories.tsx
@@ -1,12 +1,15 @@
+import { MockedProvider } from '@apollo/react-testing'
 import * as React from 'react'
 import { ChallengesCompletedCard } from '../../components/ChallengeMaterial'
-
+import SET_STAR from '../../graphql/queries/setStar'
+import GET_LESSON_MENTORS from '../../graphql/queries/getLessonMentors'
+import lessonMentorsData from '../../__dummy__/getLessonMentorsData'
 export default {
   component: ChallengesCompletedCard,
   title: 'Components/ChallengesCompletedCard'
 }
 
-export const ChallengesCompleted: React.FC = () => (
+export const StarGivenAlready: React.FC = () => (
   <ChallengesCompletedCard
     lessonId="5"
     starGiven="flimmy"
@@ -15,3 +18,35 @@ export const ChallengesCompleted: React.FC = () => (
     chatUrl="https://chat.c0d3.com/c0d3/channels/js0-foundations"
   />
 )
+
+export const StarNotGivenYet: React.FC = () => {
+  const mocks = [
+    {
+      request: {
+        query: SET_STAR,
+        variables: { mentorId: 4, lessonId: 5, comment: '1' }
+      },
+      result: {
+        data: { setStar: { success: true } }
+      }
+    },
+    {
+      request: { query: GET_LESSON_MENTORS, variables: { lessonId: '5' } },
+      result: {
+        data: { getLessonMentors: lessonMentorsData }
+      }
+    }
+  ]
+
+  return (
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <ChallengesCompletedCard
+        lessonId="5"
+        starGiven=""
+        imageSrc="icon-challenge-complete.jpg"
+        reviewUrl="https://c0d3.com/teacher/5"
+        chatUrl="https://chat.c0d3.com/c0d3/channels/js0-foundations"
+      />
+    </MockedProvider>
+  )
+}
