22387bc61ddd184016f790853378bd4481096ee4
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-02-10 19:56:22 -0300

    closes #492 - filter current user on the query

--- graphql/queryResolvers/getLessonMentors.ts
+++ graphql/queryResolvers/getLessonMentors.ts
@@ -1,5 +1,8 @@
 import { User, UserLesson } from '../../helpers/dbload'
 import { validateLessonId } from '../../helpers/validateLessonId'
+import { Op } from 'sequelize'
+import { Context } from '../../@types/helpers'
+import _ from 'lodash'
 
 type ArgsGetLessonMentors = {
   lessonId: string
@@ -7,14 +10,17 @@ type ArgsGetLessonMentors = {
 
 export const getLessonMentors = async (
   _parent: void,
-  args: ArgsGetLessonMentors
+  args: ArgsGetLessonMentors,
+  context: Context
 ): Promise<User[]> => {
   const { lessonId } = args
+  const userId: number | undefined = _.get(context, 'req.user.id', undefined)
   try {
     await validateLessonId(parseInt(lessonId))
     const results: [UserLesson & { user: User }] = await UserLesson.findAll({
       where: {
-        lessonId
+        lessonId,
+        ...(userId && { userId: { [Op.ne]: userId } })
       },
       include: [
         { model: User, as: 'user', attributes: ['username', 'name', 'id'] }
