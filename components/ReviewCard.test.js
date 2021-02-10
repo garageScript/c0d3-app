1f59df0f510e2bb00f5035b3edec6ca2d79865d9
Ulisseus <newdaddy1911@gmail.com>
2021-02-10 16:32:25 +0300

    try to fix build test

--- components/ReviewCard.test.js
+++ components/ReviewCard.test.js
@@ -48,17 +48,11 @@ describe('ReviewCard Component', () => {
     const mocks = [
       {
         request: {
-          query: ACCEPT_SUBMISSION
+          query: ACCEPT_SUBMISSION,
+          variables: { id: 1, comment: 'good job' }
         },
         result: {
           data: { id: 1, comment: 'good job', status: 'passed' }
-        },
-
-        request: {
-          query: REJECT_SUBMISSION
-        },
-        result: {
-          data: { id: 1, comment: 'error on line 3', status: 'active' }
         }
       }
     ]
