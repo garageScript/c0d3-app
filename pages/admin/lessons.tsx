110d1e410a86c1685342a105961c9e902e60d3d8
Ulisseus <newdaddy1911@gmail.com>
2021-02-21 12:32:18 +0300

    remove dublicate checks

--- pages/admin/lessons.tsx
+++ pages/admin/lessons.tsx
@@ -4,18 +4,11 @@ import { AdminLessonsSideBar } from '../../components/admin/lessons/AdminLessons
 import { withGetApp, GetAppProps } from '../../graphql'
 import { Lesson } from '../../graphql/index'
 import { AdminLayout } from '../../components/admin/AdminLayout'
-import Error, { StatusCode } from '../../components/Error'
-import LoadingSpinner from '../../components/LoadingSpinner'
+
 const Lessons: React.FC<GetAppProps> = ({ data }) => {
-  const { loading, error, lessons } = data
-  if (loading) return <LoadingSpinner />
-  if (error) {
-    return (
-      <Error code={StatusCode.INTERNAL_SERVER_ERROR} message={error.message} />
-    )
-  }
   const [selectedLesson, setSelectedLesson] = useState(0)
   const [lessonsList, setLessons] = useState<null | Lesson[]>(null)
+  const { lessons } = data
   return (
     <AdminLayout data={data}>
       <div className="row mt-4">
