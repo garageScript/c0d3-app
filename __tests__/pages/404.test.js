c1ce0d31a3358313827a8d44086565439013a580
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-03-22 15:12:24 -0300

    add snapshots

--- __tests__/pages/404.test.js
+++ __tests__/pages/404.test.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
+import {
+  render,
+  waitFor,
+  waitForElementToBeRemoved
+} from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
 import NotFound from '../../pages/404'
 
 describe('404 Error Page', () => {
   it('should render', async () => {
-    const { container } = render(
+    const { container, queryByRole } = render(
       <MockedProvider>
         <NotFound />
       </MockedProvider>
     )
-    await waitFor(() => expect(container).toMatchSnapshot)
+    await waitForElementToBeRemoved(() =>
+      queryByRole('heading', { name: /loading/i })
+    )
+    await waitFor(() => expect(container).toMatchSnapshot())
   })
 })
