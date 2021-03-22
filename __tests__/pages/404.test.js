14eb392b27dad4b4370ddbb47e1cd376fa764035
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-03-22 15:04:09 -0300

    add test for 404 page

---
+++ __tests__/pages/404.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render, waitFor } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+import NotFound from '../../pages/404'
+
+describe('404 Error Page', () => {
+  it('should render', async () => {
+    const { container } = render(
+      <MockedProvider>
+        <NotFound />
+      </MockedProvider>
+    )
+    await waitFor(() => expect(container).toMatchSnapshot)
+  })
+})
