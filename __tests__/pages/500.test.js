7cf965b1dfa998fbb6a1c763789502bf23cf0947
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-03-22 15:04:16 -0300

    add test for 500 page

---
+++ __tests__/pages/500.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render, waitFor } from '@testing-library/react'
+import { MockedProvider } from '@apollo/client/testing'
+import InternalServerError from '../../pages/500'
+
+describe('500 Error Page', () => {
+  it('should render', async () => {
+    const { container } = render(
+      <MockedProvider>
+        <InternalServerError />
+      </MockedProvider>
+    )
+    await waitFor(() => expect(container).toMatchSnapshot)
+  })
+})
