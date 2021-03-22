c13234837c127e36dfd77b1f65c9153fa34c3aa5
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-03-22 14:07:19 -0300

    remove unused import

--- __tests__/utils/expectLoading.tsx
+++ __tests__/utils/expectLoading.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
 
 import GET_APP from '../../graphql/queries/getApp'
