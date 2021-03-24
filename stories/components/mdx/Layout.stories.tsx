a5de199ee54781d28958c6d11fa6b7576bd891ad
Ulisseus <newdaddy1911@gmail.com>
2021-03-24 22:20:37 +0300

    add Layout story

---
+++ stories/components/mdx/Layout.stories.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import Layout from '../../../components/mdx/Layout'
+import { MockedProvider } from '@apollo/client/testing'
+import GET_APP from '../../../graphql/queries/getApp'
+import dummyLessonData from '../../../__dummy__/lessonData'
+import dummySessionData from '../../../__dummy__/sessionData'
+import { withTestRouter } from '../../../__tests__/utils/withTestRouter'
+
+const mocks = [
+  {
+    request: { query: GET_APP },
+    result: {
+      data: {
+        lessons: dummyLessonData,
+        session: dummySessionData,
+        alerts: []
+      }
+    }
+  }
+]
+
+export default {
+  component: Layout,
+  title: 'mdx/Layout'
+}
+
+export const WithText: React.FC = () => {
+  return withTestRouter(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <Layout>
+        <p>
+          Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+          commodi aspernatur enim, consectetur. Cumque deleniti temporibus ipsam
+          atque a dolores quisquam quisquam adipisci possimus laboriosam.
+          Quibusdam facilis doloribus debitis! Sit quasi quod accusamus eos
+          quod. Ab quos consequuntur eaque quo rem! Mollitia reiciendis porro
+          quo magni incidunt dolore amet atque facilis ipsum deleniti rem!
+        </p>
+      </Layout>
+    </MockedProvider>
+  )
+}
