79ccbb7dcff65ba9d2103b9fd44944a064ca18b5
Charles <samannaphala@gmail.com>
2021-03-24 16:34:21 +0200

    Updates 499 - adds star comments to profile (backend)

--- helpers/dbload.ts
+++ helpers/dbload.ts
@@ -77,7 +77,7 @@ Submission.belongsTo(Lesson, { as: 'lesson' })
 
 Star.belongsTo(User, { as: 'student' })
 Star.belongsTo(User, { as: 'mentor' })
-Star.belongsTo(User, { as: 'lesson' })
+Star.belongsTo(Lesson, { as: 'lesson' })
 
 User.belongsToMany(Lesson, {
   foreignKey: 'userId', // Defaults to UserId
