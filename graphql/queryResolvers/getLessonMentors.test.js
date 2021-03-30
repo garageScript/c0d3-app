6459106ce86c3dd747330c4e813fb69361bb4c1a
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-03-30 20:04:19 -0300

    refactor `getLessonMentors` with Prisma

--- graphql/queryResolvers/getLessonMentors.test.js
+++ graphql/queryResolvers/getLessonMentors.test.js
@@ -1,72 +1,90 @@
-jest.mock('../../helpers/dbload')
 jest.mock('../../helpers/validateLessonId')
 import { getLessonMentors } from './getLessonMentors'
-import { User, UserLesson } from '../../helpers/dbload'
 import { validateLessonId } from '../../helpers/validateLessonId'
-import { Op } from 'sequelize'
+import { prisma } from '../../prisma'
 describe('getLessonMentors resolver', () => {
   beforeEach(() => {
     validateLessonId.mockReturnValue(true)
   })
 
   test('should return an array of User objects', async () => {
-    UserLesson.findAll = jest.fn().mockReturnValue([
-      { user: { username: 'user1', name: 'lol', id: 2 } },
-      {
-        user: {
-          username: 'user2',
-          name: 'potato',
-          id: 240
-        }
-      }
-    ])
-    const res = await getLessonMentors(null, { lessonId: '3' })
-    expect(res).toEqual([
+    const users = [
       { username: 'user1', name: 'lol', id: 2 },
-      { username: 'user2', name: 'potato', id: 240 }
-    ])
-    expect(UserLesson.findAll).toHaveBeenCalledWith({
-      where: { lessonId: '3' },
-      include: [
-        { model: User, as: 'user', attributes: ['username', 'name', 'id'] }
-      ]
+      {
+        username: 'user2',
+        name: 'potato',
+        id: 240
+      }
+    ]
+    prisma.user.findMany = jest.fn().mockReturnValue(users)
+    const res = await getLessonMentors(null, { lessonId: 3 })
+    expect(res).toEqual(users)
+    expect(prisma.user.findMany).toHaveBeenCalledWith({
+      select: {
+        username: true,
+        name: true,
+        id: true
+      },
+      where: {
+        id: { not: undefined },
+        userLessons: {
+          some: {
+            lessonId: 3,
+            isPassed: { not: null }
+          }
+        }
+      },
+      orderBy: {
+        username: 'asc'
+      }
     })
   })
 
   test('Should throw error if lessonId does not exist \
-  in database when updating lesson', async () => {
+  in database when updating lesson', () => {
     validateLessonId.mockImplementation(() => {
       throw new Error()
     })
-    await expect(
-      getLessonMentors(null, { lessonId: '3' })
-    ).rejects.toThrowError()
+    expect(getLessonMentors(null, { lessonId: 3 })).rejects.toThrowError()
   })
 
-  test('should throw an error', async () => {
-    UserLesson.findAll = jest.fn().mockImplementation(() => {
+  test('should throw an error', () => {
+    prisma.user.findMany = jest.fn().mockImplementation(() => {
       throw new Error()
     })
     //rejects: checks for promise rejection
     //which would be the case if an error was thrown in a Promise
-    await expect(getLessonMentors(null, { lessonId: '3' })).rejects.toThrow()
+    expect(getLessonMentors(null, { lessonId: '3' })).rejects.toThrow()
   })
 
   test('should include userId in the query if session exists', async () => {
-    UserLesson.findAll = jest
+    prisma.user.findMany = jest
       .fn()
-      .mockReturnValue([{ user: { username: 'user1', name: 'lol', id: 2 } }])
+      .mockReturnValue([{ username: 'user1', name: 'lol', id: 2 }])
     const res = await getLessonMentors(
       null,
-      { lessonId: '3' },
+      { lessonId: 3 },
       { req: { user: { id: 1 } } }
     )
     expect(res).toEqual([{ username: 'user1', name: 'lol', id: 2 }])
-    expect(UserLesson.findAll).toHaveBeenCalledWith({
-      where: { lessonId: '3', userId: { [Op.ne]: 1 } },
-      include: [
-        { model: User, as: 'user', attributes: ['username', 'name', 'id'] }
-      ]
+    expect(prisma.user.findMany).toHaveBeenCalledWith({
+      select: {
+        username: true,
+        name: true,
+        id: true
+      },
+      where: {
+        id: { not: 1 },
+        userLessons: {
+          some: {
+            lessonId: 3,
+            isPassed: { not: null }
+          }
+        }
+      },
+      orderBy: {
+        username: 'asc'
+      }
     })
   })
 })
