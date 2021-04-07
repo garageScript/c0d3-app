d481ad5288aa5168e9b233cea888092c65dfc67d
Ulisseus <newdaddy1911@gmail.com>
2021-04-07 17:55:28 +0300

    update #555 - add dev setup page

--- helpers/mdxComponents.tsx
+++ helpers/mdxComponents.tsx
@@ -6,11 +6,11 @@ import styles from '../scss/mdx.module.scss'
 
 const MDXcomponents = {
   inlineCode: (props: any) => (
-    <div className={`${styles['inline-code']}`} {...props} />
+    <code className={`${styles['inline-code']}`} {...props} />
   ),
   code: CodeBlock,
   blockquote: BlockQuote,
-  img: ModalImage,
+  Image: ModalImage,
   h1: (props: any) => <h1 className={`${styles['MDX_h1']}`} {...props} />,
   h2: (props: any) => <h2 className={`${styles['MDX_h2']}`} {...props} />,
   h3: (props: any) => <h3 className={`${styles['MDX_h3']}`} {...props} />,
