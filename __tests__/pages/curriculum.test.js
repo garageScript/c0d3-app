b85efc685675b97334570965c1380ec5f9165d9a
Ulisseus <newdaddy1911@gmail.com>
2021-04-15 12:39:21 +0300

    fix failing snapshot

--- __tests__/pages/curriculum.test.js
+++ __tests__/pages/curriculum.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Curriculum, { getStaticProps } from '../../pages/curriculum'
-import { render, waitFor } from '@testing-library/react'
+import { render, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { MockedProvider } from '@apollo/client/testing'
 import GET_APP from '../../graphql/queries/getApp'
@@ -111,6 +111,7 @@ describe('Curriculum Page', () => {
     )
 
     await waitFor(() => getByRole('link', { name: 'fakeusername' }))
+    await waitFor(() => screen.getByText('%'))
 
     await waitFor(() => expect(container).toMatchSnapshot())
   })
