def8e5b1564e7d077bc80ea08af04f000ca5ce67
Ulisseus <newdaddy1911@gmail.com>
2021-04-15 22:35:49 +0300

    add state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppNav from '../components/AppNav'
 import LandingPage from '../components/LandingPage'
 import Footer from '../components/Footer'
 import { useRouter } from 'next/router'
 const IndexPage: React.FC<{}> = () => {
   const router = useRouter()
-  const isLoggedIn =
-    typeof window !== 'undefined' && window.localStorage.getItem('loggedIn')
-  isLoggedIn && router.push('/curriculum')
+  const [status, setStatus] = useState(false)
+  useEffect(() => {
+    window.localStorage.getItem('loggedIn')
+      ? router.push('/curriculum')
+      : setStatus(true)
+  }, [])
   return (
     <>
       <AppNav />
-      {!isLoggedIn && (
+      {status && (
         <>
           <LandingPage />
           <Footer footerType="py-5 bg-white text-muted" />
