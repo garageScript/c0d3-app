462f31a620fd0a24f0af9cc4c8eae43a9685663a
Charles <samannaphala@gmail.com>
2021-04-16 19:16:21 +0300

    updating session.ts to use prisma queries

--- graphql/queryResolvers/session.ts
+++ graphql/queryResolvers/session.ts
@@ -1,13 +1,7 @@
-import db from '../../helpers/dbload'
 import { Context } from '../../@types/helpers'
-import { User as UserType } from '../../helpers/models/User'
-import { Star as StarType, LessonStatus } from '../../@types/lesson'
 import _ from 'lodash'
-const { User, Submission, UserLesson, Star } = db
+import { prisma } from '../../prisma'
 
-type Submission = {
-  lessonId: string
-}
 interface mentorUsernamesMapType {
   [userId: number]: string
 }
@@ -20,41 +14,49 @@ export const session = async (_parent: void, _args: void, context: Context) => {
   const userId = _.get(user, 'id', null)
   if (!user || !userId) return { lessonStatus: [] }
 
-  // FYI: The reason we are querying with parallelized promises:
-  // https://github.com/garageScript/c0d3-app/wiki/Sequelize-Query-Performance
-  const [submissions, lessonStatus, stars] = await Promise.all([
-    Submission.findAll({
-      where: { userId },
-      include: [{ model: User, as: 'reviewer' }]
+  const [lessonStatus, submissions, stars] = await Promise.all([
+    prisma.userLesson.findMany({
+      where: {
+        userId
+      }
     }),
-    UserLesson.findAll({ where: { userId } }),
-    Star.findAll({ where: { studentId: userId } })
+    prisma.submission.findMany({
+      where: {
+        userId
+      }
+    }),
+    prisma.star.findMany({
+      where: {
+        mentorId: userId
+      }
+    })
   ])
 
+  // select Stars."lessonId",  Stars."studentId", Stars."mentorId", Users."username" FROM Stars INNER JOIN Lessons ON "lessonId" = Lessons."id" INNER JOIN Users ON "mentorId" = Users."id" WHERE "studentId" = {userId};
   const mentorIdsMap: mentorIdsMapType = stars.reduce(
-    (map: mentorIdsMapType, { dataValues }: { dataValues: StarType }) => {
-      const { lessonId, mentorId } = dataValues
-      map[lessonId] = mentorId
+    (map: mentorIdsMapType, star) => {
+      const { lessonId, mentorId } = star
+      map[lessonId!] = mentorId!
       return map
     },
     {}
   )
 
-  const mentors = await User.findAll({
+  const mentors = await prisma.user.findMany({
     where: { id: Object.values(mentorIdsMap) as number[] }
   })
 
   const mentorUsernamesMap: mentorUsernamesMapType = mentors.reduce(
-    (map: mentorUsernamesMapType, { dataValues }: { dataValues: UserType }) => {
-      const { id, username } = dataValues
+    (map: mentorUsernamesMapType, mentor) => {
+      const { id, username } = mentor
       map[id] = username
       return map
     },
     {}
   )
 
-  lessonStatus.forEach((lesson: LessonStatus) => {
-    const mentorId = mentorIdsMap[lesson.lessonId]
+  lessonStatus.forEach(lesson => {
+    const mentorId = mentorIdsMap[lesson.lessonId!]
     const username = mentorUsernamesMap[mentorId]
     lesson.starGiven = username
   })
