49602f2413a18c165c8429ceb20f1d170d2d8bb3
Ulisseus <newdaddy1911@gmail.com>
2021-04-16 18:59:39 +0300

    close #677 - remove SSR redirection from the index page (#681)

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -1,45 +1,57 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, waitFor } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
-import IndexPage, { getServerSideProps } from '../../pages/index'
+import IndexPage from '../../pages/index'
+import dummySessionData from '../../__dummy__/sessionData'
 import GET_APP from '../../graphql/queries/getApp'
+import { useRouter } from 'next/router'
 
 describe('Index Page', () => {
+  const { push } = useRouter()
+
+  const mocksWithSession = session => [
+    {
+      request: { query: GET_APP },
+      result: {
+        data: {
+          session,
+          lessons: [],
+          alerts: []
+        }
+      }
+    }
+  ]
   test('Should render index page', async () => {
     const { container } = render(
-      <MockedProvider
-        mocks={[
-          {
-            request: { query: GET_APP },
-            result: {
-              data: {
-                session: null,
-                lessons: [],
-                alerts: []
-              }
-            }
-          }
-        ]}
-        addTypename={false}
-      >
+      <MockedProvider mocks={mocksWithSession(null)} addTypename={false}>
         <IndexPage />
       </MockedProvider>
     )
     expect(container).toMatchSnapshot()
   })
-  test('Should redirect to /curriculum if connect.sid exists', async () => {
-    expect(
-      await getServerSideProps({ req: { cookies: { 'connect.sid': 'test' } } })
-    ).toEqual({
-      redirect: {
-        destination: '/curriculum',
-        permanent: false
-      }
-    })
+  test('Should not redirect to /curriculum on first load', async () => {
+    const { push } = useRouter()
+    render(
+      <MockedProvider mocks={mocksWithSession(null)} addTypename={false}>
+        <IndexPage />
+      </MockedProvider>
+    )
+    await waitFor(() => expect(push).not.toHaveBeenCalled())
   })
-  test('Should not redirect without a cookie', async () => {
-    expect(await getServerSideProps({ req: { cookies: {} } })).toEqual({
-      props: {}
+
+  test('Should redirect to /curriculum if localstorage loggedIn exists', async () => {
+    process['browser'] = true
+    window.localStorage.setItem('loggedIn', 'true')
+    render(
+      <MockedProvider
+        mocks={mocksWithSession(dummySessionData)}
+        addTypename={false}
+      >
+        <IndexPage />
+      </MockedProvider>
+    )
+    await waitFor(() => {
+      expect(push).toBeCalledWith('/curriculum')
     })
   })
 })
