2fe5f8ec2d12c796c75cb92ad5aa9435a1e86ce2
Charles Ko <samannaphala@gmail.com>
2021-04-20 10:38:18 +0300

    Update prisma/migrations/20210404163725_added_star_lesson_relation/migration.sql
    Co-authored-by: Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>

--- prisma/migrations/20210404163725_added_star_lesson_relation/migration.sql
+++ prisma/migrations/20210404163725_added_star_lesson_relation/migration.sql
@@ -1,5 +1,28 @@
+-- Clean orphan values
+DELETE FROM "stars"
+WHERE
+	"lessonId" IS NULL
+	OR "studentId" IS NULL
+	OR "mentorId" IS NULL;
+
 -- AddForeignKey
 ALTER TABLE "stars" ADD FOREIGN KEY ("lessonId") REFERENCES "lessons"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- Make the column not nullable
+-- Make the columns not nullable
 ALTER TABLE "stars" ALTER COLUMN "lessonId" SET NOT NULL;
+ALTER TABLE "stars" ALTER COLUMN "studentId" SET NOT NULL;
+ALTER TABLE "stars" ALTER COLUMN "mentorId" SET NOT NULL;
+
+-- Drop and recreate student and mentor FK to update the ON DELETE action
+ALTER TABLE "stars" DROP CONSTRAINT "stars_mentorId_fkey";
+ALTER TABLE "stars" DROP CONSTRAINT "stars_studentId_fkey";
+
+ALTER TABLE "stars" ADD CONSTRAINT "stars_mentorId_fkey" 
+FOREIGN KEY ("mentorId") REFERENCES "users"("id") 
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+ALTER TABLE "stars" ADD CONSTRAINT "stars_studentId_fkey" 
+FOREIGN KEY ("studentId") REFERENCES "users"("id") 
+ON DELETE CASCADE
+ON UPDATE CASCADE;
