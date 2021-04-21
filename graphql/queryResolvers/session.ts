d5c2e759cc6257d059436d21b667dfe9dc6c2126
Charles <samannaphala@gmail.com>
2021-04-21 17:09:19 +0300

    prettier formatting

--- graphql/queryResolvers/session.ts
+++ graphql/queryResolvers/session.ts
@@ -2,6 +2,7 @@ import _ from 'lodash'
 import { userInfo } from '../../helpers/controllers/userInfoController'
 import { Context } from '../../@types/helpers'
 import { prisma } from '../../prisma'
+import { UserLesson } from '../index'
 
 interface lessonMentorMapType {
   [lessonId: string]: string
@@ -15,8 +16,8 @@ export const session = async (_parent: void, _args: void, context: Context) => {
   // FYI: The reason we are querying with parallelized promises:
   // https://github.com/garageScript/c0d3-app/wiki/Sequelize-Query-Performance
   const [session, starsGiven] = await Promise.all([
-   userInfo(_parent, { username: user.username }),
-   prisma.star.findMany({
+    userInfo(_parent, { username: user.username }),
+    prisma.star.findMany({
       where: {
         studentId: user.id
       },
@@ -36,7 +37,7 @@ export const session = async (_parent: void, _args: void, context: Context) => {
   ])
 
   const submissions = _.get(session, 'submissions', [])
-  const userLessons = _.get(session, 'lessonStatus', [])
+  const userLessons = _.get(session, 'lessonStatus', [] as UserLesson[])
 
   const lessonMentorMap = starsGiven.reduce((map, starGiven) => {
     const mentorUsername = _.get(starGiven, 'mentor.username', '')
@@ -46,7 +47,7 @@ export const session = async (_parent: void, _args: void, context: Context) => {
 
   const lessonStatus = userLessons.map(userLesson => ({
     ...userLesson,
-    starGiven: lessonMentorMap[userLesson.lessonId] || ''
+    starGiven: lessonMentorMap[userLesson.lessonId!] || ''
   }))
 
   return {
