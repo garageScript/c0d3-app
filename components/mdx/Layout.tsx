1f2aee011b1a3dd15a811cb1241bca95a5452332
Ulisseus <newdaddy1911@gmail.com>
2021-04-23 18:55:32 +0300

    WIP add arrow navigation

--- components/mdx/Layout.tsx
+++ components/mdx/Layout.tsx
@@ -1,29 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../Layout'
 import LessonTitleCard, {
   LessonTitleProps
 } from '../../components/LessonTitleCard'
 import styles from '../../scss/mdx.module.scss'
 import { useRouter } from 'next/router'
-type LayoutProps = Partial<LessonTitleProps & {subLessons?: string[] }>
+import _ from 'lodash'
+
+type LayoutProps = Partial<LessonTitleProps & { subLessons?: string[] }>
+
+const ScrollTop: React.FC<{ scroll: number }> = ({ scroll }) => {
+  return (
+    <img
+      src="/assets/mdx/topArrow.svg"
+      className={`${scroll > 0.3 ? 'd-block' : 'd-none'} position-fixed ${
+        styles['arrow']
+      }`}
+      onClick={() =>
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        })
+      }
+    />
+  )
+}
 const LessonLayout: React.FC<LayoutProps> = props => {
   const router = useRouter()
   const location = router.pathname.split('/')
   const current = location[location.length - 1]
-  const lessonParts = props.subLessons&&props.subLessons.map((l, i) => {
-    const url = l.toLowerCase().split(' ').join('_')
-    return (
-      <a
-        className={`${styles['subtitle']} ${
-          url !== current ? 'text-muted' : `text-dark dark`
-        } d-block`}
-        href={url}
-        key={url}
-      >
-        {`Part ${i + 1}: ${l}`}
-      </a>
+  const [scroll, setScroll] = useState(0)
+  const throttled = _.throttle(() => {
+    setScroll(
+      window.scrollY / (document.body.scrollHeight - window.innerHeight)
     )
-  })
+  }, 100)
+  useEffect(() => {
+    window.addEventListener('scroll', throttled)
+  }, [])
+  const lessonParts =
+    props.subLessons &&
+    props.subLessons.map((l, i) => {
+      const url = l.toLowerCase().split(' ').join('_')
+      return (
+        <a
+          className={`${styles['subtitle']} ${
+            url !== current ? 'text-muted' : `text-dark dark`
+          } d-block`}
+          href={url}
+          key={url}
+        >
+          {`Part ${i + 1}: ${l}`}
+        </a>
+      )
+    })
   return (
     <Layout>
       <div className="mt-4">
@@ -34,6 +65,15 @@ const LessonLayout: React.FC<LayoutProps> = props => {
       <div
         className={`${styles['lesson-wrapper']} card shadow-sm mt-3 d-block border-0 p-3 p-md-4 bg-white`}
       >
+        {!props.lessonTitle && (
+          <button
+            className="btn btn-link text-primary p-0"
+            onClick={() => router.back()}
+          >
+            Go Back
+          </button>
+        )}
+        <ScrollTop scroll={scroll} />
         <div className={styles.title}>{props.lessonTitle}</div>
         {lessonParts}
         {props.children}
