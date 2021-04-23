6ab32ea8f0a76c65d69854e982b535e9b01050e2
Ulisseus <newdaddy1911@gmail.com>
2021-04-23 13:52:06 +0300

    WIP split into two parts

--- components/mdx/ChallengBar.tsx
+++ components/mdx/ChallengBar.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import NavLink from '../NavLink'
 
-const ChallengeBar: React.FC<{ src: string }> = ({ src }) => {
+const ChallengeBar: React.FC<{
+  href: string
+  description: string
+  title: string
+}> = ({ href, description, title }) => {
   return (
     <div className="d-flex justify-content-center">
-      <div className="w-100 card shadow-sm mt-3 bg-primary text-white p-2 border-0">
-        <h1 className="text-center font-weight-bold">
-          Master your skills by solving challenges
+      <div className="card shadow-sm bg-primary text-white px-4 py-2 border-0">
+        <h1 className="text-center font-weight-bold align-self-center">
+          {title}
         </h1>
-        <p className="px-2 text-center">
-          We have curated the challenges so that it can give you concrete
-          foundation.
-        </p>
-        <div className="d-flex justify-content-center">
+        <p className="px-2 m-0 text-center">{description}</p>
+        <div className="text-center">
           <NavLink
-            path={src}
-            className={`btn btn-light mt-2 text-primary w-50`}
+            path={href}
+            className={`btn btn-light mt-2 text-primary`}
           >
             View Challenges
           </NavLink>
--- components/mdx/Layout.tsx
+++ components/mdx/Layout.tsx
@@ -4,7 +4,26 @@ import LessonTitleCard, {
   LessonTitleProps
 } from '../../components/LessonTitleCard'
 import styles from '../../scss/mdx.module.scss'
-const LessonLayout: React.FC<Partial<LessonTitleProps>> = props => {
+import { useRouter } from 'next/router'
+type LayoutProps = Partial<LessonTitleProps & {subLessons?: string[] }>
+const LessonLayout: React.FC<LayoutProps> = props => {
+  const router = useRouter()
+  const location = router.pathname.split('/')
+  const current = location[location.length - 1]
+  const lessonParts = props.subLessons&&props.subLessons.map((l, i) => {
+    const url = l.toLowerCase().split(' ').join('_')
+    return (
+      <a
+        className={`${styles['subtitle']} ${
+          url !== current ? 'text-muted' : `text-dark dark`
+        } d-block`}
+        href={url}
+        key={url}
+      >
+        {`Part ${i + 1}: ${l}`}
+      </a>
+    )
+  })
   return (
     <Layout>
       <div className="mt-4">
@@ -13,9 +32,10 @@ const LessonLayout: React.FC<Partial<LessonTitleProps>> = props => {
         )}
       </div>
       <div
-        className={`${styles['lesson-wrapper']} card shadow-sm mt-3 d-block border-0 p-4 bg-white`}
+        className={`${styles['lesson-wrapper']} card shadow-sm mt-3 d-block border-0 p-3 p-md-4 bg-white`}
       >
-        <h1 className="title font-weight-bold">{props.lessonTitle}</h1>
+        <div className={styles.title}>{props.lessonTitle}</div>
+        {lessonParts}
         {props.children}
       </div>
     </Layout>
