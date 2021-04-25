7dce4edcab33eba558bad45cc7045786b115d0e1
Ulisseus <newdaddy1911@gmail.com>
2021-04-25 12:32:35 +0300

    fix mobile chrome layout

--- components/mdx/Spoiler.tsx
+++ components/mdx/Spoiler.tsx
@@ -1,30 +1,13 @@
-import React, { useState } from 'react'
-import styles from '../../scss/spoiler.module.scss'
+import React from 'react'
 type Props = {
   name?: string
 }
 const Spoiler: React.FC<Props> = ({ children, name }) => {
-  const [status, setStatus] = useState({
-    css: { display: 'none' },
-    name: 'spoiler-arrow__right'
-  })
-  const onClick = () => {
-    status.css.display === 'none'
-      ? setStatus({ css: { display: 'inherit' }, name: 'spoiler-arrow__down' })
-      : setStatus({ css: { display: 'none' }, name: 'spoiler-arrow__right' })
-  }
   return (
-    <>
-      <div className="mb-1 ml-1 spoiler" onClick={onClick}>
-        <div className="d-flex">
-          <div className={`${styles[status.name]} align-self-center mr-3`} />
-          <div className={`${styles['spoiler__title']}`}>
-            {name || 'Answer'}
-          </div>
-        </div>
-      </div>
-      <div style={status.css}>{children}</div>
-    </>
+    <details>
+      <summary>{name || 'Answer'}</summary>
+      {children}
+    </details>
   )
 }
 
