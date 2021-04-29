58a9fd4c1087ea218da2def53e5f931a110a4eb6
Ulisseus <newdaddy1911@gmail.com>
2021-04-29 21:46:12 +0300

    add comment

--- __mocks__/next/image.tsx
+++ __mocks__/next/image.tsx
@@ -3,7 +3,9 @@ import type { ImageProps } from 'next/image'
 import _ from 'lodash'
 
 const Image = (props: ImageProps) => {
-  //not all Image properties are allowed in <img/> tag
+  /*not all Image properties are allowed in <img/> tag
+  ObjectFit causes warnings, but otherwise lessons/error images won't keep their dimensions 
+  see https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit*/
   const copy = _.omit(props, 'objectFit')
   return <img {...copy} />
 }
