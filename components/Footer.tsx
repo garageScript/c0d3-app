0f9603edf3a853b10172173f2091cdbdca150364
Flacial <ruthemiumwithdiamond@protonmail.com>
2021-05-25 16:26:24 +0400

    Update footer copyright year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer: React.FC<Props> = ({ footerType }) => {
     : `mt-5 font-weight-light text-center pb-5 text-muted`
   return (
     <footer className={footerClass}>
-      &copy; Copyright 2020 GarageScript
+      &copy; Copyright 2021 GarageScript
       <br />
       All Rights Reserved
     </footer>
