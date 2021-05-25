3d32e45170fd1660030d7d6ef3f99d888d58790e
flacial <ruthemiumwithdiamond@protonmail.com>
2021-05-25 19:05:13 +0400

    Update test snapshots

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,11 +9,7 @@ const Footer: React.FC<Props> = ({ footerType }) => {
     ? `font-weight-light text-center ${footerType}`
     : `mt-5 font-weight-light text-center pb-5 text-muted`
   return (
-    <footer className={footerClass}>
-      &copy; Copyright 2021 GarageScript
-      <br />
-      All Rights Reserved
-    </footer>
+    <footer className={footerClass}>&copy; Copyright 2021 GarageScript</footer>
   )
 }
 
