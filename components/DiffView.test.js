f9b4f71461606f65158ab1a49c8f4f8c46c4a591
Ulisseus <newdaddy1911@gmail.com>
2021-05-26 11:15:40 +0300

    remove async from DiffView test

--- components/DiffView.test.js
+++ components/DiffView.test.js
@@ -46,7 +46,7 @@ describe('DiffView component', () => {
       }
     }
   ]
-  test('Should render diff with comments', async () => {
+  test('Should render diff with comments', () => {
     const { container } = render(
       <MockedProvider mocks={[]} addTypename={false}>
         <DiffView
@@ -63,7 +63,7 @@ describe('DiffView component', () => {
     userEvent.click(screen.getByText('6'))
     expect(screen.getByText('First Comment')).toBeVisible()
   })
-  test('Should add comment box', async () => {
+  test('Should add comment box', () => {
     const { container } = render(
       <MockedProvider mocks={[]} addTypename={false}>
         <DiffView
@@ -79,7 +79,7 @@ describe('DiffView component', () => {
     expect(container).toMatchSnapshot()
     expect(screen.getByText('Add comment')).toBeVisible()
   })
-  test('Should remove comment box with no comments', async () => {
+  test('Should remove comment box with no comments', () => {
     render(
       <MockedProvider mocks={[]} addTypename={false}>
         <DiffView
@@ -96,7 +96,7 @@ describe('DiffView component', () => {
     userEvent.click(screen.getByText('4'))
     expect(screen.queryByText('Add comment')).toBeFalsy()
   })
-  test('Should not add comment box if submission was accepted', async () => {
+  test('Should not add comment box if submission was accepted', () => {
     render(
       <MockedProvider mocks={[]} addTypename={false}>
         <DiffView
@@ -112,13 +112,13 @@ describe('DiffView component', () => {
     userEvent.click(screen.getByText('4'))
     expect(screen.queryByText('Add comment')).toBeFalsy()
   })
-  test('Should render empty div if no diff is provided', async () => {
+  test('Should render empty div if no diff is provided', () => {
     const { container } = render(
       <DiffView comments={comments} name="Noob Newbie" username="newbie" />
     )
     expect(container.firstChild).toEqual(null)
   })
-  test('Should default to javascript if language is not supported', async () => {
+  test('Should default to javascript if language is not supported', () => {
     const cDiff =
       'diff --git a/js7/1.c b/js7/1.c\nindex 9c96b34..853bddf 100644\n--- a/js7/1.c\n+++ b/js7/1.c\n@@ -1,8 +1,19 @@\n-// write your code here!\n const solution = () => {\n-  // global clear all timeout:\n+  const allT = [];\n+  const old = setTimeout;\n+  window.setTimeout = (func, delay) => {\n+    const realTimeout = old(func, delay);\n+    allT.push(realTimeout);\n+    return realTimeout;\n+  };\n+  window.clearAllTimouts = () => {\n+    while (allT.length) {\n+      clearTimeout(allT.pop());\n+    }\n+  };\n   cat = () => {\n-  }\n+    window.clearAllTimouts();\n+  };\n };\n \n module.exports = solution;'
     render(
@@ -131,7 +131,7 @@ describe('DiffView component', () => {
     )
     expect(screen.getByText('js7/1.c')).toBeVisible()
   })
-  test('Should do nothing with incorrect diff', async () => {
+  test('Should do nothing with incorrect diff', () => {
     const incorrectDiff =
       'diff --git \nindex 9c96b34..853bddf 100644\n---@@ -1,8 +1,19 @@\n-// write your code here!\n const solution = () => {\n-  // global clear all timeout:\n+  const allT = [];\n+  const old = setTimeout;\n+  window.setTimeout = (func, delay) => {\n+    const realTimeout = old(func, delay);\n+    allT.push(realTimeout);\n+    return realTimeout;\n+  };\n+  window.clearAllTimouts = () => {\n+    while (allT.length) {\n+      clearTimeout(allT.pop());\n+    }\n+  };\n   cat = () => {\n-  }\n+    window.clearAllTimouts();\n+  };\n };\n \n module.exports = solution;'
     render(
