aadda4968d23e235e6be62881587484f14e52951
Ulisseus <newdaddy1911@gmail.com>
2021-05-28 21:52:22 +0300

    add logging to debug vercel

--- helpers/controllers/submissionController.test.js
+++ helpers/controllers/submissionController.test.js
@@ -46,6 +46,7 @@ describe('Submissions Mutations', () => {
         .fn()
         .mockResolvedValue({ id: 1, ...submissionMock })
       prisma.lesson.findFirst = jest.fn().mockResolvedValue(null)
+      prisma.comment.deleteMany = jest.fn()
     })
 
     test('should return submission', () => {
