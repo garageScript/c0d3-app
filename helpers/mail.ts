d2dca6045c8231366879f484dfa8361e4bfa7e58
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-06-19 20:21:16 -0300

    removed debugging code

--- helpers/mail.ts
+++ helpers/mail.ts
@@ -1,11 +1,11 @@
+import nodemailer from 'nodemailer'
 import { getResetTemplate, getSignupTemplate } from './mailTemplate'
-import { createTransport } from 'nodemailer'
 
 const SMTP_HOST = process.env.SMTP_HOST ?? ''
 const SMTP_USER = process.env.SMTP_USER ?? ''
 const SMTP_PASSWORD = process.env.SMTP_PASSWORD ?? ''
 
-const transporter = createTransport({
+const transporter = nodemailer.createTransport({
   pool: true,
   host: SMTP_HOST,
   port: 465,
@@ -17,37 +17,17 @@ const transporter = createTransport({
 })
 
 export const sendSignupEmail = (email: string, token: string) =>
-  transporter
-    .sendMail({
-      from: '<hello@c0d3.com>',
-      to: email,
-      subject: 'Welcome to c0d3.com',
-      html: getSignupTemplate(token)
-    })
-    .then(metadata =>
-      console.log(`Email sent succesfully\n`, JSON.stringify(metadata, null, 2))
-    )
-    .catch(error =>
-      console.log(
-        `Error while sending the email\n`,
-        JSON.stringify(error, null, 2)
-      )
-    )
+  transporter.sendMail({
+    from: '<hello@c0d3.com>',
+    to: email,
+    subject: 'Welcome to c0d3.com',
+    html: getSignupTemplate(token)
+  })
 
 export const sendResetEmail = (email: string, token: string) =>
-  transporter
-    .sendMail({
-      from: `<admin@c0d3.com>`,
-      to: email,
-      subject: 'Forgot Password',
-      html: getResetTemplate(token)
-    })
-    .then(metadata =>
-      console.log(`Email sent succesfully\n`, JSON.stringify(metadata, null, 2))
-    )
-    .catch(error =>
-      console.log(
-        `Error while sending the email\n`,
-        JSON.stringify(error, null, 2)
-      )
-    )
+  transporter.sendMail({
+    from: `<admin@c0d3.com>`,
+    to: email,
+    subject: 'Forgot Password',
+    html: getResetTemplate(token)
+  })
