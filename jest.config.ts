b7b452d197ae707b44686a4deda2b59bfd3d009e
Charles <samannaphala@gmail.com>
2021-06-25 08:21:33 +0300

    removes nextauth from code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -15,7 +15,8 @@ const config: Config.InitialOptions = {
     'graphql/index.tsx',
     '__tests__/utils/',
     'prisma/',
-    'scripts/'
+    'scripts/',
+    '[...nextauth].ts'
   ],
   coverageThreshold: {
     global: {
