99e72d479b7e14d442b828f62f8f8a465958c2e4
Charles <samannaphala@gmail.com>
2021-06-25 15:10:07 +0300

    adds additional scopes to Discord options

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,7 +9,8 @@ export default NextAuth({
   providers: [
     Providers.Discord({
       clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      scope: 'email guilds.join gdm.join identify'
     })
   ],
   adapter: PrismaAdapter(prisma),
