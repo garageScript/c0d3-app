f25c9c8016daa401f6a1483e5e18c8255d06a0f0
Thomas Herzog <Tomrule007@gmail.com>
2021-06-27 12:43:40 -0700

    Move default env variables to .env.example

--- next.config.js
+++ next.config.js
@@ -11,16 +11,14 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   pageExtensions: ['tsx', 'js', 'jsx', 'mdx', 'ts'],
   env: {
-    DB_NAME: process.env.DB_NAME || 'c0d3dev6',
-    DB_USER: process.env.DB_USER || 'c0d3db',
-    DB_PW: process.env.DB_PW || 'letmein2',
-    DB_PORT: process.env.DB_PORT || '5432',
-    DB_HOST: process.env.DB_HOST || 'freedomains.dev',
-    SENTRY_DSN:
-      process.env.SENTRY_DSN ||
-      'https://e95626afb0454145b569bc69116f838c@o385150.ingest.sentry.io/5221680',
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PW: process.env.DB_PW,
+    DB_PORT: process.env.DB_PORT,
+    DB_HOST: process.env.DB_HOST,
+    SENTRY_DSN: process.env.SENTRY_DSN,
     POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
-    SESSION_SECRET: process.env.SESSION_SECRET || 'c0d3hard3r',
-    SERVER_URL: process.env.SERVER_URL || '/api/graphql'
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    SERVER_URL: process.env.SERVER_URL
   }
 })
