2ca441226d135b885689c6d821e10f45a32fb7d7
Thomas Herzog <Tomrule007@gmail.com>
2021-06-28 12:25:16 -0700

    Manual test error route update to use withSentry

--- pages/api/error.ts
+++ pages/api/error.ts
@@ -1,7 +1,9 @@
+import { withSentry } from '@sentry/nextjs'
 import { LoggedRequest } from '../../@types/helpers'
 import { NextApiResponse } from 'next'
 
-export default async (_: LoggedRequest, res: NextApiResponse) => {
+const handler = async (_: LoggedRequest, res: NextApiResponse) => {
   throw new Error('API throw error test')
   res.status(200).json({ name: 'John Doe' })
 }
+export default withSentry(handler)
