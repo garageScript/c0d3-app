76a3a7d52b4bd11be3f863fff79c905ed174deba
Thomas Herzog <Tomrule007@gmail.com>
2021-06-29 12:27:37 -0700

    connect SentryWebpackPluginOptions

--- next.config.js
+++ next.config.js
@@ -38,4 +38,4 @@ const SentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports)
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
