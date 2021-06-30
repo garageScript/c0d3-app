5cd01a9bb286cee583839ce21825bcd4c67ac15f
Thomas Herzog <Tomrule007@gmail.com>
2021-06-30 09:22:56 -0700

    wire new sentry/nextjs setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const { withSentryConfig } = require('@sentry/nextjs')
 const slug = require('remark-slug')
 const toc = require('remark-toc')
 const gfm = require('remark-gfm')
@@ -8,6 +9,21 @@ const withMDX = require('@next/mdx')({
     remarkPlugins: [slug, [toc, { maxDepth: 2 }], gfm]
   }
 })
-module.exports = withMDX({
+const moduleExports = withMDX({
   pageExtensions: ['tsx', 'js', 'jsx', 'mdx', 'ts']
 })
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // silent: true // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
