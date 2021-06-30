1e2130479f6a572f252963ea224df96f0a68eb9c
Thomas Herzog <Tomrule007@gmail.com>
2021-06-30 07:34:35 -0700

    remove env variable from next config

--- next.config.js
+++ next.config.js
@@ -9,18 +9,5 @@ const withMDX = require('@next/mdx')({
   }
 })
 module.exports = withMDX({
-  pageExtensions: ['tsx', 'js', 'jsx', 'mdx', 'ts'],
-  env: {
-    DB_NAME: process.env.DB_NAME || 'c0d3dev6',
-    DB_USER: process.env.DB_USER || 'c0d3db',
-    DB_PW: process.env.DB_PW || 'letmein2',
-    DB_PORT: process.env.DB_PORT || '5432',
-    DB_HOST: process.env.DB_HOST || 'freedomains.dev',
-    SENTRY_DSN:
-      process.env.SENTRY_DSN ||
-      'https://e95626afb0454145b569bc69116f838c@o385150.ingest.sentry.io/5221680',
-    POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
-    SESSION_SECRET: process.env.SESSION_SECRET || 'c0d3hard3r',
-    SERVER_URL: process.env.SERVER_URL || '/api/graphql'
-  }
+  pageExtensions: ['tsx', 'js', 'jsx', 'mdx', 'ts']
 })
