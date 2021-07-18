7b337513a91868f37615d6fc38347d5fec3936c7
kondanna <samannaphala@gmail.com>
2021-07-18 16:35:22 +0300

    lints dummy data

--- __dummy__/discordOAuthData.js
+++ __dummy__/discordOAuthData.js
@@ -1,28 +1,27 @@
 export default {
-    tokenResponse: {
-      access_token: '6qrZcUqja7812RVdnEKjpzOL4CvHBFG',
-      token_type: 'Bearer',
-      expires_in: 604800,
-      refresh_token: 'D43f5y0ahjqew82jZ4NViEr2YafMKhue',
-      scope: 'email guilds.join gdm.join identify'
-    },
-    userInfoResponse: {
-      id: '756944741073027119',
-      username: 'charolastra',
-      avatar: 'ea8f5f59aff14450e892321ba128745d',
-      discriminator: '3886',
-      public_flags: 0,
-      flags: 0,
-      locale: 'en-US',
-      mfa_enabled: true,
-      email: 'charlieworkhd@gmail.com',
-      verified: true
-    },
-    userInfo: {
-      userId: 1,
-      username: 'charolastra',
-      avatarUrl: `https://cdn.discordapp.com/avatars/756944741073027119/ea8f5f59aff14450e892321ba128745d.png`,
-      refreshToken: 'D43f5y0ahjqew82jZ4NViEr2YafMKhue'
-    }
+  tokenResponse: {
+    access_token: '6qrZcUqja7812RVdnEKjpzOL4CvHBFG',
+    token_type: 'Bearer',
+    expires_in: 604800,
+    refresh_token: 'D43f5y0ahjqew82jZ4NViEr2YafMKhue',
+    scope: 'email guilds.join gdm.join identify'
+  },
+  userInfoResponse: {
+    id: '756944741073027119',
+    username: 'charolastra',
+    avatar: 'ea8f5f59aff14450e892321ba128745d',
+    discriminator: '3886',
+    public_flags: 0,
+    flags: 0,
+    locale: 'en-US',
+    mfa_enabled: true,
+    email: 'charlieworkhd@gmail.com',
+    verified: true
+  },
+  userInfo: {
+    userId: 1,
+    username: 'charolastra',
+    avatarUrl: `https://cdn.discordapp.com/avatars/756944741073027119/ea8f5f59aff14450e892321ba128745d.png`,
+    refreshToken: 'D43f5y0ahjqew82jZ4NViEr2YafMKhue'
   }
-  
+}
