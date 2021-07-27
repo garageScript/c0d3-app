1b5f3e234e8577e57a6e5d0b04a80f30d7834431
Thomas Herzog <Tomrule007@gmail.com>
2021-07-27 15:03:01 -0700

    Capitalize Type Name

--- @types/page.d.ts
+++ @types/page.d.ts
@@ -1,11 +1,11 @@
 import { NextPage } from 'next'
 import { ReactElement, ReactNode } from 'react'
 
-export type layoutGetter = (page: ReactElement, pageProps?: {}) => ReactNode
+export type LayoutGetter = (page: ReactElement, pageProps?: {}) => ReactNode
 export interface WithLayout {
-  getLayout: layoutGetter
+  getLayout: LayoutGetter
 }
 
 export type Page<P = {}> = NextPage<P> & {
-  getLayout?: layoutGetter
+  getLayout?: LayoutGetter
 }
