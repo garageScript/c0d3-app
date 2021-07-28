d0126579018a5997784a68ed4c05c8d074e3a64f
Thomas Herzog <Tomrule007@gmail.com>
2021-07-27 21:04:33 -0700

    Modify component to use new SSG props

--- components/LessonTitleCard.tsx
+++ components/LessonTitleCard.tsx
@@ -2,77 +2,70 @@ import React from 'react'
 import NavLink from './NavLink'
 import styles from '../scss/lessonTitleCard.module.scss'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { LessonMetaData } from '../helpers/static/getLessonMetaData'
 
 export type LessonTitleProps = {
-  lessonCoverUrl: string
-  lessonUrl: string
-  lessonTitle: string
-  lessonId: number
   isPassed: boolean
-  setShow?: React.Dispatch<React.SetStateAction<boolean>>
-  show?: boolean
+  metaData: LessonMetaData
 }
 
-const LessonTitleCard: React.FC<LessonTitleProps> = props => {
+const LessonTitleCard: React.FC<LessonTitleProps> = ({
+  metaData,
+  isPassed
+}) => {
   const router = useRouter()
+  const { pathname, query } = router
+  const { lesson_slug } = router.query
+
+  // Has to be a better solution for this up one directory
+  const backOnePath = pathname.slice(0, pathname.lastIndexOf('/'))
+  const backOnePathQuery = Object.fromEntries(
+    Object.entries(query).filter(([key]) => backOnePath.includes(key))
+  )
 
   return (
-    <div className="card shadow-sm mt-3 col-12 px-0 pt-3 border-0">
+    <div className="card shadow-sm mt-4 col-12 px-0 pt-3 border-0">
       <div className="card-body p-0">
         <div className="d-flex mb-3 px-3">
           <img
             className={`${styles['lessonTitleCard__lesson-cover']} mr-3`}
-            src={`/assets/curriculum/${props.lessonCoverUrl}`}
+            src={`/assets/curriculum/${`js-${metaData.order}-cover.svg`}`}
             alt="lesson-cover"
           />
           <div>
             <p className="m-0">
-              <a
-                href="#"
-                onClick={e => {
-                  // Link does not get correct styles without the href
-                  // Prevent scroll to top before going back
-                  e.preventDefault()
-                  router.back()
+              <Link
+                href={{
+                  pathname: backOnePath,
+                  query: backOnePathQuery
                 }}
               >
-                Go Back
-              </a>
+                <a>Go Back</a>
+              </Link>
             </p>
             <h1 className={`${styles['lessonTitleCard__lesson-title']}`}>
-              {props.lessonTitle}
+              {metaData.title}
             </h1>
           </div>
         </div>
         <div className="card-footer bg-white p-0">
           <NavLink
-            path={props.lessonUrl}
+            path={`/lesson/${lesson_slug}`}
             className="btn border-right rounded-0 px-4 py-3"
-            external
           >
             LESSON
           </NavLink>
-          {/* 768 px is md bootstrap breakpoint */}
-          {typeof window !== 'undefined' &&
-          window.innerWidth <= 768 &&
-          props.setShow ? (
-            <div
-              onClick={() => props.setShow!(!props.show)}
-              className="btn border-right rounded-0 px-4 py-3"
-            >
-              SHOW CHALLENGES
-            </div>
-          ) : (
+          <NavLink
+            path={`/lesson/${lesson_slug}/challenges`}
+            className="btn border-right rounded-0 px-4 py-3"
+          >
+            CHALLENGES
+          </NavLink>
+
+          {isPassed && (
             <NavLink
-              path={`/curriculum/${props.lessonId}`}
-              className="btn border-right rounded-0 px-4 py-3"
-            >
-              CHALLENGES
-            </NavLink>
-          )}
-          {props.isPassed && (
-            <NavLink
-              path={`/review/${props.lessonId}`}
+              path={`/lesson/${lesson_slug}/review`}
               className="btn border-right rounded-0 px-4 py-3"
             >
               REVIEW
