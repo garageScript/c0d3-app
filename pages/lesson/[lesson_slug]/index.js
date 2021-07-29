1e5906f9dd998e0c32a7d11c862a670340ff0890
Thomas Herzog <Tomrule007@gmail.com>
2021-07-28 22:20:02 -0700

    Lesson page redirects to first sublesson

--- pages/lesson/[lesson_slug]/index.js
+++ pages/lesson/[lesson_slug]/index.js
@@ -1,35 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 import { getLayout } from '../../../components/LessonLayout'
 
 import { getAllLessonPaths } from '../../../helpers/static/getAllLessonPaths'
 import { getLessonMetaData } from '../../../helpers/static/getLessonMetaData'
 import { getSubLessonMDXSource } from '../../../helpers/static/getSubLessonMDXSource'
+import { useRouter } from 'next/router'
+const Index = ({ lesson_slug, sublesson_slug }) => {
+  // Auto redirect client side to first sublesson
+  const router = useRouter()
+  useEffect(() => {
+    router.replace(
+      '/lesson/[lesson_slug]/[sublesson_slug]',
+      `/lesson/${lesson_slug}/${sublesson_slug}`
+    )
+  }, [])
 
-const Index = ({ subLessons, lesson_slug }) => {
-  const orderedSublessons = subLessons.sort((a, b) => {
-    return a.frontMatter.order - b.frontMatter.order
-  })
-
-  return (
-    <>
-      <h1>sublessons</h1>
-      <ul>
-        {orderedSublessons.map(
-          ({ sublesson_slug, frontMatter: { title, order } }) => (
-            <li key={order}>
-              <Link
-                as={`/lesson/${lesson_slug}/${sublesson_slug}`}
-                href={`/lesson/[lesson_slug]/[sublesson_slug]`}
-              >
-                <a>{title}</a>
-              </Link>
-            </li>
-          )
-        )}
-      </ul>
-    </>
-  )
+  return <>Loading...</>
 }
 
 Index.getLayout = getLayout
@@ -46,10 +33,14 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { lesson_slug } }) {
   const metaData = await getLessonMetaData(lesson_slug)
   const subLessons = await getSubLessonMDXSource({ lesson_slug })
+  const firstSubLesson = subLessons.find(
+    sublesson => sublesson.frontMatter.order === 1
+  )
+
   return {
     props: {
       lesson_slug,
-      subLessons,
+      sublesson_slug: firstSubLesson.sublesson_slug,
       metaData // Consumed by LessonLayout
     }
   }
