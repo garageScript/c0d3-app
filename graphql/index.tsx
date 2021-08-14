87bef4b114fe8b64deb8cd9cf332a106bed2323f
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-08-14 10:50:43 -0300

    update graphql/index.tsx

--- graphql/index.tsx
+++ graphql/index.tsx
@@ -986,6 +986,10 @@ export type UserInfoQuery = { __typename?: 'Query' } & {
 
 export type ResolverTypeWrapper<T> = Promise<T> | T
 
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>
+}
+
 export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
   fragment: string
   resolve: ResolverFn<TResult, TParent, TContext, TArgs>
@@ -1000,6 +1004,7 @@ export type StitchingResolver<TResult, TParent, TContext, TArgs> =
   | NewStitchingResolver<TResult, TParent, TContext, TArgs>
 export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
   | ResolverFn<TResult, TParent, TContext, TArgs>
+  | ResolverWithResolve<TResult, TParent, TContext, TArgs>
   | StitchingResolver<TResult, TParent, TContext, TArgs>
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
