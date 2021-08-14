07616d655423e1d92562fa777d67a1bc475a568c
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-08-14 12:40:19 -0300

    version bumps, added nextjs eslint plugin

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,19 +1,19 @@
+import { PrismaSessionStore } from '@quixo3/prisma-session-store'
 import { withSentry } from '@sentry/nextjs'
 import { ApolloServer } from 'apollo-server-micro'
 import session from 'express-session'
 import nextConnect from 'next-connect'
-import userMiddleware from '../../helpers/middleware/user'
-import loggingMiddleware from '../../helpers/middleware/logger'
-import typeDefs from '../../graphql/typeDefs'
 import resolvers from '../../graphql/resolvers'
-import { PrismaSessionStore } from '@quixo3/prisma-session-store'
-import prisma from '../../prisma'
+import typeDefs from '../../graphql/typeDefs'
 import { apolloLogPlugin } from '../../helpers/apolloLogPlugin'
+import loggingMiddleware from '../../helpers/middleware/logger'
+import userMiddleware from '../../helpers/middleware/user'
+import prisma from '../../prisma'
 
 const ONE_DAY = 1000 * 60 * 60 * 24
 const ONE_WEEK = ONE_DAY * 7
 
-const handler: any = nextConnect() // For session middleware. TODO: Need to define types for Session
+const handler = nextConnect()
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
@@ -26,23 +26,23 @@ const apolloServer = new ApolloServer({
 
 const graphQLHandler = apolloServer.createHandler({ path: '/api/graphql' })
 
+const sessionMiddleware = session({
+  secret: process.env.SESSION_SECRET as string,
+  store: new PrismaSessionStore(prisma, {
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined,
+    checkPeriod: ONE_DAY
+  }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: ONE_WEEK
+  }
+})
+
 handler
   .use(loggingMiddleware)
-  .use(
-    session({
-      secret: process.env.SESSION_SECRET as string,
-      store: new PrismaSessionStore(prisma, {
-        dbRecordIdIsSessionId: true,
-        dbRecordIdFunction: undefined,
-        checkPeriod: ONE_DAY
-      }),
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: ONE_WEEK
-      }
-    })
-  )
+  .use(sessionMiddleware)
   .use(userMiddleware)
   .get('/api/graphql', graphQLHandler)
   .post('/api/graphql', graphQLHandler)
