cab82032fd6eb2f9353a2fe09479397f22959528
Thomas Herzog <Tomrule007@gmail.com>
2021-08-26 15:49:39 -0700

    create useBreakpoint hook for using bootstrap breakpoints in js

---
+++ helpers/useBreakpoint.test.js
@@ -0,0 +1,181 @@
+import '../__mocks__/matchMedia.mock'
+import { renderHook, act } from '@testing-library/react-hooks/server'
+import useBreakpoint from './useBreakpoint'
+
+describe('useBreakpoint', () => {
+  beforeAll(() => {
+    global.console.warn = jest.fn()
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test.each`
+    initial  | expected
+    ${true}  | ${true}
+    ${false} | ${false}
+  `(
+    'should first return initial value: $initial -> $initial',
+    ({ initial, expected }) => {
+      const { result } = renderHook(() => useBreakpoint('xs', 'down', initial))
+      expect(result.current).toBe(expected)
+    }
+  )
+
+  test.each`
+    matches  | expected
+    ${true}  | ${true}
+    ${false} | ${false}
+  `(
+    'should then return matches value after hydrate: $matches -> $matches',
+    ({ matches, expected }) => {
+      window.matchMedia.mockImplementation(query => ({
+        matches,
+        media: query,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn()
+      }))
+
+      const { result, hydrate } = renderHook(() =>
+        useBreakpoint('xs', 'down', false)
+      )
+      hydrate()
+
+      expect(result.current).toBe(expected)
+    }
+  )
+
+  test('should add and remove mediaQuery event listeners on mount / unmount', () => {
+    const mockAddEventListener = jest.fn()
+    const mockRemoveEventListener = jest.fn()
+    window.matchMedia.mockImplementation(query => ({
+      matches: true,
+      media: query,
+      addEventListener: mockAddEventListener,
+      removeEventListener: mockRemoveEventListener
+    }))
+
+    const { result, hydrate, unmount } = renderHook(() =>
+      useBreakpoint('xs', 'down', false)
+    )
+    hydrate()
+    expect(mockAddEventListener).toHaveBeenCalled()
+    unmount()
+    expect(mockRemoveEventListener).toHaveBeenCalled()
+  })
+
+  test('should update matches when event listener is called', () => {
+    const mockAddEventListener = jest.fn()
+    const mockRemoveEventListener = jest.fn()
+    window.matchMedia.mockImplementation(query => ({
+      matches: true,
+      media: query,
+      addEventListener: mockAddEventListener,
+      removeEventListener: mockRemoveEventListener
+    }))
+
+    const { result, hydrate, unmount } = renderHook(() =>
+      useBreakpoint('xs', 'down', false)
+    )
+    hydrate()
+    expect(mockAddEventListener).toHaveBeenCalledWith(
+      'change',
+      expect.any(Function)
+    )
+    // Get ref to changeHandler from first call to setup mediaQuery.addEventListener
+    const changeHandler = mockAddEventListener.mock.calls[0][1]
+
+    act(() => {
+      changeHandler({ matches: false })
+    })
+    expect(result.current).toBe(false)
+
+    act(() => {
+      changeHandler({ matches: true })
+    })
+    expect(result.current).toBe(true)
+  })
+
+  // The expected results from this test are meant to exactly match the bootstrap 4.6 queries
+  // @see https://getbootstrap.com/docs/4.6/layout/overview/#responsive-breakpoints
+  test.each`
+    breakpoint | condition | expected
+    ${'sm'}    | ${'up'}   | ${'(min-width: 576px)'}
+    ${'md'}    | ${'up'}   | ${'(min-width: 768px)'}
+    ${'lg'}    | ${'up'}   | ${'(min-width: 992px)'}
+    ${'xl'}    | ${'up'}   | ${'(min-width: 1200px)'}
+    ${'xs'}    | ${'down'} | ${'(max-width: 575.98px)'}
+    ${'sm'}    | ${'down'} | ${'(max-width: 767.98px)'}
+    ${'md'}    | ${'down'} | ${'(max-width: 991.98px)'}
+    ${'lg'}    | ${'down'} | ${'(max-width: 1199.98px)'}
+  `(
+    'media queries should be same at bootstrap docs: $breakpoint-$condition -> $expected',
+    ({ breakpoint, condition, expected }) => {
+      window.matchMedia.mockImplementation(query => ({
+        matches: true,
+        media: query,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn()
+      }))
+
+      const { result, hydrate } = renderHook(() =>
+        useBreakpoint(breakpoint, condition, false)
+      )
+      hydrate()
+      expect(window.matchMedia).toBeCalledWith(expected)
+    }
+  )
+
+  test('if no condition value is provided default to "down"', () => {
+    window.matchMedia.mockImplementation(query => ({
+      matches: true,
+      media: query,
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn()
+    }))
+
+    const { result, hydrate } = renderHook(() => useBreakpoint('xs'))
+    hydrate()
+    expect(window.matchMedia).toBeCalledWith('(max-width: 575.98px)')
+  })
+
+  test.each`
+    breakpoint | condition
+    ${'xs'}    | ${'up'}
+    ${'xl'}    | ${'down'}
+  `(
+    'should console.warning for $breakpoint-$condition as it will always be true',
+    ({ breakpoint, condition }) => {
+      const { result, hydrate } = renderHook(() =>
+        useBreakpoint(breakpoint, condition, false)
+      )
+      hydrate()
+      expect(result.current).toBe(true)
+      expect(global.console.warn).toHaveBeenCalledWith(
+        expect.stringContaining('Unnecessary useBreakpoint(')
+      )
+    }
+  )
+  test.each`
+    breakpoint | condition
+    ${'xs'}    | ${'up'}
+    ${'xl'}    | ${'down'}
+  `(
+    'should not show console.warn in production mode ',
+    ({ breakpoint, condition }) => {
+      // mock NODE_ENV
+      const REAL_NODE_ENV = process.env.NODE_ENV
+      process.env.NODE_ENV = 'production'
+
+      const { result, hydrate } = renderHook(() =>
+        useBreakpoint(breakpoint, condition, false)
+      )
+      hydrate()
+      expect(result.current).toBe(true)
+      expect(global.console.warn).not.toHaveBeenCalled()
+
+      // reset NODE_ENV
+      process.env.NODE_ENV = REAL_NODE_ENV
+    }
+  )
+})
---
+++ helpers/useBreakpoint.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+
+// Breakpoint order
+const index = {
+  xs: 0,
+  sm: 1,
+  md: 2,
+  lg: 3,
+  xl: 4
+}
+
+// Breakpoint pixels
+const pixels = [0, 576, 768, 992, 1200]
+
+/**
+ * Hook uses media queries to trigger javascript events based on bootstrap breakpoints
+ * @See https://getbootstrap.com/docs/4.6/layout/overview/#responsive-breakpoints
+ * @param breakpoint Bootstrap breakpoints:
+ * - xs: <576, sm: 576, md: 768, lg: 992, xl: 1200
+ * @param condition Default 'down' IE lessThan
+ *  - "down"  < breakpoint
+ *  - "up"   >= breakpoint
+ * @param initial default to false (Set this value to best guess for server side rendering)
+ * @returns {boolean} - if breakpoint condition is met
+ */
+export default function useBreakpoint(
+  breakpoint: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+  condition: 'down' | 'up' = 'down', // | 'only',
+  initial = false
+) {
+  const [matches, setMatches] = useState(initial)
+  useEffect(() => {
+    if (
+      (breakpoint === 'xs' && condition === 'up') ||
+      (breakpoint === 'xl' && condition === 'down')
+    ) {
+      // These cases make no sense as they will always return true
+      // so we skip attaching event listeners and return true
+      // **Also sending a warning msg in non production mode to help developers
+      setMatches(true)
+      if (process.env.NODE_ENV !== 'production')
+        console.warn(
+          'Unnecessary useBreakpoint(${breakpoint},${condition}), this condition will always return true!'
+        )
+
+      return
+    }
+
+    const query =
+      condition === 'up'
+        ? `(min-width: ${pixels[index[breakpoint]]}px)`
+        : `(max-width: ${pixels[index[breakpoint] + 1] - 0.02}px)`
+
+    const mql = window.matchMedia(query)
+    // Update if different from initial value
+    if (mql.matches !== matches) {
+      setMatches(mql.matches)
+    }
+    const changeHandler = (e: MediaQueryListEvent) => setMatches(e.matches)
+
+    mql.addEventListener('change', changeHandler)
+    return () => mql.removeEventListener('change', changeHandler)
+  }, [])
+
+  return matches
+}
