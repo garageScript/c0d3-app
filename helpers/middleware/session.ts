043bbc35eabf7eac7c5d21cc920a929617100ec4
kondanna <samannaphala@gmail.com>
2021-08-27 21:42:54 +0300

    lints session.ts

--- helpers/middleware/session.ts
+++ helpers/middleware/session.ts
@@ -5,7 +5,7 @@ import session from 'express-session'
 const ONE_DAY = 1000 * 60 * 60 * 24
 const ONE_WEEK = ONE_DAY * 7
 
-// This needs to be a function because otherwise the moment this file is required (for example in test), 
+// This needs to be a function because otherwise the moment this file is required (for example in test),
 // prisma would try to make a db connection.
 // Making this a function will allow this file to be required normally without any side effects (like automatically connecting to prisma)
 
