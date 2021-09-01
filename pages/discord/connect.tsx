13d6a6246c92c7dbdcecdbb7e350aff4c142522f
kondanna <samannaphala@gmail.com>
2021-09-01 09:49:11 +0300

    updates auth flow url, prettifies and updates snapshot

--- pages/discord/connect.tsx
+++ pages/discord/connect.tsx
@@ -7,7 +7,7 @@ import NavLink from '../../components/NavLink'
 import Link from 'next/link'
 
 const discordConnectPage =
-  'https://discord.com/api/oauth2/authorize?client_id=845470281283665970&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email%20guilds.join%20gdm.join'
+  'https://discord.com/api/oauth2/authorize?client_id=845470281283665970&redirect_uri=https%3A%2F%2Fc0d3.com%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email%20guilds.join%20gdm.join'
 
 const ConnectToDiscordPage: React.FC & WithLayout = () => {
   return (
@@ -20,23 +20,24 @@ const ConnectToDiscordPage: React.FC & WithLayout = () => {
             <a href={discordConnectPage} className="button">
               connect your Discord account
             </a>
-            , or create one if you don't already have one. Our students and
+            , or create one if you don&apos;t already have one. Our students and
             mentors use Discord to communicate and help each other, give you
             feedback on your challenges, and sometimes do virtual hangouts.
           </p>
           <p>
-            If you don't want to connect to Discord, then you won't get any
-            value out of creating an account on C0D3.com. Feel free to browse
-            our lessons and study on your own!
+            If you don&apos;t want to connect to Discord, then you won&apos;t
+            get any value out of creating an account on C0D3.com. Feel free to
+            browse our lessons and study on your own!
           </p>
           <p>
             Also, if you would like to share your reasons for not using Discord,
-            we'd love to hear about it! Your thoughts will help us make more
-            informed decisions about our community platform and if the reasons
-            are compelling enough, may inspire us to switch to an alternative.
+            we&apos;d love to hear about it! Your thoughts will help us make
+            more informed decisions about our community platform and if the
+            reasons are compelling enough, may inspire us to switch to an
+            alternative.
           </p>
         </div>
-        <Link href={discordConnectPage}>
+        <Link href={discordConnectPage} passHref>
           <button
             type="button"
             className="btn btn-primary btn-lg btn-block mb-3"
@@ -44,7 +45,9 @@ const ConnectToDiscordPage: React.FC & WithLayout = () => {
             Connect Now
           </button>
         </Link>
-        <NavLink path="/curriculum">No thanks, I'll study on my own. </NavLink>
+        <NavLink path="/curriculum">
+          No thanks, I&apos;ll study on my own.{' '}
+        </NavLink>
       </Card>
     </>
   )
