45a89363d39795ccf6ffa32aa35205fb5b7389be
Thomas Herzog <Tomrule007@gmail.com>
2021-09-08 20:25:33 -0700

    Create mdx/CodePen component

---
+++ components/mdx/CodePen.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import CSS from 'csstype'
+
+type Props = {
+  id: string
+  style?: CSS.Properties
+  height?: number
+  defaultTab?: 'js' | 'css' | 'html' | 'result'
+  showResult?: boolean
+  theme?: 'light' | 'dark'
+}
+
+const CodePen: React.FC<Props> = ({
+  id,
+  height = 300,
+  style = { width: '100%' },
+  defaultTab = 'result',
+  showResult = true,
+  theme = 'light'
+}) => {
+  // User can select one of 3 tabs for left hand side: js / css / html
+  // or pick 'result' to display full width result. selecting 'result' will
+  // override 'showResults = false' as you must display some tab.
+  // showResults true will display results on right hand side.
+  const tabString =
+    defaultTab === 'result'
+      ? 'result'
+      : `${defaultTab}${showResult ? '%2Cresult' : ''}` // %2C is URL encoded comma character
+
+  return (
+    <iframe
+      height={height}
+      style={style}
+      scrolling="no"
+      src={`https://codepen.io/c0d3codepen/embed/${id}?theme-id=${theme}&default-tab=${tabString}`}
+      frameBorder="no"
+      loading="lazy"
+      allowFullScreen={true}
+    />
+  )
+}
+
+export default CodePen
--- helpers/mdxComponents.tsx
+++ helpers/mdxComponents.tsx
@@ -5,6 +5,7 @@ import ModalImage from '../components/mdx/ModalImage'
 import styles from '../scss/mdx.module.scss'
 import Spoiler from '../components/mdx/Spoiler'
 import ChallengeBar from '../components/mdx/ChallengBar'
+import CodePen from '../components/mdx/CodePen'
 const TwoColumns: React.FC = ({ children }) => {
   return <div className={`${styles['MDX_twoColumns']}`}>{children}</div>
 }
@@ -20,6 +21,7 @@ const MDXcomponents = {
   Spoiler: Spoiler,
   twoColumns: TwoColumns,
   ChallengeBar: ChallengeBar,
+  CodePen: CodePen,
   h1: (props: any) => <h1 className={`${styles['MDX_h1']}`} {...props} />,
   h2: (props: any) => <h2 className={`${styles['MDX_h2']}`} {...props} />,
   h3: (props: any) => <h3 className={`${styles['MDX_h3']}`} {...props} />,
