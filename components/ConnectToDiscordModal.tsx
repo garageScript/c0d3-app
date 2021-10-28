9bf3a82c622f1e3c8b823f8ea0770883022ef0f3
kondanna <samannaphala@gmail.com>
2021-10-28 14:39:44 +0300

    stringifies env variable

--- components/ConnectToDiscordModal.tsx
+++ components/ConnectToDiscordModal.tsx
@@ -7,8 +7,7 @@ type ConnectToDiscordModalProps = {
   show: boolean
 }
 
-const discordConnectPage =
-  'https://discord.com/api/oauth2/authorize?client_id=845470281283665970&redirect_uri=https%3A%2F%2Fc0d3.com%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=identify%20email%20guilds.join%20gdm.join'
+const discordConnectPage = `${process.env.DISCORD_CALLBACK_URI}`
 
 const ConnectToDiscordModal: React.FC<ConnectToDiscordModalProps> = ({
   show
