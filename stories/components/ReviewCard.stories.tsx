2b5efdf2beb66cbc3dddd41b5dcf80c45dc7e1a3
Sahil <sahilkalra4321@gmail.com>
2022-02-03 04:25:07 +0000

    Fixes StoryBook errors

--- stories/components/ReviewCard.stories.tsx
+++ stories/components/ReviewCard.stories.tsx
@@ -8,47 +8,12 @@ export default {
 import { MockedProvider } from '@apollo/client/testing'
 import ACCEPT_SUBMISSION from '../../graphql/queries/acceptSubmission'
 import REJECT_SUBMISSION from '../../graphql/queries/rejectSubmission'
+import GET_PREVIOUS_SUBMISSIONS from '../../graphql/queries/getPreviousSubmissions'
 import { SubmissionStatus } from '../../graphql'
-const mocks = [
-  {
-    request: {
-      query: ACCEPT_SUBMISSION,
-      variables: { id: '1', comment: 'good job' }
-    },
-    result: {
-      data: {
-        acceptSubmission: {
-          id: '1',
-          comment: 'good job'
-        }
-      }
-    }
-  },
-  {
-    request: {
-      query: REJECT_SUBMISSION,
-      variables: {
-        id: '1',
-        comment: 'error on line 3'
-      }
-    },
-    result: {
-      data: {
-        rejectSubmission: {
-          id: '1',
-          comment: 'error on line 3'
-        }
-      }
-    }
-  }
-]
+
 const JsDiff =
   'diff --git a/js7/1.js b/js7/1.js\nindex 9c96b34..853bddf 100644\n--- a/js7/1.js\n+++ b/js7/1.js\n@@ -1,8 +1,19 @@\n-// write your code here!\n const solution = () => {\n-  // global clear all timeout:\n+  const allT = [];\n+  const old = setTimeout;\n+  window.setTimeout = (func, delay) => {\n+    const realTimeout = old(func, delay);\n+    allT.push(realTimeout);\n+    return realTimeout;\n+  };\n+  window.clearAllTimouts = () => {\n+    while (allT.length) {\n+      clearTimeout(allT.pop());\n+    }\n+  };\n   cat = () => {\n-  }\n+    window.clearAllTimouts();\n+  };\n };\n \n module.exports = solution;'
-const user = {
-  username: 'fake user',
-  id: 1,
-  isAdmin: false
-}
+
 const submissionData = {
   id: 1,
   status: SubmissionStatus.Open,
@@ -90,6 +55,60 @@ const submissionData = {
   },
   createdAt: '1524401718267',
   updatedAt: '1524401718267'
+}
+
+const mocks = [
+  {
+    request: {
+      query: ACCEPT_SUBMISSION,
+      variables: { id: '1', comment: 'good job' }
+    },
+    result: {
+      data: {
+        acceptSubmission: {
+          id: '1',
+          comment: 'good job'
+        }
+      }
+    }
+  },
+  {
+    request: {
+      query: REJECT_SUBMISSION,
+      variables: {
+        id: '1',
+        comment: 'error on line 3'
+      }
+    },
+    result: {
+      data: {
+        rejectSubmission: {
+          id: '1',
+          comment: 'error on line 3'
+        }
+      }
+    }
+  },
+  {
+    request: {
+      query: GET_PREVIOUS_SUBMISSIONS,
+      variables: {
+        challengeId: 146,
+        userId: 1
+      }
+    },
+    result: {
+      data: {
+        getPreviousSubmissions: [submissionData]
+      }
+    }
+  }
+]
+
+const user = {
+  username: 'fake user',
+  id: 1,
+  isAdmin: false
 }
 
 export const ActiveCard: React.FC = () => (
