abbca92a080fa00a95883bdea9f005e137f7bb5d
flacial <ruthemiumwithdiamond@protonmail.com>
2022-02-08 11:42:40 -0500

    Add outline Button style

--- components/theme/colors.tsx
+++ components/theme/colors.tsx
@@ -1,8 +1,11 @@
+import styles from '../../scss/__variables.module.scss'
+
 export type ColorTypes = 'white' | 'black' | 'darkgrey' | 'lightgrey'
 
 export const colors = {
   white: '#fff',
   black: '#292929',
   darkgrey: '#5d656e',
-  lightgrey: '#8e8e8e'
+  lightgrey: '#8e8e8e',
+  danger: styles.danger
 }
