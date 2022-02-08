dc3de52bb37a3c3090439f0df89f0918c4be1c1d
flacial <ruthemiumwithdiamond@protonmail.com>
2022-02-08 06:11:35 -0500

    Create deleteComment resolver

---
+++ graphql/queryResolvers/deleteComment.ts
@@ -0,0 +1,28 @@
+import prisma from '../../prisma'
+import { MutationDeleteCommentArgs } from '../../graphql'
+import { Context } from '../../@types/helpers'
+
+export const deleteComment = async (
+  _parent: void,
+  arg: MutationDeleteCommentArgs,
+  ctx: Context
+) => {
+  const { id } = arg
+
+  const authorId = ctx.req.user?.id
+  if (!authorId) throw new Error('No authorId field')
+
+  const comment = await prisma.comment.findUnique({
+    where: {
+      id
+    }
+  })
+
+  if (comment?.authorId !== authorId) throw new Error('Comment not by the user')
+
+  return prisma.comment.delete({
+    where: {
+      id
+    }
+  })
+}
