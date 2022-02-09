94517604f0f12703abc5c27be35fcdab75739582
flacial <ruthemiumwithdiamond@protonmail.com>
2022-02-09 15:06:31 -0500

    Revert "Update colors.tsx to have all variables colors"
    This reverts commit 9760f1512088d5df241b71e8a4c4fce5788e1174.

--- components/theme/colors.tsx
+++ components/theme/colors.tsx
@@ -1,16 +1,11 @@
-import colorsVars from '../../scss/__variables.module.scss'
+import styles from '../../scss/__variables.module.scss'
 
-export type ColorTypes =
-  | 'white'
-  | 'black'
-  | 'darkgrey'
-  | 'lightgrey'
-  | keyof typeof colorsVars
+export type ColorTypes = 'white' | 'black' | 'darkgrey' | 'lightgrey' | 'danger'
 
 export const colors = {
   white: '#fff',
   black: '#292929',
   darkgrey: '#5d656e',
   lightgrey: '#8e8e8e',
-  ...colorsVars
+  danger: styles.danger
 }
