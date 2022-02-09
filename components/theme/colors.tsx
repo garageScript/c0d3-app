e131a71de5478ef3d9be88654c01e192cca75cc4
flacial <ruthemiumwithdiamond@protonmail.com>
2022-02-09 15:00:46 -0500

    Revert "Update colors to dynamically set the colors types"
    This reverts commit e3e1315e0f2f0af9d2e37a728947de147c61eec9.

--- components/theme/colors.tsx
+++ components/theme/colors.tsx
@@ -1,7 +1,16 @@
 import colorsVars from '../../scss/__variables.module.scss'
 
-export type ColorTypes = keyof typeof colorsVars
+export type ColorTypes =
+  | 'white'
+  | 'black'
+  | 'darkgrey'
+  | 'lightgrey'
+  | keyof typeof colorsVars
 
 export const colors = {
+  white: '#fff',
+  black: '#292929',
+  darkgrey: '#5d656e',
+  lightgrey: '#8e8e8e',
   ...colorsVars
 }
