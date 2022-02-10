c87588a92baf4d3929fd1edb50590bbb13e977df
flacial <ruthemiumwithdiamond@protonmail.com>
2022-02-10 16:57:51 -0500

    Create CopyButton component

---
+++ components/CopyButton.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { Button } from './theme/Button'
+import { AlertIcon, CheckIcon, CopyIcon } from '@primer/octicons-react'
+import * as Sentry from '@sentry/react'
+import { ColorTypes } from './theme/colors'
+
+enum State {
+  NotCopied,
+  Copied,
+  Error
+}
+
+enum ColorType {
+  Primary = 'primary',
+  Info = 'info',
+  Success = 'success'
+}
+
+const CopyButton: React.FC<{ value: any }> = ({ value }: { value: any }) => {
+  const [copyState, setCopyState] = useState<State>(State.NotCopied)
+  const [type, setType] = useState(ColorType.Primary)
+
+  const showSuccess = () => {
+    setTimeout(() => {
+      setCopyState(State.NotCopied)
+      setType(ColorType.Primary)
+    }, 2000)
+
+    return <CheckIcon />
+  }
+
+  const handleOnClick = async () => {
+    try {
+      setType(ColorType.Primary)
+      await navigator.clipboard.writeText(value)
+      setCopyState(State.Copied)
+      setType(ColorType.Success)
+    } catch (e) {
+      setCopyState(State.Error)
+      setType(ColorType.Info)
+      Sentry.captureException(e)
+    }
+  }
+
+  return (
+    <Button
+      outline
+      type={type}
+      color={type as ColorTypes}
+      onClick={handleOnClick}
+    >
+      {copyState === State.Copied ? (
+        showSuccess()
+      ) : copyState === State.Error ? (
+        <AlertIcon />
+      ) : (
+        <CopyIcon />
+      )}
+    </Button>
+  )
+}
+
+export default CopyButton
