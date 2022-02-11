70e22cf08e55754c21794dd1901dae49836481c0
mino323 <86011419+mino323@users.noreply.github.com>
2022-02-11 16:30:48 +0100

    adds_moduleCrud

---
+++ graphql/queryResolvers/molduleCrud.ts
@@ -0,0 +1,43 @@
+import type { MutationDeleteModuleArgs, MutationAddModuleArgs } from '..'
+import prisma from '../../prisma'
+import { Context } from '../../@types/helpers'
+import { isAdminOrThrow } from '../../helpers/isAdmin'
+import type { Module } from '@prisma/client'
+
+export const modules = (): Promise<Module[]> => {
+  return prisma.module.findMany({
+    include: {
+      author: true,
+      lesson: true
+    }
+  })
+}
+
+export const addModule = (
+  _parent: void,
+  args: MutationAddModuleArgs,
+  ctx: Context
+): Promise<Module> => {
+  const { req } = ctx
+  isAdminOrThrow(req)
+  const { content, lessonId, name } = args
+  const authorId = req.user?.id
+  if (!authorId) throw new Error('No User')
+  return prisma.module.create({
+    data: { authorId, content, lessonId, name }
+  })
+}
+
+export const deleteModule = async (
+  _parent: void,
+  arg: MutationDeleteModuleArgs,
+  ctx: Context
+) => {
+  const { req } = ctx
+  isAdminOrThrow(req)
+  const authorId = req.user?.id
+  if (!authorId) throw new Error('No User')
+  const { id } = arg
+  await prisma.module.delete({ where: { id } })
+  return { success: true }
+}
