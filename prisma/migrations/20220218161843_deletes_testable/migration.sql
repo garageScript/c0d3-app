dd386bc56bb799ed6920950733aac21233b70c46
mino323 <86011419+mino323@users.noreply.github.com>
2022-02-18 17:20:03 +0100

    drops column testable

---
+++ prisma/migrations/20220218161843_deletes_testable/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `testable` on the `exercises` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "exercises" DROP COLUMN "testable";
