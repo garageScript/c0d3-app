315b35d7edc401e4545a5fe313113030a9ac89a0
flacial <ruthemiumwithdiamond@protonmail.com>
2022-03-02 22:04:21 +0400

    Init next-auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import NextAuth from 'next-auth'
+// import DiscordProvider from 'next-auth/providers/discord'
+
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  NextAuth(req, res, {
+    providers: [
+      // Commented to describe what provider it'll have
+      // DiscordProvider({
+      //   clientId: process.env.DISCORD_CLIENT_ID,
+      //   clientSecret: process.env.DISCORD_CLIENT_SECRET
+      // })
+    ]
+  })
