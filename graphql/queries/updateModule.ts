673ac51e43be35c7a3e125a26cfd0060a5104d83
mino323 <86011419+mino323@users.noreply.github.com>
2022-03-18 11:12:18 +0200

    change front end module queries

---
+++ graphql/queries/updateModule.ts
@@ -0,0 +1,16 @@
+import { gql } from '@apollo/client'
+
+const UPDATE_MODULE = gql`
+  mutation updateModule(id: Int!, $lessonId: Int!, $name: String!, $content: String!) {
+    updateModule( id: $id, lessonId: $lessonId, name: $name, content: $content) {
+      id
+      name
+      content
+      lesson {
+        title
+      }
+    }
+  }
+`
+
+export default UPDATE_MODULE
