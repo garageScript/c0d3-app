d409a6fed6a17dcd0aca8634e432148c9ae5236b
flacial <ruthemiumwithdiamond@protonmail.com>
2022-03-24 17:21:05 +0400

    Update story

--- stories/components/AdminLessonNav.stories.tsx
+++ stories/components/AdminLessonNav.stories.tsx
@@ -32,3 +32,76 @@ export const Basic: React.FC = () => {
     />
   )
 }
+
+export const WithManyItems: React.FC = () => {
+  return (
+    <AdminLessonNav
+      navItems={[
+        {
+          value: 'Betrouka'
+        },
+        {
+          value: 'Agwamvi'
+        },
+        {
+          value: 'Tekrupbez'
+        },
+        {
+          value: 'Hopebam'
+        },
+        {
+          value: 'Wezwowpoj'
+        },
+        {
+          value: 'Zokdiwju'
+        },
+        {
+          value: 'Hesculnu'
+        },
+        {
+          value: 'Odvipjih'
+        },
+        {
+          value: 'Hebrakbi'
+        },
+        {
+          value: 'Utfaana'
+        },
+        {
+          value: 'Ihieconus'
+        },
+        {
+          value: 'Paheze'
+        },
+        {
+          value: 'Upanwe'
+        },
+        {
+          value: 'Dewtaiha'
+        }
+      ]}
+      tabs={[
+        () => (
+          <>
+            <h1>Some text</h1>
+            <p>Some paragraph that makes sense</p>
+            <small>Small text for the vibes</small>
+          </>
+        ),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add'),
+        fn('Modules with delete and add')
+      ]}
+    />
+  )
+}
