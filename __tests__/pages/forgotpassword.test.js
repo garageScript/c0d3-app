9ddc0291f748385dc71fdc36f6900ba568855a5b
flacial <ruthemiumwithdiamond@protonmail.com>
2022-04-05 04:57:02 +0400

    test: mock next-auth in forgotpassword

--- __tests__/pages/forgotpassword.test.js
+++ __tests__/pages/forgotpassword.test.js
@@ -1,3 +1,4 @@
+import '../../__mocks__/next-auth/nextAuthAPI.mock'
 import React from 'react'
 import { GraphQLError } from 'graphql'
 import { render, fireEvent, waitFor } from '@testing-library/react'
