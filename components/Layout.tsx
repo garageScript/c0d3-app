43855dd907863075a78bfcd6228f3ae7d4c555c7
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-05-13 13:41:25 +0400

    feat: Redesign Layout component
    Add margin-top and background colors

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,15 +3,21 @@ import AppNav from './AppNav'
 import Footer from './Footer'
 import Head from 'next/head'
 import type { LayoutGetter } from '../@types/page'
-const Layout: React.FC<{ title?: string }> = ({ children, title }) => (
-  <>
+import styles from '../scss/layout.module.scss'
+
+const Layout: React.FC<{ title?: string; bg?: 'white' | 'light-gray' }> = ({
+  children,
+  title,
+  bg
+}) => (
+  <main className={styles[bg === 'white' ? 'white' : 'light-gray']}>
     <Head>
       <title key="title">{title ? `${title} — C0D3` : 'C0D3'}</title>
     </Head>
     <AppNav />
-    <div className="container-md">{children}</div>
+    <div className={`container-md ${styles.container}`}>{children}</div>
     <Footer />
-  </>
+  </main>
 )
 
 export const getLayout: LayoutGetter = page => <Layout>{page}</Layout>
