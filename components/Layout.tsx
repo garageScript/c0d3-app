7f79ca529f570d6222840934a7e1b6601076ee02
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-05-18 16:27:55 +0400

    refactor: Return Fragment element

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,14 +6,14 @@ import type { LayoutGetter } from '../@types/page'
 import styles from '../scss/layout.module.scss'
 
 const Layout: React.FC<{ title?: string }> = ({ children, title }) => (
-  <main>
+  <>
     <Head>
       <title key="title">{title ? `${title} — C0D3` : 'C0D3'}</title>
     </Head>
     <AppNav />
     <div className={`container-md ${styles.container}`}>{children}</div>
     <Footer />
-  </main>
+  </>
 )
 
 export const getLayout: LayoutGetter = page => <Layout>{page}</Layout>
