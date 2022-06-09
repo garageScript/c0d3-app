06d93a8418a9c545dc6f7c3a3002a6e184041285
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-06-09 23:34:37 +0400

    chore: Add missing lesson prop

--- stories/components/Breadcrumbs.stories.tsx
+++ stories/components/Breadcrumbs.stories.tsx
@@ -8,8 +8,8 @@ export default {
 }
 
 const lessons = [
-  { title: 'Foundations of JavaScript', id: 1 },
-  { title: 'Variables', id: 2 }
+  { title: 'Foundations of JavaScript', id: 1, slug: 'js0' },
+  { title: 'Variables', id: 2, slug: 'js0' }
 ] as GetAppQuery['lessons']
 
 const parameters = {
@@ -21,7 +21,8 @@ const parameters = {
 export const Basic = () => {
   const [lesson, setLesson] = useState({
     title: lessons[0].title,
-    id: lessons[0].id
+    id: lessons[0].id,
+    slug: lessons[0].slug
   })
 
   return (
@@ -40,7 +41,8 @@ Basic.parameters = parameters
 export const WithOmitHomeRoute = () => {
   const [lesson, setLesson] = useState({
     title: lessons[0].title,
-    id: lessons[0].id
+    id: lessons[0].id,
+    slug: lessons[0].slug
   })
 
   return (
