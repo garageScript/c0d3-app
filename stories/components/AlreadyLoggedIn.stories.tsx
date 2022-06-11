fd3caf4b98c4fff4077cc13aa1150119190091dc
HS-90 <77421872+HS-90@users.noreply.github.com>
2022-06-11 19:01:12 -0400

    story for AlreadyLoggedIn component

---
+++ stories/components/AlreadyLoggedIn.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AlreadyLoggedIn from '../../components/AlreadyLoggedIn'
+
+export default {
+  component: AlreadyLoggedIn,
+  title: 'Components/AlreadyLoggedIn'
+}
+
+export const Basic: React.FC = () => <AlreadyLoggedIn />
