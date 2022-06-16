68b350f19ae4976658c55e7f7173f6963dc6c493
HS-90 <77421872+HS-90@users.noreply.github.com>
2022-06-15 22:21:18 -0400

    AdminLessonCard story

---
+++ stories/components/AdminLessonCard.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import AdminLessonCard from '../../components/admin/lessons/AdminLessonCard'
+
+export default {
+    title: 'Components/AdminLessonCard',
+    component: AdminLessonCard
+}
+
+const props = {
+    heading: 'Foundations of Javascript',
+    description:'A super simple introduction to help you get started!',
+    pendingFlaggedQuestions: 3,
+    width:'35%',
+    editLessonUrl:'_blank'
+}
+
+export const Basic = () => {
+    return (
+        <AdminLessonCard 
+          heading = {props.heading}
+          description = {props.description}
+          pendingFlaggedQuestions = {props.pendingFlaggedQuestions}
+          width = {props.width}
+          editLessonUrl = {props.editLessonUrl}
+        />
+    )
+}
