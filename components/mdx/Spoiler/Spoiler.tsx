e0626ee43818179ddce71197d387c2fd8df523fe
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-07-14 21:44:16 +0400

    feat: Create Spoiler component folder
    Related to https://github.com/garageScript/c0d3-app/issues/2080

--- components/mdx/Spoiler/Spoiler.tsx
+++ components/mdx/Spoiler/Spoiler.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import styles from './spoiler.module.scss'
+
 type Props = {
   name?: string
 }
 const Spoiler: React.FC<Props> = ({ children, name }) => {
   return (
-    <details className="mb-4">
+    <details className="mb-3">
       <summary>{name || 'Answer'}</summary>
-      <div className="ms-4">{children}</div>
+      <div className={styles.content}>{children}</div>
     </details>
   )
 }
