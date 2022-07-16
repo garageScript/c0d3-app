b63980c5c4d5a2f23aa7c8d6286059ba7dd6a113
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-07-16 18:58:44 +0400

    Revert "Create AdditionalResources component folder"
    This reverts commit 70a28f8dc957f124301123de43026c3d6b7036a2.

--- components/AdditionalResources.tsx
+++ components/AdditionalResources.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavLink from '../NavLink'
-import styles from './additionalResources.module.scss'
+import NavLink from './NavLink'
+import styles from '../scss/additionalResources.module.scss'
 
 const AdditionalResources: React.FC = () => {
   return (
