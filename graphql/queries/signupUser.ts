30449195613f8700646b077aa83d16fdd4087420
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-07-24 03:55:01 +0400

    feat: Update signup mutation to take password

--- graphql/queries/signupUser.ts
+++ graphql/queries/signupUser.ts
@@ -6,12 +6,14 @@ const SIGNUP_USER = gql`
     $lastName: String!
     $email: String!
     $username: String!
+    $password: String!
   ) {
     signup(
       firstName: $firstName
       lastName: $lastName
       email: $email
       username: $username
+      password: $password
     ) {
       success
       username
