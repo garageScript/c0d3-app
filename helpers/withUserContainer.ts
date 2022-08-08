9db4c6be8888ded9ef23737da506b967b3d79c0c
Harry Sun <yhsun21@gmail.com>
2022-08-08 17:21:07 -0400

    modified exercise and module Crud tests to work

--- helpers/withUserContainer.ts
+++ helpers/withUserContainer.ts
@@ -4,9 +4,11 @@ export const withUserContainer =
   <Type, ArgsType>(
     resolver: (_parent: void, args: ArgsType, ctx: Context) => Type
   ) =>
-  (_parent: void, args: ArgsType, ctx: Context) => {
+  async (_parent: void, args: ArgsType, ctx: Context) => {
     const { req } = ctx
+
     const authorId = req.user?.id
+
     if (!authorId) throw new Error('No user')
 
     return resolver(_parent, args, ctx)
