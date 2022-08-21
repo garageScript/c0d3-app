3f92d363b3400c122ebf216377942e273f6a5e39
Harry Sun <yhsun21@gmail.com>
2022-08-21 10:59:30 -0400

    remove undefined from lodash get

--- containers/withUserContainer.ts
+++ containers/withUserContainer.ts
@@ -8,7 +8,7 @@ export const withUserContainer =
   async (_parent: void, args: ArgsType, ctx: Context) => {
     const { req } = ctx
 
-    const authorId = _.get(req, 'user.id', undefined)
+    const authorId = _.get(req, 'user.id')
     if (!authorId) throw new Error('No user')
 
     return resolver(_parent, args, ctx)
