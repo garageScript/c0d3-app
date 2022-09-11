786a27a4e9672054ceb96ca654334fd431e5888d
Bryan Jennings <bryanjenningz@gmail.com>
2022-09-11 15:53:53 -0700

    Move ExercisePreviewCard with styling into the same folder

--- components/ExercisePreviewCard/ExercisePreviewCard.tsx
+++ components/ExercisePreviewCard/ExercisePreviewCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../scss/exercisePreviewCard.module.scss'
+import styles from './exercisePreviewCard.module.scss'
 
 export type ExercisePreviewCardProps = {
   moduleName: string
