0554ed99cfd3522f6a64cf2b821cc29e38b9266d
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-09-16 18:17:33 +0400

    fix: Discord buttons appear for everyone

--- components/ProfileImageInfo.tsx
+++ components/ProfileImageInfo.tsx
@@ -17,6 +17,7 @@ import { Button as BsButton } from 'react-bootstrap'
 import { useMutation } from '@apollo/client'
 import { UNLINK_DISCORD } from '../graphql/queries/unlinkDiscord'
 import { useRouter } from 'next/router'
+import { useGetAppQuery } from '../graphql'
 
 type DiscordModalProps = {
   show: boolean
@@ -70,6 +71,11 @@ type ProfileImageInfoProps = {
 }
 
 const ProfileImageInfo: React.FC<ProfileImageInfoProps> = ({ user }) => {
+  const { data } = useGetAppQuery()
+
+  const username = data?.session.user?.username
+  const isUserProfile = username && username === user.username
+
   const [unlinkDiscord] = useMutation(UNLINK_DISCORD)
   const router = useRouter()
 
@@ -126,6 +132,83 @@ const ProfileImageInfo: React.FC<ProfileImageInfoProps> = ({ user }) => {
     </>
   )
 
+  const DiscordDetails = () => {
+    if (isUserProfile) {
+      return user.discordUserId ? (
+        <>
+          <div className="d-flex align-items-center mb-2">
+            <Image
+              src="/assets/discordClydeLogo.svg"
+              height={20}
+              width={20}
+              objectFit="contain"
+            />
+            <span className="fs-5 ms-2 mb-0">
+              <Link
+                href={`https://discordapp.com/users/${user.discordUserId}/`}
+              >
+                {user.discordUsername}
+              </Link>
+            </span>
+          </div>
+          <Button onClick={handleShow} type="danger" outline color="danger">
+            Unlink Discord
+          </Button>
+          <DiscordModal
+            show={show}
+            handleClose={handleClose}
+            handleOnClick={handleUnlinkClick}
+            title={'Unlink Discord'}
+            Body={UnlinkDiscordBody}
+            btnText={'Unlink'}
+            btnVariant={'danger'}
+            textId={'unlink-discord-btn'}
+          />
+        </>
+      ) : (
+        <>
+          <button
+            type="button"
+            className="btn btn-primary btn-md btn-block mt-3"
+            onClick={handleShow}
+          >
+            Connect to Discord
+          </button>
+          <DiscordModal
+            show={show}
+            handleClose={handleClose}
+            handleOnClick={handleConnectClick}
+            title={'Connect to Discord'}
+            Body={ConnectToDiscordBody}
+            btnText={'Connect to Discord'}
+            btnVariant={'primary'}
+            textId={'connect-discord-btn'}
+          />
+        </>
+      )
+    }
+
+    if (user.discordUserId) {
+      return (
+        <div className="d-flex align-items-center mb-2">
+          <Image
+            src="/assets/discordClydeLogo.svg"
+            height={20}
+            width={20}
+            objectFit="contain"
+          />
+          <span className="fs-5 ms-2 mb-0">
+            <Link href={`https://discordapp.com/users/${user.discordUserId}/`}>
+              {user.discordUsername}
+            </Link>
+          </span>
+        </div>
+      )
+    }
+
+    return <></>
+  }
+
   return (
     <div className="card shadow-sm">
       <div
@@ -140,58 +223,7 @@ const ProfileImageInfo: React.FC<ProfileImageInfoProps> = ({ user }) => {
         <span className="fs-5 text-muted">{`@${user.username}`}</span>
       </div>
       <div className="d-flex ms-auto me-auto mb-4 mt-3 flex-column">
-        {user.discordUserId ? (
-          <>
-            <div className="d-flex align-items-center mb-2">
-              <Image
-                src="/assets/discordClydeLogo.svg"
-                height={20}
-                width={20}
-                objectFit="contain"
-              />
-              <span className="fs-5 ms-2 mb-0">
-                <Link
-                  href={`https://discordapp.com/users/${user.discordUserId}/`}
-                >
-                  {user.discordUsername}
-                </Link>
-              </span>
-            </div>
-            <Button onClick={handleShow} type="danger" outline color="danger">
-              Unlink Discord
-            </Button>
-            <DiscordModal
-              show={show}
-              handleClose={handleClose}
-              handleOnClick={handleUnlinkClick}
-              title={'Unlink Discord'}
-              Body={UnlinkDiscordBody}
-              btnText={'Unlink'}
-              btnVariant={'danger'}
-              textId={'unlink-discord-btn'}
-            />
-          </>
-        ) : (
-          <>
-            <button
-              type="button"
-              className="btn btn-primary btn-md btn-block mt-3"
-              onClick={handleShow}
-            >
-              Connect to Discord
-            </button>
-            <DiscordModal
-              show={show}
-              handleClose={handleClose}
-              handleOnClick={handleConnectClick}
-              title={'Connect to Discord'}
-              Body={ConnectToDiscordBody}
-              btnText={'Connect to Discord'}
-              btnVariant={'primary'}
-              textId={'connect-discord-btn'}
-            />
-          </>
-        )}
+        <DiscordDetails />
       </div>
     </div>
   )
