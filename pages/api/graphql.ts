f30c846e39858b3df9232494da14bdfe31c85b85
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-09-27 02:13:49 +0400

    feat(test-2): Enable playground for preview deployments

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -9,7 +9,7 @@ import userMiddleware from '../../helpers/middleware/user'
 import sessionMiddleware from '../../helpers/middleware/session'
 
 // VERCEL_ENV is a reserved env key by Vercel that specify the deployment type: "preview", "production", or "deployment"
-const isPreview = process.env.VERCEL_ENV === 'preview'
+const isPreview = process.env.VERCEL_ENV !== 'preview'
 
 const handler = nextConnect()
 const apolloServer = new ApolloServer({
