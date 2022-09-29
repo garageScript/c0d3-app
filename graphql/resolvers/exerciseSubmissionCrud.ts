499013be0a179783086fb0813a5d0a40320411b7
Bryan Jennings <bryanjenningz@gmail.com>
2022-09-29 11:27:53 -0700

    Filter exercise submissions so user only gets their exercise submissions

--- graphql/resolvers/exerciseSubmissionCrud.ts
+++ graphql/resolvers/exerciseSubmissionCrud.ts
@@ -1,10 +1,23 @@
+import { Context } from '../../@types/helpers'
 import prisma from '../../prisma'
 
-export const exerciseSubmissions = () => {
+export const exerciseSubmissions = (
+  _parent: void,
+  _args: void,
+  context: Context
+) => {
+  const userId = context.req.user?.id
+  if (!userId) return []
+
   return prisma.exerciseSubmission.findMany({
     include: {
       exercise: true,
       user: true
+    },
+    where: {
+      user: {
+        id: userId
+      }
     }
   })
 }
