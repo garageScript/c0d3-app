a1aff91ba8c71d2f0d2f2f507f3496ecb9aed151
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-11-23 16:19:49 +0400

    refactor: Set password with confirm-token provider

--- __tests__/pages/reset.test.js
+++ __tests__/pages/reset.test.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { render, fireEvent, waitFor, act } from '@testing-library/react'
 import { MockedProvider } from '@apollo/client/testing'
 import GET_APP from '../../graphql/queries/getApp'
-import UPDATE_PASSWORD from '../../graphql/queries/updatePassword'
 import ResetPassword from '../../pages/confirm/[token]'
 import { useRouter } from 'next/router'
+import { signIn } from 'next-auth/react'
 
 describe('ResetPassword Page', () => {
   const { query } = useRouter()
   const fakeToken = 'fake token'
   query['token'] = fakeToken
+
   const fillOutResetForm = (getByTestId, password) => {
     const passwordField = getByTestId('password')
     const confirmPasswordField = getByTestId('confirmPassword')
@@ -27,37 +28,27 @@ describe('ResetPassword Page', () => {
     })
   }
 
-  test('Should render confirm success on success', async () => {
-    const fakeResetPassword = 'fake reset password'
-
-    const mocks = [
-      {
-        request: { query: GET_APP },
-        result: {
-          data: {
-            session: null,
-            lessons: [],
-            alerts: []
-          }
-        }
-      },
-      {
-        request: {
-          query: UPDATE_PASSWORD,
-          variables: {
-            token: fakeToken,
-            password: fakeResetPassword
-          }
-        },
-        result: {
-          data: {
-            changePw: {
-              success: true
-            }
-          }
+  const mocks = [
+    {
+      request: { query: GET_APP },
+      result: {
+        data: {
+          session: null,
+          lessons: [],
+          alerts: []
         }
       }
-    ]
+    }
+  ]
+
+  test('Should render confirm success on success', async () => {
+    signIn.mockReturnValue({
+      ok: true,
+      error: '',
+      status: 200
+    })
+
+    const fakeResetPassword = 'fake reset password'
 
     const { container, getByTestId, getByText } = render(
       <MockedProvider mocks={mocks} addTypename={false}>
@@ -77,31 +68,39 @@ describe('ResetPassword Page', () => {
     })
   })
 
-  test('Should render alert on error', async () => {
+  test('Should display server error on 401 status code', async () => {
+    signIn.mockReturnValue({
+      ok: false,
+      error: '',
+      status: 401
+    })
+
     const fakeResetPassword = 'fake reset password'
 
-    const mocks = [
-      {
-        request: { query: GET_APP },
-        result: {
-          data: {
-            session: null,
-            lessons: [],
-            alerts: []
-          }
-        }
-      },
-      {
-        request: {
-          query: UPDATE_PASSWORD,
-          variables: {
-            token: fakeToken,
-            password: fakeResetPassword
-          }
-        },
-        error: new Error('invalid token')
-      }
-    ]
+    const { container, getByTestId, getByText } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <ResetPassword />
+      </MockedProvider>
+    )
+
+    const submitButton = getByTestId('submit')
+
+    fillOutResetForm(getByTestId, fakeResetPassword)
+    fireEvent.click(submitButton)
+
+    await waitFor(() => {
+      expect(getByText('Internal server error')).toBeTruthy()
+      expect(container).toMatchSnapshot()
+    })
+  })
+
+  test('Should render alert on error', async () => {
+    signIn.mockReturnValue({
+      ok: false,
+      error: 'Invalid token'
+    })
+
+    const fakeResetPassword = 'fake reset password'
 
     const { container, getByTestId, getByText } = render(
       <MockedProvider mocks={mocks} addTypename={false}>
