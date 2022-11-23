18123fb055522a45ab87618d60d710ec0cdbb6ae
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-11-23 16:19:18 +0400

    feat: Create the provider to set user password
    When next-auth handles setting the user password, it can set the session.

--- helpers/nextAuth.test.js
+++ helpers/nextAuth.test.js
@@ -3,18 +3,30 @@ jest.mock('./middleware/user')
 jest.mock('./middleware/session')
 jest.mock('./middleware/logger')
 jest.mock('../graphql/resolvers/authController.ts')
+jest.mock('../graphql/resolvers/passwordController')
+jest.mock('./encoding')
+
 import { updateRefreshandAccessTokens } from './discordAuth'
 import prismaMock from '../__tests__/utils/prismaMock'
-import { authorize, providers, jwt, session, signIn } from './nextAuth'
+import {
+  authorize,
+  providers,
+  jwt,
+  session,
+  signIn,
+  authorizeEmailVerification
+} from './nextAuth'
 import loggingMiddleware from './middleware/logger'
 import sessionMiddleware from './middleware/session'
 import userMiddleware from './middleware/user'
 import { getUserSession } from './getUserSession'
 import { login, signup } from '../graphql/resolvers/authController'
+import { changePw } from '../graphql/resolvers/passwordController'
+import { decode } from './encoding'
 
 const partialCredentials = { username: 'noob', password: 'noob123' }
 const credentials = {
-  ...partialCredentials,
+  username: 'noob',
   firstName: 'noob',
   lastName: 'pro',
   email: 'noob@gmail.com'
@@ -38,7 +50,11 @@ describe('Providers', () => {
 
     const value = providers(req, res)
     const hasProviders =
-      value[0].id === 'discord' && value[1].id === 'credentials'
+      value[0].id === 'discord' &&
+      value[1].id === 'credentials' &&
+      value[2].options.id === 'confirm-token'
+
+    value[2]
 
     expect(hasProviders).toBeTruthy()
   })
@@ -48,6 +64,7 @@ describe('Signin callback', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
+
   it('Should return true when provider is not discord', async () => {
     expect.assertions(1)
     const signInCallback = signIn(req, res)
@@ -64,39 +81,13 @@ describe('Signin callback', () => {
     expect(value).toBe(true)
   })
 
-  it('Should return false if account does not exist', async () => {
+  it('Should cancel signIn flow if account does not exist', async () => {
     expect.assertions(1)
-
-    userMiddleware.mockImplementation((req, _res, next) => {
-      req.user = {
-        id: 123,
-        username: 'fakeUser'
-      }
-      next()
-    })
-
     const signInCallback = signIn(req, res)
 
     const value = await signInCallback({
-      account: null,
       user: {
         id: 123
-      }
-    })
-
-    expect(value).toBe(false)
-  })
-
-  it('Should return false if user object has email and no id', async () => {
-    expect.assertions(1)
-    const signInCallback = signIn(req, res)
-
-    const value = await signInCallback({
-      account: {
-        provider: 'credentials'
-      },
-      user: {
-        email: 'noob@c0d3.com'
       }
     })
 
@@ -126,7 +117,7 @@ describe('Signin callback', () => {
       })
 
       expect(updateRefreshandAccessTokens).toBeCalled()
-      expect(value).toBe('/discord/success')
+      expect(value).toBe(true)
     })
   })
 
@@ -154,7 +145,7 @@ describe('Signin callback', () => {
         }
       })
 
-      expect(value).toBe('/curriculum')
+      expect(value).toBe(true)
     })
 
     it('Should redirect to /discord/404user when user is not found', async () => {
@@ -217,7 +208,7 @@ describe('Signin callback', () => {
 })
 
 describe('JWT callback', () => {
-  it('Should set user in token', () => {
+  it('Should set user in token if flow type is credentials (login/signup)', async () => {
     expect.assertions(1)
 
     const options = {
@@ -226,15 +217,16 @@ describe('JWT callback', () => {
       },
       user: {
         id: 1
-      }
+      },
+      account: { type: 'credentials' }
     }
 
-    const value = jwt(options)
+    const value = await jwt(options)
 
     expect(value.user.id).toBe(1)
   })
 
-  it('Should not set user in token if there is no user', () => {
+  it('Should not set user in token if there is no user', async () => {
     expect.assertions(1)
 
     const options = {
@@ -246,14 +238,32 @@ describe('JWT callback', () => {
       }
     }
 
-    const value = jwt({ ...options, user: null })
+    const value = await jwt({ ...options, user: null })
 
     expect(value.user).toBeFalsy()
+  })
+
+  it('Should set user from database as token user if flow type is oauth (discord)', async () => {
+    expect.assertions(1)
+
+    const options = {
+      token: {
+        user: null
+      },
+      user: {
+        id: 1
+      },
+      account: { type: 'oauth' }
+    }
+
+    await jwt(options)
+
+    expect(prismaMock.user.findFirst).toBeCalled()
   })
 })
 
 describe('Session callback', () => {
-  it('Should set session.user to token.user', async () => {
+  it("Should set session's user to token's user", async () => {
     expect.assertions(1)
 
     const options = {
@@ -267,9 +277,26 @@ describe('Session callback', () => {
       }
     }
 
+    prismaMock.user.findFirst.mockResolvedValue({ id: options.token.user.id })
+
     const value = await session(options)
 
     expect(value.user.id).toBe(1)
+  })
+
+  it("Should not set session's user to token's user if the token's user does not exist", async () => {
+    expect.assertions(1)
+
+    const options = {
+      token: {},
+      session: {
+        user: null
+      }
+    }
+
+    const value = await session(options)
+
+    expect(value.user).toBe(null)
   })
 })
 
@@ -295,15 +322,83 @@ describe('Authorize callback', () => {
     signup.mockImplementation(() => ({ id }))
     const user = await authorize(req, res)(credentials)
 
-    expect(user.id).toBe(id)
+    expect(user).toStrictEqual({})
   })
 
-  test('Should return null', async () => {
+  test('Should cancel flow if login/signup failed', async () => {
     expect.assertions(1)
 
-    signup.mockImplementation(() => null)
-    const user = await authorize(req, res)(credentials)
+    const id = 123
+    prismaMock.user.findFirst.mockResolvedValue({ id })
+
+    login.mockResolvedValue(undefined)
+    const user = await authorize(req, res)(partialCredentials)
 
     expect(user).toBe(null)
+  })
+})
+
+describe('Confirm token authorize callback', () => {
+  const credentials = {
+    password: 'password',
+    token: 'eyJ1c2VySWQiOjksInVzZXJUb2tlbiI6IkZCeFBTbkRfc3o5WldKVmV5NWpk1111'
+  }
+  test('Should throw error if token or password are not present', async () => {
+    expect.assertions(1)
+
+    expect(
+      authorizeEmailVerification({ password: '', token: '' })
+    ).rejects.toStrictEqual(new Error('Missing token or password'))
+  })
+
+  test('Should change password successfully', async () => {
+    expect.assertions(1)
+
+    prismaMock.user.findFirst.mockResolvedValue({
+      id: 123
+    })
+
+    decode.mockResolvedValue({
+      userId: 123
+    })
+    changePw.mockResolvedValue({
+      success: true
+    })
+
+    expect(authorizeEmailVerification(credentials)).resolves.toStrictEqual({
+      id: 123
+    })
+  })
+
+  test('Should return null if user from db is invalid', async () => {
+    expect.assertions(1)
+
+    prismaMock.user.findFirst.mockResolvedValue(null)
+
+    decode.mockResolvedValue({
+      userId: 123
+    })
+    changePw.mockResolvedValue({
+      success: true
+    })
+
+    expect(authorizeEmailVerification(credentials)).resolves.toBe(null)
+  })
+
+  test('Should handle promise reject', async () => {
+    expect.assertions(1)
+
+    prismaMock.user.findFirst.mockResolvedValue({
+      id: 123
+    })
+
+    decode.mockResolvedValue({
+      userId: 123
+    })
+    changePw.mockRejectedValue(new Error('Invalid token'))
+
+    expect(authorizeEmailVerification(credentials)).rejects.toStrictEqual(
+      new Error('Invalid token')
+    )
   })
 })
--- helpers/nextAuth.ts
+++ helpers/nextAuth.ts
@@ -1,29 +1,51 @@
 import { updateRefreshandAccessTokens } from './discordAuth'
-import { Awaitable, CallbacksOptions, DefaultSession, User } from 'next-auth'
+import { Awaitable, CallbacksOptions, Session, User } from 'next-auth'
 import { LoggedRequest } from '../@types/helpers'
 import { Request, Response } from 'express'
 import { NextApiResponse } from 'next'
 import { getUserSession } from './getUserSession'
-import { get } from 'lodash'
+import { toNumber, toString } from 'lodash'
 import { login, signup } from '../graphql/resolvers/authController'
 import prisma from '../prisma'
 import DiscordProvider from 'next-auth/providers/discord'
-import CredentialsProvider, {
-  CredentialsConfig
-} from 'next-auth/providers/credentials'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { JWT } from 'next-auth/jwt'
+import { changePw } from '../graphql/resolvers/passwordController'
+import { decode } from './encoding'
+import { GraphQLError } from 'graphql'
+import { User as GraphqlUser } from '../graphql'
 
-export const authorize = (
-  req: LoggedRequest & Request,
-  res: NextApiResponse & Response
-) => {
-  const authorize: CredentialsConfig['authorize'] = async credentials => {
+type Credentials =
+  | Record<'username' | 'password' | 'email' | 'firstName' | 'lastName', string>
+  | undefined
+type TokenCredentials = Record<'token' | 'password', string> | undefined
+
+type UserData = Pick<
+  GraphqlUser,
+  'id' | 'username' | 'name' | 'isAdmin' | 'discordId'
+>
+
+const excludeUserData = (user: UserData | null) => {
+  return {
+    id: user?.id,
+    username: user?.username,
+    name: user?.name,
+    isAdmin: user?.isAdmin,
+    discordId: user?.discordId
+  }
+}
+const throwError = (str: string) => Promise.reject(new Error(str))
+
+export const authorize =
+  (req: LoggedRequest & Request, res: NextApiResponse & Response) =>
+  async (credentials: Credentials) => {
     const context = { req, res }
 
-    const username = get(credentials, 'username')
-    const password = get(credentials, 'password')
-    const email = get(credentials, 'email')
-    const firstName = get(credentials, 'firstName')
-    const lastName = get(credentials, 'lastName')
+    const username = credentials?.username
+    const password = credentials?.password
+    const email = credentials?.email
+    const firstName = credentials?.firstName
+    const lastName = credentials?.lastName
 
     const isSignUpFlow = username && email && firstName && lastName
     const isLoginFlow = username && password
@@ -47,13 +69,40 @@ export const authorize = (
 
     if (!user) return null
 
-    // What we return here is passed to jwt and session callbacks
-    return prisma.user.findFirst({
+    const dbUser = (await prisma.user.findFirst({
       where: { id: user.id }
-    }) as Awaitable<User | null>
-  }
+    })) as Awaitable<User | null>
 
-  return authorize
+    return dbUser
+  }
+export const authorizeEmailVerification = async (
+  credentials: TokenCredentials
+) => {
+  try {
+    const password = credentials?.password
+    const token = credentials?.token
+
+    if (!password || !token) return throwError('Missing token or password')
+
+    const { userId } = decode(token)
+
+    await changePw(undefined, {
+      password: credentials?.password,
+      token: credentials?.token
+    })
+
+    const dbUser = (await prisma.user.findFirst({
+      where: {
+        id: userId
+      }
+    })) as Awaitable<User | null>
+
+    if (!dbUser) return null
+
+    return dbUser
+  } catch (err) {
+    return throwError((err as GraphQLError).message)
+  }
 }
 
 export const providers = (
@@ -74,6 +123,15 @@ export const providers = (
       lastName: { label: 'Last name', type: 'text' }
     },
     authorize: authorize(req, res)
+  }),
+  CredentialsProvider({
+    id: 'confirm-token',
+    name: 'Verify token',
+    credentials: {
+      token: { label: 'Token', type: 'text' },
+      password: { label: 'Password', type: 'password' }
+    },
+    authorize: authorizeEmailVerification
   })
 ]
 
@@ -83,17 +141,23 @@ export const signIn = (
 ) => {
   const signIn: CallbacksOptions['signIn'] = async ({ account, user }) => {
     if (!account) return false
-    if ('email' in user && !('id' in user)) return false
 
     const { provider } = account
 
     if (provider === 'discord') {
       const c0d3User = await getUserSession(req, res)
-
       const { access_token, expires_at, refresh_token } = account
 
       // Connect to discord
       if (c0d3User?.id) {
+        const differentAccountConnected = await prisma.user.findFirst({
+          where: {
+            discordId: user.id
+          }
+        })
+
+        if (differentAccountConnected) return '/discord/success&error=connected'
+
         await updateRefreshandAccessTokens(
           c0d3User.id,
           user.id,
@@ -102,8 +166,7 @@ export const signIn = (
           new Date(expires_at! * 1000)
         )
 
-        // Cancel auth flow. Session won't be created
-        return '/discord/success'
+        return true
       }
 
       // Login with Discord
@@ -114,10 +177,7 @@ export const signIn = (
       })
 
       if (userInfo) {
-        req.session.userId = userInfo.id
-
-        // Cancel auth flow. Session won't be created
-        return '/curriculum'
+        return true
       }
 
       return '/discord/404user'
@@ -129,16 +189,50 @@ export const signIn = (
   return signIn
 }
 
-// jwt callback is first called then session callback
-export const jwt: CallbacksOptions['jwt'] = ({ token, user }) => {
-  if (user) token.user = user
+// on getSession/useSession, jwt callback is first called then session callback
+// what get returned is what gets set as the JWT token content
+export const jwt: CallbacksOptions['jwt'] = async ({
+  token,
+  user,
+  account
+}) => {
+  // for Discord. will only run once.
+  if (account?.type === 'oauth' && user) {
+    const dbUser = await prisma.user.findFirst({
+      where: {
+        discordId: toString(user.id)
+      }
+    })
+
+    token.user = { id: dbUser?.id }
+  }
+
+  // for Credentials provider or Login/Signup flows. will only run once
+  if (account?.type === 'credentials') {
+    token.user = { id: user?.id }
+  }
+
   return token
 }
 
+// will run for every getSession/useSession calls.
+// it gets passed what the above jwt callback return as `token`
 export const session: CallbacksOptions['session'] = async ({
   session,
   token
+}: {
+  session: Session
+  token: JWT
 }) => {
-  session.user = token.user as DefaultSession['user']
+  if (token.user as User) {
+    const dbUser = await prisma.user.findFirst({
+      where: {
+        id: toNumber((token.user as User).id)
+      }
+    })
+
+    session.user = excludeUserData(dbUser)
+  }
+
   return session
 }
