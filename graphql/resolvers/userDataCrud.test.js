83846cc2b578f8571971832e3a217d8ae0262242
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-12-28 02:28:55 +0400

    feat: Create userUpdatePassword resolver
    This resolver will be used for changing the user password in the settings/account page

--- graphql/resolvers/userDataCrud.test.js
+++ graphql/resolvers/userDataCrud.test.js
@@ -2,50 +2,157 @@
  * @jest-environment node
  */
 
+jest.mock('bcrypt')
+
 import prismaMock from '../../__tests__/utils/prismaMock'
-import { updateUserNames } from './userDataCrud'
+import { updateUserNames, updateUserPassword } from './userDataCrud'
+import bcrypt from 'bcrypt'
 
 const mockUsername = 'birdCatcher'
 const mockName = 'bird spike'
+const mockPassword = 'password'
+const newPasswordMock = 'drowssap'
 
 describe('User Data CRUD resolvers', () => {
-  it('Should update the user username and name', async () => {
-    expect.assertions(1)
+  describe('Update user names resolver', () => {
+    it('Should update the user username and name', async () => {
+      expect.assertions(1)
 
-    const userMock = {
-      username: mockUsername,
-      name: mockName
-    }
+      const userMock = {
+        username: mockUsername,
+        name: mockName
+      }
 
-    prismaMock.user.update.mockResolvedValueOnce(userMock)
-    prismaMock.user.findFirst.mockResolvedValueOnce(null)
+      prismaMock.user.update.mockResolvedValueOnce(userMock)
+      prismaMock.user.findFirst.mockResolvedValueOnce(null)
 
-    expect(
-      updateUserNames(
-        null,
-        { username: mockUsername, mockName },
-        { req: { user: { id: 1 } } }
-      )
-    ).resolves.toStrictEqual(userMock)
+      expect(
+        updateUserNames(
+          null,
+          { username: mockUsername, mockName },
+          { req: { user: { id: 1 } } }
+        )
+      ).resolves.toStrictEqual(userMock)
+    })
+
+    it('Should not update the user username and name if username already used', async () => {
+      expect.assertions(1)
+
+      const userMock = {
+        username: mockUsername,
+        name: mockName
+      }
+
+      prismaMock.user.update.mockResolvedValueOnce(userMock)
+      prismaMock.user.findFirst.mockResolvedValueOnce(userMock)
+
+      expect(
+        updateUserNames(
+          null,
+          { username: mockUsername, mockName },
+          { req: { user: { id: 1 } } }
+        )
+      ).rejects.toEqual(Error('Username is already used'))
+    })
   })
+  describe('Update user password resolver', () => {
+    it('Should update the user password', async () => {
+      expect.assertions(1)
 
-  it('Should not update the user username and name if username already used', async () => {
-    expect.assertions(1)
+      const userMock = {
+        username: mockUsername,
+        name: mockName,
+        password: mockPassword
+      }
 
-    const userMock = {
-      username: mockUsername,
-      name: mockName
-    }
+      prismaMock.user.findFirst.mockResolvedValueOnce(userMock)
+      bcrypt.compare.mockResolvedValueOnce(true)
 
-    prismaMock.user.update.mockResolvedValueOnce(userMock)
-    prismaMock.user.findFirst.mockResolvedValueOnce(userMock)
+      expect(
+        updateUserPassword(
+          null,
+          {
+            newPassword: newPasswordMock,
+            currentPassword: mockPassword,
+            newPasswordAgain: newPasswordMock
+          },
+          { req: { user: { id: 1 } } }
+        )
+      ).resolves.toEqual({ success: true })
+    })
+    it('Should not update the user password if the new password and the re-entered one do not match', async () => {
+      expect.assertions(1)
 
-    expect(
-      updateUserNames(
-        null,
-        { username: mockUsername, mockName },
-        { req: { user: { id: 1 } } }
-      )
-    ).rejects.toEqual(Error('Username is already used'))
+      const userMock = {
+        username: mockUsername,
+        name: mockName,
+        password: mockPassword
+      }
+
+      prismaMock.user.findFirst.mockResolvedValueOnce(userMock)
+
+      expect(
+        updateUserPassword(
+          null,
+          {
+            newPassword: newPasswordMock,
+            currentPassword: mockPassword,
+            newPasswordAgain: newPasswordMock.slice(
+              0,
+              newPasswordMock.length - 2
+            )
+          },
+          { req: { user: { id: 1 } } }
+        )
+      ).rejects.toEqual(Error("Passwords don't match"))
+    })
+    it('Should not update the user password if the current password is invalid', async () => {
+      expect.assertions(1)
+
+      const userMock = {
+        username: mockUsername,
+        name: mockName,
+        password: mockPassword
+      }
+
+      prismaMock.user.findFirst.mockResolvedValueOnce(userMock)
+      bcrypt.compare.mockResolvedValueOnce(false)
+
+      expect(
+        updateUserPassword(
+          null,
+          {
+            newPassword: newPasswordMock,
+            currentPassword: 'someWrongPassword',
+            newPasswordAgain: newPasswordMock
+          },
+          { req: { user: { id: 1 } } }
+        )
+      ).rejects.toEqual(Error('Current password is invalid'))
+    })
+    it('Should not update the user password if the user password is not found', async () => {
+      expect.assertions(1)
+
+      const userMock = {
+        username: mockUsername,
+        name: mockName,
+        password: null
+      }
+
+      prismaMock.user.findFirst.mockResolvedValueOnce(userMock)
+      bcrypt.compare.mockResolvedValueOnce(false)
+
+      expect(
+        updateUserPassword(
+          null,
+          {
+            newPassword: newPasswordMock,
+            currentPassword: 'someWrongPassword',
+            newPasswordAgain: newPasswordMock
+          },
+          { req: { user: { id: 1 } } }
+        )
+      ).rejects.toEqual(Error('Current password is invalid'))
+    })
   })
 })
