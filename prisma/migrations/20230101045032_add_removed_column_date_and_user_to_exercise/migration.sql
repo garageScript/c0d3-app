bb3af6dff42ec660c6cfd8843f7345a8ed73e598
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-01-01 09:03:50 +0400

    Revert "feat: Add Remove exercise fields to Exercise schema"
    This reverts commit 68a5a3df405b4d73243a18bf365a39fb55fb781b.

--- prisma/migrations/20230101045032_add_removed_column_date_and_user_to_exercise/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "exercises" ADD COLUMN     "removedAt" TIMESTAMP(3),
-ADD COLUMN     "removedById" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "exercises" ADD CONSTRAINT "exercises_removedById_fkey" FOREIGN KEY ("removedById") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
