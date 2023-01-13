43e3484f98c3a469a45ded5b46c2b87d70fa02a1
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-01-13 21:18:33 +0400

    feat: Handle when an exercise is already flagged

--- stories/components/ExerciseReportCard.stories.tsx
+++ stories/components/ExerciseReportCard.stories.tsx
@@ -34,7 +34,36 @@ export const Basic = () => {
           }
         ]}
       >
-        <ExerciseReportCard exerciseId={1} />
+        <ExerciseReportCard exerciseId={1} flaggedAt={!!''} />
+      </MockedProvider>
+    </div>
+  )
+}
+
+export const FlaggedExercise = () => {
+  return (
+    <div>
+      <MockedProvider
+        mocks={[
+          {
+            request: {
+              query: FLAG_EXERCISE,
+              variables: {
+                id: 1,
+                flagReason: ''
+              }
+            },
+            result: {
+              data: {
+                flagExercise: {
+                  id: 1
+                }
+              }
+            }
+          }
+        ]}
+      >
+        <ExerciseReportCard exerciseId={1} flaggedAt={!!'today'} />
       </MockedProvider>
     </div>
   )
