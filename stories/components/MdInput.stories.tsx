28e182458e7bc24074e44cad135e2dc61deeef8f
Scott Hallock <scott.hallock@gmail.com>
2023-03-31 01:31:02 -0500

    improve MdInput stories

--- stories/components/MdInput.stories.tsx
+++ stories/components/MdInput.stories.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Story } from '@storybook/react'
 import { MdInput } from '../../components/MdInput'
 import { Button } from '../../components/theme/Button'
 
@@ -7,18 +8,46 @@ export default {
   title: 'Components/MdInput'
 }
 
-export const Basic: React.FC = () => <MdInput />
-
-export const White: React.FC = () => <MdInput bgColor={'white'} />
-
-export const _WithPresetValue: React.FC = () => {
-  return <MdInput bgColor={'white'} value="HinGa DIngA dUrgEN" />
+const Template: Story = (args: { presetValue?: string }) => {
+  // Mock state of parent component (ReviewCard.tsx)
+  // so that MdInput responds to typing
+  const [commentValue, setCommentValue] = useState(args.presetValue || '')
+  return <MdInput value={commentValue} onChange={setCommentValue} {...args} />
 }
 
-export const _WithSubmissionButtons: React.FC = () => {
+export const Basic = Template.bind({})
+
+export const White = Template.bind({})
+White.args = {
+  bgColor: 'white',
+  presetValue:
+    'Did you notice the white background instead of the default light gray?'
+}
+
+export const WithPresetValue = Template.bind({})
+WithPresetValue.args = {
+  presetValue: `Well done! Keep up the good work :)
+
+Info: there is a \`Array.prototype.reduceRight\` function which you could have used`
+}
+
+export const WithHTMLStrings = Template.bind({})
+WithHTMLStrings.args = {
+  presetValue: `What does the Preview look like...
+
+... if I wrap the HTML in backticks?
+
+\`<div style="color: white; background-color: purple; >C0D3 C0D3 C0D3</div>\`
+
+... if I don't?
+
+<div style="color: white; background-color: purple; padding: 10px">C0D3 C0D3 C0D3</div>`
+}
+
+export const WithSubmissionButtons = () => {
   return (
     <div>
-      <MdInput bgColor={'white'} />
+      <MdInput value="This input does not respond to typing" />
       <Button m="1" btnType="success" color="white">
         Accept
       </Button>
