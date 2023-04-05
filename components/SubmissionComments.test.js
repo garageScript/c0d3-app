8601a300e229dc980be50b0fabb77581ca5c8d71
Scott Hallock <scott.hallock@gmail.com>
2023-04-05 18:27:17 -0400

    refactor: remove unneeded waitFor() calls

--- components/SubmissionComments.test.js
+++ components/SubmissionComments.test.js
@@ -149,7 +149,7 @@ describe('Test SubmissionComments Component', () => {
       variables: { userId: 3, challengeId: 6 },
       data: { getPreviousSubmissions: submissionsData }
     })
-    expect.assertions(4)
+
     const { container, findByText } = render(
       <MockedProvider cache={cache} addTypename={false} mocks={mocks}>
         <Wrapper>
@@ -162,20 +162,22 @@ describe('Test SubmissionComments Component', () => {
     fireEvent.click(editButton)
     const discardButton = container.querySelector('.btn-light')
 
-    await waitFor(() => {
-      expect(discardButton).toBeInTheDocument()
-      expect(container.querySelector('.btn-info')).toBeInTheDocument()
+    expect(discardButton).toBeInTheDocument()
+    expect(container.querySelector('.btn-info')).toBeInTheDocument()
 
-      const editButtonTwo = container.querySelector('.btn-outline-info.btn-sm')
-      fireEvent.click(editButtonTwo)
-    })
+    const editButtonTwo = container.querySelector('.btn-outline-info.btn-sm')
+    fireEvent.click(editButtonTwo)
 
-    const warningText =
-      'You can only edit one comment in a single comment chain at a time.'
-    expect(await findByText(warningText)).toBeTruthy()
+    expect(
+      await findByText(
+        'You can only edit one comment in a single comment chain at a time.'
+      )
+    ).toBeTruthy()
 
     fireEvent.click(discardButton)
     expect(editButton).toBeInTheDocument()
+
+    expect.assertions(4)
   })
 
   it('should update comment when save changes button is clicked', async () => {
