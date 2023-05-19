922a719b6478967a0501f513c95acff4d4103f2d
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-05-19 19:20:14 +0400

    feat(app): 2849 Send notifications to reviewers on new comment
    Reviewers will now get notified when a new comment is added to a line they previously commented on
    
    Closes: 2849

--- graphql/resolvers/addComment.ts
+++ graphql/resolvers/addComment.ts
@@ -1,5 +1,5 @@
 import prisma from '../../prisma'
-import { MutationAddCommentArgs } from '../index'
+import { InputMaybe, MutationAddCommentArgs } from '../index'
 import { Context } from '../../@types/helpers'
 import { getDiscordMessageUserIdString } from '../../helpers/getDiscordMessageUserIdString'
 import { sendDirectMessage } from '../../helpers/discordBot'
@@ -11,6 +11,129 @@ import {
   PRIMARY_COLOR_HEX,
   PROFILE_URL
 } from '../../constants'
+import { Challenge, Comment, Lesson, Submission, User } from '@prisma/client'
+
+type CommentWithAuthorAndSubmission = Comment & {
+  author: {
+    id: number
+    username: string
+    discordId: string | null
+    name: string
+  }
+  submission: Submission & {
+    user: { id: number; username: string; discordId: string | null }
+    lesson: Lesson
+    challenge: Challenge
+  }
+}
+
+type SendNotificationsParams = {
+  line: InputMaybe<number> | undefined
+  fileName: InputMaybe<string> | undefined
+  submissionId: number
+  comment: CommentWithAuthorAndSubmission
+}
+
+const sendNotifications = async ({
+  line,
+  submissionId,
+  fileName,
+  comment
+}: SendNotificationsParams) => {
+  const restOfComments = await prisma.comment.findMany({
+    where: {
+      line,
+      submissionId,
+      fileName
+    },
+    include: {
+      author: true
+    }
+  })
+
+  const [uniqueParticipants] = restOfComments?.reduce(
+    (
+      acc: [(Comment & { author: User })[], { [key: string]: boolean }],
+      e: Comment & { author: User }
+    ) => {
+      const [arr, map] = acc
+
+      if (!map[e.authorId]) {
+        arr.push(e)
+        map[e.authorId] = true
+      }
+
+      return acc
+    },
+    [[], {}]
+  )
+
+  const { submission, author } = comment
+
+  const notificationEmbed = (isSubmissionOwner: boolean): APIEmbed => {
+    const params = {
+      title: isSubmissionOwner
+        ? `New comment by a reviewer on submission`
+        : `New comment on submission`,
+      description: isSubmissionOwner
+        ? 'commented on your submission to the'
+        : 'added a comment on'
+    }
+
+    return {
+      color: PRIMARY_COLOR_HEX,
+      title: params.title,
+      url: `${CURRICULUM_URL}/${submission.lesson.slug}`,
+      thumbnail: {
+        url: getLessonCoverPNG(submission.lesson.order)
+      },
+      author: {
+        name: author.name,
+        url: `${PROFILE_URL}/${author.username}`,
+        icon_url: C0D3_ICON_URL
+      },
+      description: `${getDiscordMessageUserIdString(author)} ${
+        params.description
+      } challenge **${submission.challenge.title}**${
+        comment.line ? ` on **Line ${comment.line}**` : ''
+      }.`,
+      fields: [
+        {
+          name: 'Comment',
+          value: comment.content
+        }
+      ]
+    }
+  }
+
+  // sends a notification to the submission author
+  if (author.id !== submission.user.id && submission.user.discordId) {
+    await sendDirectMessage(
+      submission.user.discordId,
+      '',
+      notificationEmbed(true)
+    )
+  }
+
+  // sends a notification to all of those who commented on
+  // the same submission line except the comment author
+  uniqueParticipants.forEach(participant => {
+    if (
+      participant.author.id !== author.id ||
+      participant.author.id === submission.user.id
+    ) {
+      return
+    }
+
+    if (participant.author.discordId) {
+      sendDirectMessage(
+        participant.author.discordId,
+        '',
+        notificationEmbed(false)
+      )
+    }
+  })
+}
 
 export const addComment = async (
   _parent: void,
@@ -54,36 +177,12 @@ export const addComment = async (
     }
   })
 
-  const { author, submission } = comment
-
-  if (author.id !== submission.user.id && submission.user.discordId) {
-    const notificationEmbed: APIEmbed = {
-      color: PRIMARY_COLOR_HEX,
-      title: 'New comment on submission',
-      url: `${CURRICULUM_URL}/${submission.lesson.slug}`,
-      thumbnail: {
-        url: getLessonCoverPNG(submission.lesson.order)
-      },
-      author: {
-        name: author.name,
-        url: `${PROFILE_URL}/${author.username}`,
-        icon_url: C0D3_ICON_URL
-      },
-      description: `${getDiscordMessageUserIdString(
-        author
-      )} commented on your submission to the challenge **${
-        submission.challenge.title
-      }**${comment.line ? ` on **Line ${comment.line}**` : ''}.`,
-      fields: [
-        {
-          name: 'Comment',
-          value: comment.content
-        }
-      ]
-    }
-
-    await sendDirectMessage(submission.user.discordId, '', notificationEmbed)
-  }
+  await sendNotifications({
+    line,
+    fileName,
+    submissionId,
+    comment: comment
+  })
 
   return comment
 }
