897b4c998681bc4123efec8f29111b31fa90b397
Flacial <ruthemiumwithdiamond@protonmail.com>
2023-07-15 12:01:11 +0400

    Clean classes

--- components/mdx/ChallengBar.tsx
+++ components/mdx/ChallengBar.tsx
@@ -9,7 +9,7 @@ const ChallengeBar: React.FC<{
 }> = ({ href, description, title }) => {
   return (
     <div className="my-3 mt-5">
-      <div className="d-flex flex-col flex-md-row gap-3 justify-content-between align-items-none align-items-md-center card shadow-sm bg-primary text-white px-4 py-4 border-0">
+      <div className="d-flex flex-col flex-md-row gap-3 justify-content-between align-items-none align-items-md-center card shadow-sm bg-primary text-white p-4 border-0">
         <div>
           <span className="align-self-center fs-4">{title}</span>
           <p className="m-0">{description}</p>
@@ -17,7 +17,7 @@ const ChallengeBar: React.FC<{
         <div className="d-flex flex-column gap-2">
           <NavLink
             path={href}
-            className="btn btn-light text-primary py-3 px-3"
+            className="btn btn-light text-primary p-3"
             external
           >
             View Challenges
